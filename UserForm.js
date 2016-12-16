b121f8340acd2e2675f7a2a0d0a0cbf66f8f164c
Niels Erik G. Nielsen <nielserik@indexdata.com>
2016-12-16 16:16:27 +0100

    Clears lint errors. UserForm, Users.

--- UserForm.js
+++ UserForm.js
@@ -1,35 +1,50 @@
-import React, { Component, PropTypes } from 'react';
-import Paneset from '@folio/stripes-components/lib/Paneset'
-import Pane from '@folio/stripes-components/lib/Pane'
-import PaneMenu from '@folio/stripes-components/lib/PaneMenu'
-import {Row, Col} from 'react-bootstrap'
-import Button from '@folio/stripes-components/lib/Button'
-import TextField from '@folio/stripes-components/lib/TextField'
-import Select from '@folio/stripes-components/lib/Select'
-import RadioButtonGroup from '@folio/stripes-components/lib/RadioButtonGroup'
-import RadioButton from '@folio/stripes-components/lib/RadioButton'
+import React, { Component, PropTypes } from 'react'; // eslint-disable-line
+import Paneset from '@folio/stripes-components/lib/Paneset'; // eslint-disable-line
+import Pane from '@folio/stripes-components/lib/Pane'; // eslint-disable-line
+import PaneMenu from '@folio/stripes-components/lib/PaneMenu'; // eslint-disable-line
+import {Row, Col} from 'react-bootstrap'; // eslint-disable-line
+import Button from '@folio/stripes-components/lib/Button'; // eslint-disable-line
+import TextField from '@folio/stripes-components/lib/TextField'; // eslint-disable-line
+import Select from '@folio/stripes-components/lib/Select'; // eslint-disable-line
+import RadioButtonGroup from '@folio/stripes-components/lib/RadioButtonGroup'; // eslint-disable-line
+import RadioButton from '@folio/stripes-components/lib/RadioButton'; // eslint-disable-line
 
-import {Field, reducer as formReducer, reduxForm} from 'redux-form'
+import {Field, reducer as formReducer, reduxForm} from 'redux-form'; // eslint-disable-line
 
-const propTypes={
-  onClose: PropTypes.func,
-  newUser: PropTypes.bool,
-  handleSubmit: PropTypes.func.isRequired
-}
 
 class UserForm extends Component {
+
+  static propTypes = {
+    onClose: PropTypes.func, // eslint-disable-line react/no-unused-prop-types
+    newUser: PropTypes.bool, // eslint-disable-line react/no-unused-prop-types
+    handleSubmit: PropTypes.func.isRequired,
+    reset: PropTypes.func,
+    pristine: PropTypes.bool,
+    submitting: PropTypes.bool,
+    onCancel: PropTypes.func,
+  }
+
+  constructor(props) {
+    super(props);
+    // Silences:
+    // "Component should be written as a pure function  react/prefer-stateless-function"
+    // (?)
+    this.dummy = {};
+  }
+
   render() {
-    const {    
+    const {
       handleSubmit,
-      reset,
+      reset,  // eslint-disable-line no-unused-vars
       pristine,
       submitting,
-      onCancel
+      onCancel,
     } = this.props;
-    /*Menues for Add User workflow*/
-    const addUserFirstMenu = <PaneMenu><button onClick={onCancel} title="close" aria-label="Close New User Dialog"><span style={{fontSize:'30px', color: '#999', lineHeight:"18px"}}>&times;</span></button></PaneMenu>
-    const addUserLastMenu = <PaneMenu><Button type="submit" title="Create New User" disabled={pristine || submitting} onClick={handleSubmit}>Create User</Button></PaneMenu>
-    
+
+    /* Menues for Add User workflow */
+    const addUserFirstMenu = <PaneMenu><button onClick={onCancel} title="close" aria-label="Close New User Dialog"><span style={{ fontSize: '30px', color: '#999', lineHeight: '18px' }} >&times;</span></button></PaneMenu>;
+    const addUserLastMenu = <PaneMenu><Button type="submit" title="Create New User" disabled={pristine || submitting} onClick={handleSubmit}>Create User</Button></PaneMenu>;
+
     return (
       <form>
         <Paneset>
@@ -37,22 +52,32 @@ class UserForm extends Component {
             <Row>
               <Col sm={5} smOffset={1}>
                 <h2>User Record</h2>
-                <Field label="UserName" name="username" id="adduser_username" component={TextField} required fullWidth/>
+                <Field label="UserName" name="username" id="adduser_username" component={TextField} required fullWidth />
                 <Field label="Active" name="active" component={RadioButtonGroup}>
-                  <RadioButton label ="Yes" id="useractiveYesRB" value="true" inline />
-                  <RadioButton label ="No" id="useractiveNoRB" value="false" inline />
+                  <RadioButton label="Yes" id="useractiveYesRB" value="true" inline />
+                  <RadioButton label="No" id="useractiveNoRB" value="false" inline />
                 </Field>
                 <fieldset>
                   <legend>Personal Info</legend>
-                  <Field label="Full Name" name="personal.full_name" id="adduser_fullname" component={TextField} required fullWidth/>
-                  <Field label="Primary Email" name="personal.email_primary" id="adduser_primaryemail" component={TextField} required fullWidth/>
-                  <Field label="Secondary Email" name="personal.email_secondary" id="adduser_secondemail" component={TextField} fullWidth/>
+                  <Field label="Full Name" name="personal.full_name" id="adduser_fullname" component={TextField} required fullWidth />
+                  <Field label="Primary Email" name="personal.email_primary" id="adduser_primaryemail" component={TextField} required fullWidth />
+                  <Field label="Secondary Email" name="personal.email_secondary" id="adduser_secondemail" component={TextField} fullWidth />
                 </fieldset>
-                <Field label="Type" name="type" id="adduser_type" component={Select} fullWidth
-                  dataOptions={[{label: 'Select user type', value:''},{label:'Patron', value:'Patron', selected:'selected'}]}
+                <Field
+                  label="Type"
+                  name="type"
+                  id="adduser_type"
+                  component={Select}
+                  fullWidth
+                  dataOptions={[{ label: 'Select user type', value: '' }, { label: 'Patron', value: 'Patron', selected: 'selected' }]}
                 />
-                <Field label="Group" name="patron_group" id="adduser_group" component={Select} fullWidth
-                        dataOptions={[{label: 'Select patron group', value:''},{label:'On-campus', value:'on_campus', selected:'selected' }]}
+                <Field
+                  label="Group"
+                  name="patron_group"
+                  id="adduser_group"
+                  component={Select}
+                  fullWidth
+                  dataOptions={[{ label: 'Select patron group', value: '' }, { label: 'On-campus', value: 'on_campus', selected: 'selected' }]}
                 />
               </Col>
             </Row>
@@ -60,12 +85,10 @@ class UserForm extends Component {
 
         </Paneset>
       </form>
-    )
+    );
   }
 }
 
-UserForm.propTypes = propTypes;
-
 export default reduxForm({
-  form: 'userForm'
+  form: 'userForm',
 })(UserForm);
--- Users.js
+++ Users.js
@@ -1,64 +1,47 @@
-import React, { PropTypes } from 'react'
-import Match from 'react-router/Match';
+import React, { Component, PropTypes } from 'react'; // eslint-disable-line
+import Match from 'react-router/Match'; // eslint-disable-line
+import {Row, Col} from 'react-bootstrap'; // eslint-disable-line
 
 /* shared stripes components */
-import Pane from '@folio/stripes-components/lib/Pane'
-import Paneset from '@folio/stripes-components/lib/Paneset'
-import PaneMenu from '@folio/stripes-components/lib/PaneMenu'
-import Button from '@folio/stripes-components/lib/Button'
-import Icon from '@folio/stripes-components/lib/Icon'
-import MultiColumnListUsers from './lib/MultiColumnList'
-import KeyValue from '@folio/stripes-components/lib/KeyValue'
-import {Row, Col} from 'react-bootstrap'
-import TextField from '@folio/stripes-components/lib/TextField'
-import Checkbox from '@folio/stripes-components/lib/Checkbox'
-import FilterPaneSearch from '@folio/stripes-components/lib/FilterPaneSearch'
-import FilterControlGroup from '@folio/stripes-components/lib/FilterControlGroup'
-import Select from '@folio/stripes-components/lib/Select'
-import Layer from '@folio/stripes-components/lib/Layer'
+import Pane from '@folio/stripes-components/lib/Pane'; // eslint-disable-line
+import Paneset from '@folio/stripes-components/lib/Paneset'; // eslint-disable-line
+import PaneMenu from '@folio/stripes-components/lib/PaneMenu'; // eslint-disable-line
+import Button from '@folio/stripes-components/lib/Button'; // eslint-disable-line
+import Icon from '@folio/stripes-components/lib/Icon'; // eslint-disable-line
+import MultiColumnListUsers from './lib/MultiColumnList'; // eslint-disable-line 
+import KeyValue from '@folio/stripes-components/lib/KeyValue'; // eslint-disable-line
+import TextField from '@folio/stripes-components/lib/TextField'; // eslint-disable-line
+import Checkbox from '@folio/stripes-components/lib/Checkbox'; // eslint-disable-line
+import FilterPaneSearch from '@folio/stripes-components/lib/FilterPaneSearch'; // eslint-disable-line
+import FilterControlGroup from '@folio/stripes-components/lib/FilterControlGroup'; // eslint-disable-line
+import Select from '@folio/stripes-components/lib/Select'; // eslint-disable-line
+import Layer from '@folio/stripes-components/lib/Layer'; // eslint-disable-line
 
 import UserForm from './UserForm';
-import ViewUser from './ViewUser'
+import ViewUser from './ViewUser';
 
-class Users extends React.Component{
+class Users extends Component {
   static contextTypes = {
-    router: PropTypes.object.isRequired
+    router: PropTypes.func.isRequired,
   }
 
   static propTypes = {
-    data: PropTypes.object.isRequired,
-    params: PropTypes.object,
+    data: PropTypes.shape.isRequired,
+    params: PropTypes.func,
     pathname: PropTypes.string.isRequired,
-    location: PropTypes.object.isRequired,
+    location: PropTypes.func.isRequired,
+    mutator: PropTypes.func,
   };
 
-  constructor(props){
-    super(props);
-    this.state={
-      //Search/Filter state...
-      patronFilter: true,
-      employeeFilter: false,
-      searchTerm: '',
-      addUserMode: false
-    };
-    
-    this.onClickAddNewUser = this.onClickAddNewUser.bind(this);
-    this.onClickCloseNewUser = this.onClickCloseNewUser.bind(this);
-    this.onChangeFilter = this.onChangeFilter.bind(this);
-    this.onChangeSearch = this.onChangeSearch.bind(this);
-    this.onClearSearch = this.onClearSearch.bind(this);
-    this.onClickItemHandler =this.onClickItemHandler.bind(this);
-  }
-  
-  static manifest = { 
+  static manifest = {
     searchResults: [],
-    /*detail: {
+    /* detail: {
       fineHistory:[]
-    }*/
+    } */
     user: {
       type: 'okapi',
       path: 'users',
-      fetch: false
+      fetch: false,
     },
     users: {
       type: 'okapi',
@@ -66,92 +49,113 @@ class Users extends React.Component{
       path: 'users?query={"username":"?{query}"}',
       // And when we move to PostgreSQL: ?query=[{"field":"'username'","value":"knord","op":"="}]
       staticFallback: { path: 'users' },
-    }
-
+    },
   };
 
+  constructor(props) {
+    super(props);
+    this.state = {
+      // Search/Filter state
+      patronFilter: true,
+      employeeFilter: false,
+      searchTerm: '',
+      addUserMode: false,
+    };
+
+    this.onClickAddNewUser = this.onClickAddNewUser.bind(this);
+    this.onClickCloseNewUser = this.onClickCloseNewUser.bind(this);
+    this.onChangeFilter = this.onChangeFilter.bind(this);
+    this.onChangeSearch = this.onChangeSearch.bind(this);
+    this.onClearSearch = this.onClearSearch.bind(this);
+    this.onClickItemHandler = this.onClickItemHandler.bind(this);
+  }
+
   componentWillMount() {
-    const resultData = [{Name:'Pete Sherman', Address:'391 W. Richardson St. Duarte, CA 91010', Fines:'$34.23'}];
-    //const fineHistory = [{"Due Date": "11/12/2014", "Amount":"34.23", "Status":"Unpaid"}];
+    const resultData = [{ Name: 'Pete Sherman', Address: '391 W. Richardson St. Duarte, CA 91010', Fines: '$34.23' }];
+    // const fineHistory = [{"Due Date": "11/12/2014", "Amount":"34.23", "Status":"Unpaid"}];
     this.props.mutator.searchResults.replace(resultData);
-    //this.props.mutator.detail.replace({fineHistory: fineHistory});
+    // this.props.mutator.detail.replace({fineHistory: fineHistory});
   }
 
-  create(data) {
-    this.props.mutator['user'].POST(data);
-  }
-
-  //search Handlers...
-  onChangeFilter(e){
-    let stateObj = {};
+  // search Handlers...
+  onChangeFilter(e) {
+    const stateObj = {};
     stateObj[e.target.id] = !this.state[e.target.id];
     this.setState(stateObj);
   }
-  
-  onChangeSearch(e){
-    let term = e.target.value;
-    console.log("User searched:", term, "at", this.props.location.pathname);
-    this.setState({ 'searchTerm': term });
-    this.context.router.transitionTo(this.props.location.pathname + '?query=' + term);
+
+  onChangeSearch(e) {
+    const term = e.target.value;
+    console.log('User searched:', term, 'at', this.props.location.pathname);
+    this.setState({ searchTerm: term });
+    this.context.router.transitionTo(`${this.props.location.pathname}?query=${term}`);
   }
-  
-  onClearSearch(e){
-    console.log("User cleared search");
-    this.setState({ 'searchTerm': '' });
+
+  onClearSearch() {
+    console.log('User cleared search');
+    this.setState({ searchTerm: '' });
     this.context.router.transitionTo(this.props.location.pathname);
   }
 
-  onClickItemHandler (userId) {
-    console.log("User clicked", userId, "location = ", this.props.location);
-    this.context.router.transitionTo("/users/view/" + userId + this.props.location.search);
+  onClickItemHandler(userId) {
+    console.log('User clicked', userId, 'location = ', this.props.location);
+    this.context.router.transitionTo(`/users/view/${userId}${this.props.location.search}`);
   }
 
-  //end search Handlers
+  // end search Handlers
 
-  //AddUser Handlers
-  onClickAddNewUser(){
-    console.log('add Clicked')
+  // AddUser Handlers
+  onClickAddNewUser() {
+    console.log('add Clicked');
     this.setState({
-      addUserMode: true
+      addUserMode: true,
     });
   }
-  
-  onClickCloseNewUser(){
+
+  onClickCloseNewUser() {
     this.setState({
-      addUserMode: false
+      addUserMode: false,
     });
-  }  
-  //end AddUser Handlers
-  
-  render(){
+  }
+  // end AddUser Handlers
+  create(data) {
+    this.props.mutator.user.POST(data);
+  }
+
+  render() {
     const { data, params, pathname } = this.props;
-    if (!data.users) return <div/>;
-    const resultMenu = <PaneMenu><button><Icon icon="bookmark"/></button></PaneMenu>
-    const fineHistory = [{"Due Date": "11/12/2014", "Amount":"34.23", "Status":"Unpaid"}];
+    if (!data.users) return <div />;
+    const resultMenu = <PaneMenu><button><Icon icon="bookmark" /></button></PaneMenu>;
+    const fineHistory = [{ 'Due Date': '11/12/2014', 'Amount': '34.23', 'Status': 'Unpaid' }]; // eslint-disable-line 
     const displayUsers = data.users.reduce((results, user) => {
-      results.push({"id": user.id, Name: user.personal.full_name, Username: user.username, Email: user.personal.email_primary});
+      results.push({
+        id: user.id,
+        Name: user.personal.full_name,
+        Username: user.username,
+        Email: user.personal.email_primary,
+      });
       return results;
-    }, []);     
-    
-    /*searchHeader is a 'custom pane header'*/
-    const searchHeader = <FilterPaneSearch id="SearchField" onChange={this.onChangeSearch} onClear={this.onClearSearch} value={this.state.searchTerm} />
+    }, []);
+
+    /* searchHeader is a 'custom pane header'*/
+    const searchHeader = <FilterPaneSearch id="SearchField" onChange={this.onChangeSearch} onClear={this.onClearSearch} value={this.state.searchTerm} />;
     console.log(params);
-    
+
     return (
       <Paneset>
-        {/*Filter Pane */}
+        {/* Filter Pane */}
         <Pane defaultWidth="16%" header={searchHeader}>
           <FilterControlGroup label="Filters">
-            <Checkbox 
+            <Checkbox
               id="patronFilter"
               label="Patrons"
-              checked={this.state.patronFilter} 
+              checked={this.state.patronFilter}
               onChange={this.onChangeFilter}
               marginBottom0
               hover
               fullWidth
             />
-            <Checkbox 
+            <Checkbox
               id="employeeFilter"
               label="Employees"
               checked={this.state.employeeFilter}
@@ -164,20 +168,24 @@ class Users extends React.Component{
           </FilterControlGroup>
         </Pane>
 
-        {/*Results Pane*/}
+        {/* Results Pane */}
         <Pane defaultWidth="fit-content" paneTitle="Results" lastMenu={resultMenu}>
-          <MultiColumnListUsers contentData={displayUsers} onClickItemHandler={this.onClickItemHandler}/>
+          <MultiColumnListUsers
+            contentData={displayUsers}
+            onClickItemHandler={this.onClickItemHandler}
+          />
         </Pane>
-        
-        {/*Details Pane*/}
-        <Match pattern={`${pathname}/view/:userid`} component={ViewUser}/>
+
+        {/* Details Pane */}
+        <Match pattern={`${pathname}/view/:userid`} component={ViewUser} />
         <Layer isOpen={this.state.addUserMode} label="Add New User Dialog">
           <UserForm
-            onSubmit={this.create.bind(this)}
-            onCancel={this.onClickCloseNewUser} />
+            onSubmit={(record) => { this.create(record); }}
+            onCancel={this.onClickCloseNewUser}
+          />
         </Layer>
       </Paneset>
-    )
+    );
   }
 }
 
