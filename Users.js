820451631c045d2a42a7a9bba62cad26e201143a
JCoburn <JCoburn@CORP-IT-250WM6.ebsco.com>
2017-01-05 08:41:52 -0600

    refix width of Results column...

--- Users.js
+++ Users.js
@@ -168,7 +168,7 @@ class Users extends Component {
         </Pane>
 
         {/* Results Pane */}
-        <Pane defaultWidth="fit-content" paneTitle="Results" lastMenu={resultMenu}>
+        <Pane defaultWidth="32%" paneTitle="Results" lastMenu={resultMenu}>
           <MultiColumnListUsers
             contentData={displayUsers}
             onClickItemHandler={this.onClickItemHandler}
