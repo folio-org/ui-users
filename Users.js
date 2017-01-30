9442400955209f592d7556952dcfbf937728dbce
Mike Taylor <mike@indexdata.com>
2017-01-30 18:10:49 +0000

    Reorder stripes-components imports to match ui-items/Items.js

--- Users.js
+++ Users.js
@@ -8,10 +8,10 @@ import Paneset from '@folio/stripes-components/lib/Paneset'; // eslint-disable-l
 import PaneMenu from '@folio/stripes-components/lib/PaneMenu'; // eslint-disable-line
 import Button from '@folio/stripes-components/lib/Button'; // eslint-disable-line
 import Icon from '@folio/stripes-components/lib/Icon'; // eslint-disable-line
+import Checkbox from '@folio/stripes-components/lib/Checkbox'; // eslint-disable-line
+import TextField from '@folio/stripes-components/lib/TextField'; // eslint-disable-line
 import MultiColumnList from '@folio/stripes-components/lib/MultiColumnList'; // eslint-disable-line
 import KeyValue from '@folio/stripes-components/lib/KeyValue'; // eslint-disable-line
-import TextField from '@folio/stripes-components/lib/TextField'; // eslint-disable-line
-import Checkbox from '@folio/stripes-components/lib/Checkbox'; // eslint-disable-line
 import FilterPaneSearch from '@folio/stripes-components/lib/FilterPaneSearch'; // eslint-disable-line
 import FilterControlGroup from '@folio/stripes-components/lib/FilterControlGroup'; // eslint-disable-line
 import Select from '@folio/stripes-components/lib/Select'; // eslint-disable-line
