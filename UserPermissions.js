ef5dd98d602ad0abb5612eb160da0d69c2668586
jeremythuff <huff@library.tamu.edu>
2017-03-06 15:30:23 -0800

    Permission names are now displaying displayName

--- UserPermissions.js
+++ UserPermissions.js
@@ -64,10 +64,11 @@ class UserPermissions extends React.Component {
     const permissionsDD = (
       <ListDropdown
         items={_.filter(this.props.availablePermissions, function(perm) {
+
           const permInUse = _.some(usersPermissions, perm);
 
           // This should be replaced with proper search when possible.
-          const permNotFiltered = _.includes(perm.permissionName.toLowerCase(), this.state.searchTerm.toLowerCase());
+          const permNotFiltered = _.includes(perm.displayName.toLowerCase(), this.state.searchTerm.toLowerCase());
 
           return !permInUse && permNotFiltered;
         }.bind(this))}
@@ -78,7 +79,7 @@ class UserPermissions extends React.Component {
 
     const listFormatter = item => (
       <li key={item.permissionName} >
-        {item.permissionName}
+        {item.displayName}
         <Button
           buttonStyle="fieldControl"
           align="end"
