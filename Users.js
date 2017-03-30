8a2fb4303a31c9de57dcb53b77815481c6eafeef
Mike Taylor <mike@indexdata.com>
2017-03-30 16:39:30 +0100

    Pass paneWidth into ViewUser. Last part of STRIPES-288.

--- Users.js
+++ Users.js
@@ -286,7 +286,9 @@ class Users extends React.Component {
         </Pane>
 
         {/* Details Pane */}
-        <Route path={`${this.props.match.path}/view/:userid/:username`} render={props => <this.connectedViewUser stripes={stripes} {...props} />} />
+        <Route
+          path={`${this.props.match.path}/view/:userid/:username`}
+          render={props => <this.connectedViewUser stripes={stripes} paneWidth="44%" {...props} />} />
         <Layer isOpen={data.addUserMode ? data.addUserMode.mode : false} label="Add New User Dialog">
           <UserForm
             initialValues={{ available_patron_groups: this.props.data.patronGroups }}
