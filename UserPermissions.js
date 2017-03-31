63074922b8a7981c2a615b95b8e56c08c8c56dae
jeremythuff <jeremythuff@gmail.com>
2017-03-31 15:31:30 -0500

    (WIP) Seperating rendering and connect work for permissions

--- UserPermissions.js
+++ UserPermissions.js
@@ -2,14 +2,7 @@ import _ from 'lodash';
 // We have to remove node_modules/react to avoid having multiple copies loaded.
 // eslint-disable-next-line import/no-unresolved
 import React, { PropTypes } from 'react';
-import { Row, Col, Dropdown } from 'react-bootstrap';
-import DropdownMenu from '@folio/stripes-components/lib/DropdownMenu';
-import Button from '@folio/stripes-components/lib/Button';
-import Icon from '@folio/stripes-components/lib/Icon';
-import List from '@folio/stripes-components/lib/List';
-import IfPermission from '@folio/stripes-components/lib/IfPermission';
-import ListDropdown from './lib/ListDropdown';
-import css from './UserPermissions.css';
+import RenderPermissions from './lib/RenderPermissions';
 
 const propTypes = {
   stripes: PropTypes.shape({
@@ -55,6 +48,7 @@ class UserPermissions extends React.Component {
 
     this.onToggleAddPermDD = this.onToggleAddPermDD.bind(this);
     this.addPermission = this.addPermission.bind(this);
+    this.removePermission = this.removePermission.bind(this);
     this.onChangeSearch = this.onChangeSearch.bind(this);
     this.isPermAvailable = this.isPermAvailable.bind(this);
   }
@@ -78,6 +72,7 @@ class UserPermissions extends React.Component {
   }
 
   removePermission(perm) {
+    console.log(this.props);
     this.props.mutator.userPermissions.DELETE(perm, this.props.viewUserProps, perm.permissionName);
   }
 
@@ -92,65 +87,21 @@ class UserPermissions extends React.Component {
   }
 
   render() {
+
     const { userPermissions } = this.props.data;
 
-    if (!this.props.stripes.hasPerm('perms.users.read'))
-      return null;
-
-    const permissionsDD = (
-      <ListDropdown
-        items={_.filter(this.props.availablePermissions, this.isPermAvailable)}
-        onClickItem={this.addPermission}
-        onChangeSearch={this.onChangeSearch}
-      />
-    );
-
-    const listFormatter = item => (
-      <li key={item.permissionName} >
-        {!item.displayName ? item.permissionName : item.displayName}
-        <Button
-          buttonStyle="fieldControl"
-          align="end"
-          type="button"
-          onClick={() => this.removePermission(item)}
-          aria-label={`Remove Permission: ${item.permissionName}`}
-          title="Remove Permission"
-        >
-          <IfPermission {...this.props} perm="perms.users.delete">
-            <Icon icon="hollowX" iconClassName={css.removePermissionIcon} iconRootClass={css.removePermissionButton} />
-          </IfPermission>
-        </Button>
-      </li>
-    );
-
-    return (
-      <div style={{ marginBottom: '1rem' }}>
-        <hr />
-        <Row>
-          <Col xs={5}>
-            <h3 className="marginTop0">User permissions</h3>
-          </Col>
-          {/* <Col xs={4} sm={3}>
-            <TextField
-              rounded
-              endControl={<Button buttonStyle="fieldControl"><Icon icon='clearX'/></Button>}
-              startControl={<Icon icon='search'/>}
-              placeholder="Search"
-              fullWidth
-              />
-          </Col>*/}
-          <Col xs={7}>
-            <IfPermission {...this.props} perm="perms.users.modify">
-              <Dropdown open={this.state.addPermissionOpen} pullRight onToggle={this.onToggleAddPermDD} id="AddPermissionDropdown" style={{ float: 'right' }}>
-                <Button align="end" bottomMargin0 bsRole="toggle" aria-haspopup="true">&#43; Add Permission</Button>
-                <DropdownMenu bsRole="menu" onToggle={this.onToggleAddPermDD} aria-label="available permissions" width="40em">{permissionsDD}</DropdownMenu>
-              </Dropdown>
-            </IfPermission>
-          </Col>
-        </Row>
-        <List itemFormatter={listFormatter} items={userPermissions || []} isEmptyMessage="This user has no permissions applied." />
-      </div>
-    );
+    return (<RenderPermissions 
+              {...this.props}
+              heading="User permissions" 
+              isPermAvailable={this.isPermAvailable} 
+              addPermission={this.addPermission} 
+              onChangeSearch={this.onChangeSearch} 
+              removePermission={this.removePermission} 
+              state={this.state} 
+              onToggleAddPermDD={this.onToggleAddPermDD}
+              userPermissions={userPermissions}
+           ></RenderPermissions>);
+      
   }
 }
 
