7f6c7137421e2b65fc737d9b8aca5c38b3e1c8c2
Mike Taylor <mike@indexdata.com>
2017-04-26 11:04:05 +0100

    Displaying user permissions now checks for users.item.put
    Previously it checked for users-bl.item.put, and it worked. But it
    seems to have changed again in v0.12.0 of folio-backend-auth.
    
    (To be fair, the new permission makes more sense to me. But I am about
    ready for them to stop changing.)

--- ViewUser.js
+++ ViewUser.js
@@ -112,7 +112,7 @@ class ViewUser extends Component {
     const { data: { users, patronGroups }, match: { params: { userid } } } = this.props;
 
     const detailMenu = (<PaneMenu>
-      <IfPermission {...this.props} perm="users-bl.item.put">
+      <IfPermission {...this.props} perm="users.item.put">
         <button onClick={this.onClickEditUser} title="Edit User"><Icon icon="edit" />Edit</button>
       </IfPermission>
     </PaneMenu>);
