d564af4237c5ad28defa7bdc6ad828edba982fbb
Mike Taylor <mike@indexdata.com>
2017-05-02 20:04:42 +0100

    Fix all ESLint errors
    Includes switching to functional style.
    
    Fixes STRIPES-229.

--- settings/index.js
+++ settings/index.js
@@ -10,11 +10,10 @@ import Pane from '@folio/stripes-components/lib/Pane';
 import NavList from '@folio/stripes-components/lib/NavList';
 import NavListSection from '@folio/stripes-components/lib/NavListSection';
 
+// Should this list be loaded dynamically from configuration?
 import PermissionSets from './PermissionSets';
 import PatronGroupsSettings from './PatronGroupsSettings';
 
-
-// Should this list be loaded dynamically from configuration?
 const pages = [
   {
     route: 'perms',
@@ -34,51 +33,48 @@ const pages = [
 ];
 
 
-class UsersSettings extends React.Component {
-  static propTypes = {
-    stripes: PropTypes.shape({
-      hasPerm: PropTypes.func.isRequired,
-    }).isRequired,
-  };
+const UsersSettings = (props) => {
+  const navLinks = pages.map(p => (
+    <Link
+      key={p.route}
+      to={`/settings/users/${p.route}`}
+    >{p.label}</Link>
+  ));
 
-  render() {
-    const navLinks = pages.map(p => (
-      <Link
-        key={p.route}
-        to={`/settings/users/${p.route}`}
-      >
-        {p.label}
-      </Link>
-    ));
+  const routes = pages.map((p) => {
+    const connect = props.stripes.connect;
+    const Current = connect(p.component);
 
-    const routes = pages.map((p) => {
-      const connect = this.props.stripes.connect;
-      const Current = connect(p.component);
+    return (<Route
+      key={p.route}
+      path={`/settings/users/${p.route}`}
+      render={props2 => <Current {...props2} stripes={props.stripes} />}
+    />);
+  });
 
-      return (<Route
-        key={p.route}
-        path={`/settings/users/${p.route}`}
-        render={props => <Current {...props} stripes={this.props.stripes} />}
-      />);
-    });
+  return (
+    <Paneset nested defaultWidth="80%">
+      <Pane defaultWidth="25%" paneTitle="Users">
+        <NavList>
+          <NavListSection activeLink="">
+            {navLinks}
+          </NavListSection>
+        </NavList>
+      </Pane>
 
-    return (
-      <Paneset nested defaultWidth="80%">
-        <Pane defaultWidth="25%" paneTitle="Users">
-          <NavList>
-            <NavListSection activeLink="">
-              {navLinks}
-            </NavListSection>
-          </NavList>
-        </Pane>
-        
-        <Switch>
-          {routes}
-          <Route component={() => <div>Choose category</div>} />
-        </Switch>
-      </Paneset>
-    );
-  }
-}
+      <Switch>
+        {routes}
+        <Route component={() => <div>Choose category</div>} />
+      </Switch>
+    </Paneset>
+  );
+};
+
+UsersSettings.propTypes = {
+  stripes: PropTypes.shape({
+    connect: PropTypes.func.isRequired,
+    hasPerm: PropTypes.func.isRequired,
+  }).isRequired,
+};
 
 export default UsersSettings;
