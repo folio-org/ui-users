3ffe6daced193aca37310495bd16cfa631db9e1f
Niels Erik G. Nielsen <nielserik@indexdata.com>
2017-06-08 11:16:17 +0200

    Add client side data files for option lists

---
+++ data/addressTypes.js
@@ -0,0 +1,7 @@
+export default [ { "id": "001", "desc": "Claim"},
+                 { "id": "002", "desc": "Home" },
+                 { "id": "003", "desc": "Order" },
+                 { "id": "004", "desc": "Payment" },
+                 { "id": "005", "desc": "Returns"}, 
+                 { "id": "005", "desc": "Work"},
+               ];
