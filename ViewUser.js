3263b58cb175bb19467fd64be3adfbae7810c4df
Mike Taylor <mike@indexdata.com>
2017-06-22 16:05:56 +0100

    Upgrade loan-storage interface dependency to v2.0
    Fixes UIU-100.
    
    (Note: "fixes" in the sense that loans information will appear on
    http://folio-testing.aws.indexdata.com/ where newer versions of the
    circulation and circulation-storage module are running. Conversely, it
    will vanish on my development box, since I am running against the
    folio-backend-auth VM, version 0.20.0, which has oldeer version of
    these modules that present v1.0 of the interface. C'est la vie.)

--- ViewUser.js
+++ ViewUser.js
@@ -268,7 +268,7 @@ class ViewUser extends Component {
         <MultiColumnList fullWidth contentData={fineHistory} />
         <hr />
         <IfPermission perm="circulation.loans.collection.get">
-          <IfInterface name="loan-storage" version="1.0">
+          <IfInterface name="loan-storage" version="2.0">
             <this.connectedUserLoans onClickViewLoanActionsHistory={this.onClickViewLoanActionsHistory} onClickViewLoansHistory={this.onClickViewLoansHistory} {...this.props} />
           </IfInterface>
         </IfPermission>
