ded9249c9de1e80076b2e168f08a0436d8079929
William Welling <wwelling@library.tamu.edu>
2017-11-17 09:36:49 -0600

    improved patron group setting additional field components

--- settings/PatronGroupsSettings.js
+++ settings/PatronGroupsSettings.js
@@ -1,3 +1,4 @@
+import _ from 'lodash';
 import React from 'react';
 import PropTypes from 'prop-types';
 import ControlledVocab from '@folio/stripes-smart-components/lib/ControlledVocab';
@@ -5,17 +6,71 @@ import ControlledVocab from '@folio/stripes-smart-components/lib/ControlledVocab
 import { RenderPatronGroupLastUpdated, RenderPatronGroupNumberOfUsers } from '../lib/RenderPatronGroup';
 
 class PatronGroupsSettings extends React.Component {
+
   static propTypes = {
     stripes: PropTypes.shape({
       connect: PropTypes.func.isRequired,
     }).isRequired,
   };
 
+  static manifest = Object.freeze({
+    usersPerGroup: {
+      type: 'okapi',
+      records: 'users',
+      path: 'users?limit=0&facets=patronGroup'
+    },
+    users: {
+      type: 'okapi',
+      records: 'users',
+      path: 'users?query=(%{usersLastUpdating.query})',
+      accumulate: 'true'
+    },
+    groups: {
+      type: 'okapi',
+      records: 'usergroups',
+      path: 'groups',
+    },
+    usersLastUpdating: {}
+  });
+
   constructor(props) {
     super(props);
     this.connectedControlledVocab = props.stripes.connect(ControlledVocab);
     this.connectedPatronGroupLastUpdated = props.stripes.connect(RenderPatronGroupLastUpdated);
     this.connectedPatronGroupNumberOfUsers = props.stripes.connect(RenderPatronGroupNumberOfUsers);
+  }
+
+  componentWillReceiveProps(nextProps) {
+    if(this.propsReadyToFetchUsers(nextProps)) {
+      const ids = this.getLastUpdaterIds(nextProps.resources.groups.records);
+      const query = this.craftQueryForLastUpdaters(ids);
+      this.props.mutator.usersLastUpdating.update({ query });
+    }
+  }
+
+  propsReadyToFetchUsers(nextProps) {
+    return nextProps.resources.groups.hasLoaded && this.props.resources.usersLastUpdating.query === undefined;
+  }
+
+  getLastUpdaterIds(groups) {
+    const userIds = [];
+    for (let group of groups) {
+      if(group.metadata && !_.includes(userIds, group.metadata.updatedByUserId)) {
+        userIds.push(group.metadata.updatedByUserId);
+      }
+    }
+    return userIds;
+  }
+
+  craftQueryForLastUpdaters(ids) {
+    let query = "";
+    for(let id of ids) {
+      if(query.length > 0) {
+        query += " or ";
+      }
+      query += `id=${id}`;
+    }
+    return query;
   }
 
   render() {
@@ -28,13 +83,20 @@ class PatronGroupsSettings extends React.Component {
         visibleFields={['group', 'desc']}
         itemTemplate={{ group: 'string', id: 'string', desc: 'string' }}
         nameKey="group"
-        additionalFields={[ {
-          component: this.connectedPatronGroupLastUpdated,
-          gloss: "Last Updated",
-        }, {
-          component: this.connectedPatronGroupNumberOfUsers,
-          gloss: "# of Users",
-        }]}
+        additionalFields={
+          {
+            lastUpdated: {
+              component: this.connectedPatronGroupLastUpdated,
+              gloss: "Last Updated",
+              inheritedProps: this.props,
+            },
+            numberOfUsers: {
+              component: this.connectedPatronGroupNumberOfUsers,
+              gloss: "# of Users",
+              inheritedProps: this.props,
+            },
+          }
+        }
       />
     );
   }
