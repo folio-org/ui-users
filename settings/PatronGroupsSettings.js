94eb89c6aefcce7e972260e1bee84786e4cdef73
Jason Skomorowski <jason@indexdata.com>
2017-12-17 20:12:49 -0500

    Handle PatronGroup create/update/delete

--- settings/PatronGroupsSettings.js
+++ settings/PatronGroupsSettings.js
@@ -1,5 +1,7 @@
+/* eslint-disable react/prop-types */
 import React from 'react';
-import { graphql } from 'react-apollo';
+import _ from 'lodash';
+import { graphql, compose } from 'react-apollo';
 import gql from 'graphql-tag';
 import { Col } from 'react-flexbox-grid';
 
@@ -9,21 +11,53 @@ import EditableList from '@folio/stripes-components/lib/structures/EditableList'
 
 const UpdatedWrapper = props => <Col key={props.gloss} xs>{props.item.updated}</Col>;
 
-const PatronGroups = gql`
-  query {
-    groups {
-      group
-      desc
-      metadata {
-        updatedDate
-        updatedByUser {
-          personal {
-            firstName
-            lastName
-          }
+const PatronGroupFragment = gql`
+  fragment GroupFragment on Group {
+    id
+    group
+    desc
+    metadata {
+      updatedDate
+      updatedByUser {
+        personal {
+          firstName
+          lastName
         }
       }
     }
+  }
+`;
+
+const PatronGroups = gql`
+  query {
+    groups {
+      ...GroupFragment
+    }
+  }
+  ${PatronGroupFragment}
+`;
+
+const createGroup = gql`
+  mutation createGroup($record: GroupInput!) {
+    createGroup(record: $record) {
+      ...GroupFragment
+    }
+  }
+  ${PatronGroupFragment}
+`;
+
+const updateGroup = gql`
+  mutation updateGroup($id: ID!, $record: GroupInput!) {
+    updateGroup(id: $id, record: $record) {
+      ...GroupFragment
+    }
+  }
+  ${PatronGroupFragment}
+`;
+
+const deleteGroup = gql`
+  mutation deleteGroup($id: ID!) {
+    deleteGroup(id: $id)
   }
 `;
 
@@ -39,15 +73,27 @@ class PatronGroupsSettings extends React.Component {
     return `${date} by ${last + maybeComma + first}`;
   }
 
+  handleCreate = (record) => {
+    return this.props.createGroup({ variables: { record: { group: record.group, desc: record.desc } } });
+  }
+
+  handleUpdate = (record) => {
+    const changed = _.pick(record, ['group', 'desc']);
+    return this.props.updateGroup({ variables: { id: record.id, record: changed } });
+  }
+
+  handleDelete = (id) => {
+    return this.props.deleteGroup({ variables: { id } });
+  }
+
   render() {
     if (this.props.data.loading || !Array.isArray(this.props.data.groups)) return <div />;
-    console.log(this.props.data);
     const listContent = this.props.data.groups.map(group => ({
+      id: group.id,
       group: group.group,
       desc: group.desc,
       updated: this.parseUpdated(group),
     }));
-    console.log(listContent);
     return (
       <Paneset>
         <Pane defaultWidth="fill" fluidContentWidth paneTitle="Patron Groups">
@@ -56,10 +102,11 @@ class PatronGroupsSettings extends React.Component {
             createButtonLabel="+ Add new"
             visibleFields={['group', 'desc']}
             itemTemplate={{ group: 'string', id: 'string', desc: 'string' }}
+            actionSuppression={{ delete: () => true, edit: () => false }}
             additionalFields={{ updated: { gloss: 'Last updated', component: UpdatedWrapper } }}
-            onUpdate={x => console.log(x)}
-            onCreate={x => console.log(x)}
-            onDelete={x => console.log(x)}
+            onUpdate={this.handleUpdate}
+            onCreate={this.handleCreate}
+            onDelete={this.handleDelete}
             isEmptyMessage="There are no patron groups"
             label="Patron Groups"
             nameKey="group"
@@ -70,4 +117,9 @@ class PatronGroupsSettings extends React.Component {
   }
 }
 
-export default graphql(PatronGroups)(PatronGroupsSettings);
+export default compose(
+  graphql(PatronGroups),
+  graphql(createGroup, { name: 'createGroup' }),
+  graphql(updateGroup, { name: 'updateGroup' }),
+  graphql(deleteGroup, { name: 'deleteGroup' }),
+)(PatronGroupsSettings);
