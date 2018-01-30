e5efccc628982c2017e8bb114cf824b392f42818
Michal Kuklis <michal.kuklis@gmail.com>
2018-01-30 10:53:35 -0500

    Refactor settings to use ConfigManager

--- settings/ProfilePictureSettings.js
+++ settings/ProfilePictureSettings.js
@@ -1,84 +1,52 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import Callout from '@folio/stripes-components/lib/Callout';
-import ProfilePictureForm from './ProfilePictureForm';
+
+import { Row, Col } from '@folio/stripes-components/lib/LayoutGrid';
+import Checkbox from '@folio/stripes-components/lib/Checkbox';
+import { Field } from 'redux-form';
+import ConfigManager from '@folio/stripes-smart-components/lib/ConfigManager';
 
 class ProfilePictureSettings extends React.Component {
   static propTypes = {
-    resources: PropTypes.object.isRequired,
-    mutator: PropTypes.shape({
-      recordId: PropTypes.shape({
-        replace: PropTypes.func,
-      }),
-      settings: PropTypes.shape({
-        POST: PropTypes.func,
-        PUT: PropTypes.func,
-      }),
-    }).isRequired,
     label: PropTypes.string,
+    stripes: PropTypes.shape({
+      connect: PropTypes.func.isRequired,
+    }).isRequired,
   };
-
-  static manifest = Object.freeze({
-    recordId: {},
-    settings: {
-      type: 'okapi',
-      records: 'configs',
-      path: 'configurations/entries?query=(module=USERS and configName=profile_pictures)',
-      POST: {
-        path: 'configurations/entries',
-      },
-      PUT: {
-        path: 'configurations/entries/%{recordId}',
-      },
-    },
-  });
 
   constructor(props) {
     super(props);
-    this.onSave = this.onSave.bind(this);
+    this.configManager = props.stripes.connect(ConfigManager);
   }
 
-  onSave(data) {
-    const setting = this.props.resources.settings.records[0];
-    const value = data.profilePictures;
-    let promise;
-
-    if (setting) {
-      if (setting.metadata) delete setting.metadata;
-      this.props.mutator.recordId.replace(setting.id);
-      setting.value = value;
-      promise = this.props.mutator.settings.PUT(setting);
-    } else {
-      promise = this.props.mutator.settings.POST(
-        {
-          module: 'USERS',
-          configName: 'profile_pictures',
-          value,
-        },
-      );
-    }
-
-    promise.then(() => {
-      this.callout.sendCallout({ message: 'Setting was successfully updated.' });
-    });
+  // eslint-disable-next-line class-methods-use-this
+  getInitialValues(settings) {
+    const value = settings.length && settings[0].value === 'true';
+    return { profile_pictures: value };
   }
 
   render() {
-    const settings = (this.props.resources.settings || {}).records || [];
-    const profilePictures = settings.length && settings[0].value === 'true';
-
     return (
-      <div style={{ width: '100%' }}>
-        <ProfilePictureForm
-          onSubmit={this.onSave}
-          initialValues={{ profilePictures }}
-          label={this.props.label}
-        />
-        <Callout ref={ref => (this.callout = ref)} />
-      </div>
+      <this.configManager
+        getInitialValues={this.getInitialValues}
+        label={this.props.label}
+        moduleName="USERS"
+        configName="profile_pictures"
+      >
+        <Row>
+          <Col xs={12}>
+            <Field
+              component={Checkbox}
+              id="profile_pictures"
+              name="profile_pictures"
+              label="Display profile pictures"
+
+            />
+          </Col>
+        </Row>
+      </this.configManager>
     );
   }
-
 }
 
 export default ProfilePictureSettings;
