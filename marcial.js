fb93e59b361568a927b82cb331b98173b68d7476
Marcial Contreras <marcial@dgb.unam.mx>
2018-02-18 21:55:48 -0600

    adicion de marcial

---
+++ marcial.js
@@ -0,0 +1,34 @@
+import _ from 'lodash';
+import React from 'react';
+import { FormattedDate, FormattedTime } from 'react-intl';
+
+export function formatDate(dateStr) {
+  if (!dateStr) return dateStr;
+  return (<FormattedDate value={dateStr} />);
+}
+
+export function formatDateTime(dateStr) {
+  if (!dateStr) return dateStr;
+  return (<span><FormattedDate value={dateStr} /> <FormattedTime value={dateStr} /></span>);
+}
+
+export function getFullName(user) {
+  const lastName = _.get(user, ['personal', 'lastName'], '');
+  const firstName = _.get(user, ['personal', 'firstName'], '');
+  const middleName = _.get(user, ['personal', 'middleName'], '');
+
+  return `${lastName}${firstName ? ', ' : ' '}${firstName} ${middleName}`;
+}
+
+export function getRowURL(user) {
+  return `/users/view/${user.id}`;
+}
+
+export function isSubstringsInString(listSubStrings, testString) {
+  return new RegExp(listSubStrings.join('|')).test(testString);
+}
+
+export function eachPromise(arr, fn) {
+  if (!Array.isArray(arr)) return Promise.reject(new Error('Array not found'));
+  return arr.reduce((prev, cur) => (prev.then(() => fn(cur))), Promise.resolve());
+}
