8d65878c5c48fd8200426961a99bda282bf1f251
Charles Ledvina <charles@indexdata.com>
2018-02-27 17:23:39 -0600

    Fix lint errors and add new_proxy.js to test.js.

--- test/ui-testing/new_proxy.js
+++ test/ui-testing/new_proxy.js
@@ -8,7 +8,6 @@ module.exports.test = function foo(uiTestCtx) {
     this.timeout(Number(config.test_timeout));
 
     describe('Login > Find user two users > Add proxy to user 1 > Delete sponsor in user 2 > Logout\n', () => {
-
       let userIds = [];
       before((done) => {
         login(nightmare, config, done); // logs in with the default admin credentials
@@ -28,71 +27,73 @@ module.exports.test = function foo(uiTestCtx) {
         nightmare
           .click('#clickable-users-module')
           .wait('#list-users div[role="listitem"]:nth-child(9)')
-	  .evaluate(() => {
-	    let ubc = [];
-	    const list = document.querySelectorAll('#list-users div[role="listitem"]');
-	    list.forEach((node) => {
-	     let status = node.querySelector('a div:nth-child(1)').innerText;
-	     let barcode = node.querySelector('a div:nth-child(3)').innerText;
-	     let uuid = node.querySelector('a').href.replace(/.+?([^/]+)\?.*/,"$1");
-	     if (barcode && status.match(/Active/)) {
-	       ubc.push({ barcode: barcode, uuid: uuid });
-	     }
-	    });
-	    return ubc;
-	  })
-	  .then(result => {
-	    done();
-	    userIds = result;
-	  })
-	  .catch(done);
+          .evaluate(() => {
+            const ubc = [];
+            const list = document.querySelectorAll('#list-users div[role="listitem"]');
+            list.forEach((node) => {
+              const status = node.querySelector('a div:nth-child(1)').innerText;
+              const barcode = node.querySelector('a div:nth-child(3)').innerText;
+              const uuid = node.querySelector('a').href.replace(/.+?([^/]+)\?.*/, '$1');
+              if (barcode && status.match(/Active/)) {
+                ubc.push({
+                  barcode,
+                  uuid,
+                });
+              }
+            });
+            return ubc;
+          })
+          .then((result) => {
+            done();
+            userIds = result;
+          })
+          .catch(done);
       });
 
-      it(`should add a proxy for user 1`, (done) => {
+      it('should add a proxy for user 1', (done) => {
         nightmare
-	  .insert('#input-user-search', userIds[0].barcode)
+          .insert('#input-user-search', userIds[0].barcode)
           .wait('#clickable-edituser')
           .click('#clickable-edituser')
           .wait('#proxy button[title^="expand"]')
           .click('#proxy button[title^="expand"]')
-	  .wait('#proxy button[title^="Find"]')
-	  .click('#proxy button[title^="Find"]')
-	  .wait('div[aria-label="Select User"] #input-user-search')
-	  .insert('div[aria-label="Select User"] #input-user-search', userIds[1].barcode)
-	  .wait(222)
+          .wait('#proxy button[title^="Find"]')
+          .click('#proxy button[title^="Find"]')
+          .wait('div[aria-label="Select User"] #input-user-search')
+          .insert('div[aria-label="Select User"] #input-user-search', userIds[1].barcode)
+          .wait(222)
           .wait(`div[aria-label="Select User"] #list-users div[role="listitem"] > a > div[title="${userIds[1].barcode}"]`)
           .click(`div[aria-label="Select User"] #list-users div[role="listitem"] > a > div[title="${userIds[1].barcode}"]`)
-	  .wait('#clickable-updateuser')
-	  .click('#clickable-updateuser')
+          .wait('#clickable-updateuser')
+          .click('#clickable-updateuser')
           .then(() => { done(); })
           .catch(done);
-      }); 
+      });
 
-      it(`should delete a sponsor of user 2`, (done) => {
+      it('should delete a sponsor of user 2', (done) => {
         nightmare
-	  //.wait('#input-user-search')
-	  .wait(4444)
-	  .evaluate(() => {
-	    document.querySelector('#input-user-search').value = '';
-	  })
-	  .wait(222)
-	  .type('#input-user-search', userIds[1].barcode)
-	  .wait(`#list-users div[role="listitem"] > a > div[title="${userIds[1].barcode}"]`)
-	  .click(`#list-users div[role="listitem"] > a > div[title="${userIds[1].barcode}"]`)
-	  .wait(222)
+          .wait(4444)
+          .evaluate(() => {
+            document.querySelector('#input-user-search').value = '';
+          })
+          .wait(222)
+          .type('#input-user-search', userIds[1].barcode)
+          .wait(`#list-users div[role="listitem"] > a > div[title="${userIds[1].barcode}"]`)
+          .click(`#list-users div[role="listitem"] > a > div[title="${userIds[1].barcode}"]`)
+          .wait(222)
           .click('#clickable-edituser')
           .wait('#proxy button[title^="expand"]')
           .click('#proxy button[title^="expand"]')
-	  .wait(`#proxy a[href*="${userIds[0].uuid}"]`)
-	  .xclick(`id("proxy")//a[contains(@href, "${userIds[0].uuid}")]/../../../..//button`)
-	  .wait('#clickable-deleteproxy-confirmation-confirm')
-	  .click('#clickable-deleteproxy-confirmation-confirm')
-	  .wait('#clickable-updateuser')
-	  .click('#clickable-updateuser')
-	  .wait(1111)
+          .wait(`#proxy a[href*="${userIds[0].uuid}"]`)
+          .xclick(`id("proxy")//a[contains(@href, "${userIds[0].uuid}")]/../../../..//button`)
+          .wait('#clickable-deleteproxy-confirmation-confirm')
+          .click('#clickable-deleteproxy-confirmation-confirm')
+          .wait('#clickable-updateuser')
+          .click('#clickable-updateuser')
+          .wait(1111)
           .then(() => { done(); })
           .catch(done);
-      }); 
+      });
     });
   });
 };
