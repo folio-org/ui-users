09808bdfd52d6eb76f8c79318acca1456047e168
Rasmus Wølk <rasmuswoelk@gmail.com>
2018-03-08 17:56:07 +0100

    Added AppIcon in ViewUser PaneHeader.

--- ViewUser.js
+++ ViewUser.js
@@ -341,7 +341,7 @@ class ViewUser extends React.Component {
     const userFormData = this.getUserFormData(user, addresses, sponsors, proxies, permissions);
 
     return (
-      <Pane id="pane-userdetails" defaultWidth={this.props.paneWidth} paneTitle={getFullName(user)} lastMenu={detailMenu} dismissible onClose={this.props.onClose} appIcon="users">
+      <Pane id="pane-userdetails" defaultWidth={this.props.paneWidth} paneTitle={getFullName(user)} lastMenu={detailMenu} dismissible onClose={this.props.onClose} appIcon={{ app: 'users' }}>
         <Row end="xs"><Col xs><ExpandAllButton accordionStatus={this.state.sections} onToggle={this.handleExpandAll} /></Col></Row>
 
         <this.connectedUserInfo user={user} patronGroup={patronGroup} settings={settings} stripes={stripes} />
