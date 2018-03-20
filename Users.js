a752b47dec94dbc4d798b1d1c4f09e93f3805bb7
Roman Ruiz-Esparza <rjruizes@ncsu.edu>
2018-03-20 12:03:08 -0400

    UIU-150: apply filters from URL on init (#305)
    * refresh users with filterConfig when it loads
    
    * eslint

--- Users.js
+++ Users.js
@@ -52,6 +52,9 @@ class Users extends React.Component {
       creds: PropTypes.shape({
         POST: PropTypes.func.isRequired,
       }),
+      initializedFilterConfig: PropTypes.shape({
+        replace: PropTypes.func.isRequired,
+      }),
       perms: PropTypes.shape({
         POST: PropTypes.func.isRequired,
       }),
@@ -73,6 +76,7 @@ class Users extends React.Component {
   }
 
   static manifest = Object.freeze({
+    initializedFilterConfig: false,
     query: { initialValue: {} },
     resultCount: { initialValue: INITIAL_RESULT_COUNT },
     records: {
@@ -134,7 +138,11 @@ class Users extends React.Component {
     const pg = (this.props.resources.patronGroups || {}).records || [];
     if (pg && pg.length) {
       const pgFilterConfig = filterConfig.find(group => group.name === 'pg');
+      const oldValuesLength = pgFilterConfig.values.length;
       pgFilterConfig.values = pg.map(rec => ({ name: rec.group, cql: rec.id }));
+      if (oldValuesLength === 0) {
+        this.props.mutator.initializedFilterConfig.replace(true); // triggers refresh of users
+      }
     }
   }
 
@@ -162,9 +170,8 @@ class Users extends React.Component {
   }
 
   render() {
-    const props = this.props;
     const { onSelectRow, disableRecordCreation, onComponentWillUnmount, showSingleResult, browseOnly } = this.props;
-    const patronGroups = (props.resources.patronGroups || {}).records || [];
+    const patronGroups = (this.props.resources.patronGroups || {}).records || [];
 
     const resultsFormatter = {
       Status: user => (user.active ? 'Active' : 'Inactive'),
@@ -197,8 +204,8 @@ class Users extends React.Component {
       viewRecordPerms="users.item.get"
       newRecordPerms="users.item.post,login.item.post,perms.users.item.post"
       disableRecordCreation={disableRecordCreation}
-      parentResources={props.resources}
-      parentMutator={props.mutator}
+      parentResources={this.props.resources}
+      parentMutator={this.props.mutator}
       showSingleResult={showSingleResult}
       columnMapping={columnMapping}
       browseOnly={browseOnly}
