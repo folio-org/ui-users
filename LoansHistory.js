f34b77be44055c9f777f31a64d463e7fb4e8b4fb
Rasmus Wølk <rasmuswoelk@gmail.com>
2018-03-22 12:50:57 +0100

    Grouped OpenLoans and ClosedLoans in a 'Loans'-folder. Added 'ActionsBar' component that is used in both ClosedLoans and OpenLoans. The 'ActionsBar' might be a patteren we'll re-use but for now it's grouped within Loans.

--- LoansHistory.js
+++ LoansHistory.js
@@ -7,8 +7,7 @@ import Button from '@folio/stripes-components/lib/Button';
 import SegmentedControl from '@folio/stripes-components/lib/SegmentedControl';
 
 import { getFullName } from './util';
-import OpenLoans from './lib/OpenLoans';
-import ClosedLoans from './lib/ClosedLoans';
+import { OpenLoans, ClosedLoans } from './lib/Loans';
 import css from './LoansHistory.css';
 
 /**
