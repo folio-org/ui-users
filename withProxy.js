0294052c72ea29519fe8d77a24e54e2c87659157
Michal Kuklis <michal.kuklis@gmail.com>
2018-05-24 13:56:50 -0400

    Remove meta from proxy object

--- withProxy.js
+++ withProxy.js
@@ -164,10 +164,10 @@ const withProxy = WrappedComponent =>
       const editPromises = edited.map(rec => (proxiesFor.PUT(rec.proxy)));
       const removePromises = removed.map(rec => (proxiesFor.DELETE(rec.proxy)));
       const addPromises = added.map((rec) => {
-        const proxy = Object.assign({}, rec.proxy, { meta: { createdDate: moment().format() } });
+       //const proxy = Object.assign({}, rec.proxy, { meta: { createdDate: moment().format() } });
         const data = (resourceName === 'proxies') ?
-          { ...proxy, proxyUserId: rec.user.id, userId } :
-          { ...proxy, proxyUserId: userId, userId: rec.user.id };
+          { ...rec.proxy, proxyUserId: rec.user.id, userId } :
+          { ...rec.proxy, proxyUserId: userId, userId: rec.user.id };
 
         return proxiesFor.POST(data);
       });
