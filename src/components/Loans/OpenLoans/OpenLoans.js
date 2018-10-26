cf045acfedf1447213e6ddee8c684a2265c65820
ADITYA MATUKUMALLI <aditya.matukumalli@gmail.com>
2018-10-26 15:43:12 -0400

    Increase column widths to prevent overlap when selecting columns, refs STCOM-379 (#549)

--- src/components/Loans/OpenLoans/OpenLoans.js
+++ src/components/Loans/OpenLoans/OpenLoans.js
@@ -746,7 +746,7 @@ class OpenLoans extends React.Component {
           id="list-loanshistory"
           fullWidth
           formatter={this.getLoansFormatter()}
-          columnWidths={{ '  ': 28, 'title': 150, 'itemStatus': 100, 'dueDate': 140, 'requests': 90, 'barcode': 110, 'Fee/Fine': 50, 'Call number': 110, 'Contributors': 160, 'renewals': 70, 'loanPolicy': 100, 'location': 100, 'loanDate': 100, ' ': 50 }}
+          columnWidths={{ '  ': 28, 'title': 150, 'itemStatus': 100, 'dueDate': 140, 'requests': 90, 'barcode': 130, 'Fee/Fine': 70, 'Call number': 120, 'Contributors': 160, 'renewals': 70, 'loanPolicy': 100, 'location': 100, 'loanDate': 100, ' ': 50 }}
           visibleColumns={visibleColumns}
           columnMapping={columnMapping}
           columnOverflow={{ ' ': true }}
