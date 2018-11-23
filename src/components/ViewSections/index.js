def7ef9b17090412f6555834a9dffb42c5fc7667
DGB2018 <lmanillah@dgb.unam.mx>
2018-11-23 15:58:10 -0600

    Add import for Patron Blocks

--- src/components/ViewSections/index.js
+++ src/components/ViewSections/index.js
@@ -2,6 +2,7 @@ export { default as UserInfo } from './UserInfo';
 export { default as ExtendedInfo } from './ExtendedInfo';
 export { default as ContactInfo } from './ContactInfo';
 export { default as ProxyPermissions } from './ProxyPermissions';
+export { default as PatronBlock } from './PatronBlock';
 export { default as UserLoans } from './UserLoans';
 export { default as UserAccounts } from './UserAccounts';
 export { default as UserPermissions } from './UserPermissions';
