31cc8ffc147ba782df4231c9108d84612b13503a
DGB2018 <lmanillah@dgb.unam.mx>
2018-12-14 19:13:51 -0600

    Fix modal have two decimal places and notify. UIU-769

--- src/components/Accounts/Actions/PayModal.js
+++ src/components/Accounts/Actions/PayModal.js
@@ -43,6 +43,14 @@ const validate = (values, props) => {
   return errors;
 };
 
+const asyncValidate = (values, dispatch, props, blurredField) => {
+  if (blurredField === 'amount') {
+    const amount = parseFloat(values.amount || 0).toFixed(2);
+    dispatch(change('payment', 'amount', amount));
+  }
+  return new Promise(resolve => resolve());
+};
+
 class PayModal extends React.Component {
   static propTypes = {
     onClose: PropTypes.func,
@@ -56,7 +64,6 @@ class PayModal extends React.Component {
     invalid: PropTypes.bool,
     pristine: PropTypes.bool,
     reset: PropTypes.func,
-    dispatch: PropTypes.func,
     commentRequired: PropTypes.bool,
     intl: intlShape.isRequired,
   };
@@ -65,9 +72,11 @@ class PayModal extends React.Component {
     super(props);
     this.state = {
       amount: 0,
+      notify: true,
     };
 
     this.onChangeAmount = this.onChangeAmount.bind(this);
+    this.onToggleNotify = this.onToggleNotify.bind(this);
   }
 
   shouldComponentUpdate(nextProps, nextState) {
@@ -80,7 +89,7 @@ class PayModal extends React.Component {
       this.setState({
         amount: parseFloat(selected).toFixed(2),
       });
-      this.props.initialize({ amount: selected, notify: true });
+      this.props.initialize({ amount: parseFloat(selected).toFixed(2), notify: true });
     }
 
     return (this.props.accounts !== nextProps.accounts
@@ -94,6 +103,12 @@ class PayModal extends React.Component {
     this.setState({
       amount: e.target.value,
     });
+  }
+
+  onToggleNotify() {
+    this.setState(prevState => ({
+      notify: !prevState.notify,
+    }));
   }
 
   render() {
@@ -152,9 +167,6 @@ class PayModal extends React.Component {
                     name="amount"
                     component={TextField}
                     onChange={this.onChangeAmount}
-                    onBlur={(e, value, next) => {
-                      this.props.dispatch(change('payment', 'amount', parseFloat(next).toFixed(2)));
-                    }}
                     fullWidth
                     autoFocus
                     required
@@ -215,6 +227,8 @@ class PayModal extends React.Component {
               <Field
                 name="notify"
                 component={Checkbox}
+                checked={this.state.notify}
+                onChange={this.onToggleNotify}
                 inline
               />
               {' Notify patron'}
@@ -236,5 +250,7 @@ class PayModal extends React.Component {
 export default reduxForm({
   form: 'payment',
   fields: [],
+  asyncBlurFields: ['amount'],
+  asyncValidate,
   validate,
 })(injectIntl(PayModal));
