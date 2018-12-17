6cd59b56c2b2041c3b4dd46f2cadd651f42605bb
Anastasia Hahanova <anastasia_hahanova@epam.com>
2018-12-17 11:57:35 +0200

    Implement pane header dropdown on detail records

--- test/bigtest/network/factories/user-personal.js
+++ test/bigtest/network/factories/user-personal.js
@@ -1,8 +1,6 @@
 import { Factory, faker } from '@bigtest/mirage';
 
 export default Factory.extend({
-  lastName: () => faker.name.lastName(),
-  firstName: () => faker.name.firstName(),
   email: () => faker.internet.email(),
   phone: () => faker.phone.phoneNumber(),
   mobilePhone: () => faker.phone.phoneNumber(),
