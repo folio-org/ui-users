20f7891fa59ed4c13cde2d817ac1b6842e42e0e4
Michal Kuklis <michal.kuklis@gmail.com>
2019-02-07 10:19:28 -0500

    Fix failing users test

--- test/bigtest/interactors/users.js
+++ test/bigtest/interactors/users.js
@@ -1,12 +1,13 @@
 import {
   interactor,
   scoped,
-  collection
+  collection,
+  clickable
 } from '@bigtest/interactor';
 
 export default @interactor class UsersInteractor {
   static defaultScope = '[data-test-user-instances]';
-
+  clickInactiveUsersCheckbox = clickable('[name="active.Include inactive users"]');
   instances = collection('[role=row] a');
   instance = scoped('[data-test-instance-details]');
 }
