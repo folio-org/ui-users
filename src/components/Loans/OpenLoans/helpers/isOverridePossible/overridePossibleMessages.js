fdd382ce0d40e5e89b1eb475e6b81a3301d33ebd
oleksandr_antonenko <Oleksandr_Antonenko@epam.com>
2019-03-13 15:05:26 +0200

    UIU-881: Add override possible message.

--- src/components/Loans/OpenLoans/helpers/isOverridePossible/overridePossibleMessages.js
+++ src/components/Loans/OpenLoans/helpers/isOverridePossible/overridePossibleMessages.js
@@ -4,15 +4,19 @@ export default [
     showDueDatePicker: true,
   },
   {
+    message: 'item is not loanable',
+    showDueDatePicker: true,
+  },
+  {
+    message: 'renewal date falls outside of date ranges in fixed loan policy',
+    showDueDatePicker: true,
+  },
+  {
     message: 'loan at maximum renewal number',
     showDueDatePicker: false,
   },
   {
     message: 'renewal date falls outside of date ranges in rolling loan policy',
     showDueDatePicker: false,
-  },
-  {
-    message: 'renewal date falls outside of date ranges in fixed loan policy',
-    showDueDatePicker: true,
   },
 ];
