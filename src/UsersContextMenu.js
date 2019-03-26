79da87e0f37df3f000567178e55f43609239a248
John Coburn <jcoburn@ebsco.com>
2019-03-26 13:23:59 -0500

    apply translations to context menu

--- src/UsersContextMenu.js
+++ src/UsersContextMenu.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { AppContextMenu } from '@folio/stripes/core';
 import { NavList, NavListItem, MenuSection } from '@folio/stripes/components';
+import { FormattedMessage } from 'react-intl';
 import packageInfo from '../package';
 
 const UsersContextMenu = () => (
@@ -8,9 +9,15 @@ const UsersContextMenu = () => (
     {(handleToggle) => (
       <NavList>
         <MenuSection id="usersAppMenu" label="Users application menu">
-          <NavListItem to={packageInfo.stripes.home} onClick={handleToggle}>Home page</NavListItem>
-          <NavListItem to={`/settings${packageInfo.stripes.home}`} onClick={handleToggle}>Settings</NavListItem>
-          <NavListItem href="https://wiki.folio.org/display/FOLIOtips/Users" target="_blank" onClick={handleToggle}>Usage tips</NavListItem>
+          <NavListItem to={packageInfo.stripes.home} onClick={handleToggle}>
+            <FormattedMessage id="ui-users.contextMenu.homePage" />
+          </NavListItem>
+          <NavListItem to={`/settings${packageInfo.stripes.home}`} onClick={handleToggle}>
+            <FormattedMessage id="ui-users.contextMenu.settings" />
+          </NavListItem>
+          <NavListItem href="https://wiki.folio.org/display/FOLIOtips/Users" target="_blank" onClick={handleToggle}>
+            <FormattedMessage id="ui-users.contextMenu.usageTips" />
+          </NavListItem>
         </MenuSection>
       </NavList>
     )}
