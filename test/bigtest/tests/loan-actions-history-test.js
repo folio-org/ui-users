c000731fdb8937e3bb9047ac1ad84cecf9a763d6
oleksandr_antonenko <Oleksandr_Antonenko@epam.com>
2019-04-01 13:23:16 +0300

    UIU-930: Add tests for loan-actions-history and open-loans.

---
+++ test/bigtest/tests/loan-actions-history-test.js
@@ -0,0 +1,42 @@
+import { beforeEach, describe, it } from '@bigtest/mocha';
+import { expect } from 'chai';
+
+import setupApplication from '../helpers/setup-application';
+import LoanActionsHistory from '../interactors/loan-actions-history';
+
+import translations from '../../../translations/ui-users/en.json';
+
+describe.only('loans actions history', () => {
+  setupApplication({ permissions: {
+    'manualblocks.collection.get': true,
+    'circulation.loans.collection.get': true,
+  } });
+
+  const requestsAmount = 2;
+
+  beforeEach(async function () {
+    const user = this.server.create('user');
+    const loan = this.server.create('loan', { status: { name: 'Open' }, loanPolicyId: 'test' });
+
+    this.server.createList('request', requestsAmount, { itemId: loan.itemId });
+    this.visit(`/users/view/${user.id}?layer=loan&loan=${loan.id}&query=%20%20&sort=name`);
+  });
+
+  it('should be presented', () => {
+    expect(LoanActionsHistory.isPresent).to.be.true;
+  });
+
+  describe('requests', () => {
+    it('should be presented', () => {
+      expect(LoanActionsHistory.requests.isPresent).to.be.true;
+    });
+
+    it('should have proper value', () => {
+      expect(LoanActionsHistory.requests.value.text).to.equal(requestsAmount.toString());
+    });
+
+    it('should have proper label', () => {
+      expect(LoanActionsHistory.requests.label.text).to.equal(translations['loans.details.requestQueue']);
+    });
+  });
+});
---
+++ test/bigtest/tests/open-loans-test.js
@@ -0,0 +1,44 @@
+import {
+  beforeEach,
+  describe,
+  it,
+} from '@bigtest/mocha';
+import { expect } from 'chai';
+
+import setupApplication from '../helpers/setup-application';
+import OpenLoansInteractor from '../interactors/open-loans';
+
+describe('Open Loans', () => {
+  setupApplication({ permissions: {
+    'manualblocks.collection.get': true,
+    'circulation.loans.collection.get': true,
+  } });
+
+  const requestsAmount = 2;
+
+  beforeEach(async function () {
+    const user = this.server.create('user');
+    const loan = this.server.create('loan', { status: { name: 'Open' } });
+
+    this.server.createList('request', requestsAmount, { itemId: loan.itemId });
+    this.visit(`/users/view/${user.id}?layer=open-loans&query=%20&sort=requests`);
+  });
+
+  it('should be presented', () => {
+    expect(OpenLoansInteractor.isPresent).to.be.true;
+  });
+
+  describe('loan list', () => {
+    it('should be presented', () => {
+      expect(OpenLoansInteractor.list.isPresent).to.be.true;
+    });
+
+    describe('loan item', () => {
+      describe('requests', () => {
+        it('loan should have requests', () => {
+          expect(OpenLoansInteractor.requests(0).text).to.equal(requestsAmount.toString());
+        });
+      });
+    });
+  });
+});
