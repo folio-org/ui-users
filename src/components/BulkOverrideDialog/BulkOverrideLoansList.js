folio-org/ui-users

commit 21b0d5d36f959014de9df24f620615b29eaa262a

UIU-691: Apply internationalization to all hardcoded strings for Settings (#600)
* Apply internationalization to all hardcoded strings for Settings

src/withRenew.js
@@ -57,7 +57,6 @@ const withRenew = WrappedComponent => class WithRenewComponent extends React.Com
           } else {
             resp.text().then((error) => {
               reject(error);
-              alert(error); // eslint-disable-line no-alert
             });
           }
         });
@@ -92,10 +91,20 @@ const withRenew = WrappedComponent => class WithRenewComponent extends React.Com
 
       const policyName = this.getPolicyName(errors);
       let message = errors.reduce((msg, err) => ((msg) ? `${msg}, ${err.message}` : err.message), '');
-      message = `Loan cannot be renewed because: ${message}.`;
+      message = (
+        <FormattedMessage
+          id="ui-users.loanNotRenewedReason"
+          values={{ message }}
+        />
+      );
 
       if (policyName) {
-        message = `${message} Please review ${policyName} before retrying renewal.`;
+        message = (
+          <FormattedMessage
+            id="ui-users.reviewBeforeRenewal"
+            values={{ message, policyName }}
+          />
+        );
       }
 
       return message;
@@ -107,7 +116,10 @@ const withRenew = WrappedComponent => class WithRenewComponent extends React.Com
 
       return (
         <div>
-          <WrappedComponent renew={this.renew} {...this.props} />
+          <WrappedComponent
+            renew={this.renew}
+            {...this.props}
+          />
           {errors &&
             <ErrorModal
               id="renewal-failure-modal"

src/AccountActionsHistory.js
@@ -3,11 +3,10 @@ import React from 'react';
 import {
   FormattedMessage,
   FormattedTime,
-  intlShape,
-  injectIntl,
 } from 'react-intl';
 import Link from 'react-router-dom/Link';
 import PropTypes from 'prop-types';
+
 import {
   Paneset,
   Pane,
@@ -17,8 +16,32 @@ import {
   KeyValue,
   MultiColumnList,
 } from '@folio/stripes/components';
+
 import { Actions } from './components/Accounts/Actions';
 import { getFullName } from './util';
+
+import css from './AccountsHistory.css';
+
+const columnWidths = {
+  action: 250,
+  amount: 100,
+  balance: 100,
+  transactioninfo: 200,
+  created: 100,
+  source: 200,
+  comments: 700
+};
+
+const columns = [
+  'date',
+  'action',
+  'amount',
+  'balance',
+  'transactioninfo',
+  'created',
+  'source',
+  'comments',
+];
 
 class AccountActionsHistory extends React.Component {
   static manifest = Object.freeze({
@@ -62,13 +85,12 @@ class AccountActionsHistory extends React.Component {
     onCancel: PropTypes.func.isRequired,
     onClickViewLoanActionsHistory: PropTypes.func.isRequired,
     handleAddRecords: PropTypes.func.isRequired,
-    intl: intlShape.isRequired,
   };
 
   constructor(props) {
     super(props);
     const {
-      stripes : { connect },
+      stripes: { connect },
     } = props;
     this.onSort = this.onSort.bind(this);
     this.onChangeActions = this.onChangeActions.bind(this);
@@ -97,17 +119,8 @@ class AccountActionsHistory extends React.Component {
         comment: false,
         regular: false,
       },
-      sortOrder: [
-        'date',
-        'action',
-        'amount',
-        'balance',
-        'transactioninfo',
-        'created',
-        'source',
-        'comments',
-      ],
-      sortDirection: ['desc', 'desc']
+      sortOrder: [...columns],
+      sortDirection: ['desc', 'desc'],
     };
   }
 
@@ -189,30 +202,32 @@ class AccountActionsHistory extends React.Component {
     } = this.state;
 
     const {
+      handleAddRecords,
       onCancel,
-      intl,
+      onClickViewLoanActionsHistory,
+      patronGroup: patron,
+      resources,
+      stripes,
+      user,
     } = this.props;
-    const account = _.get(this.props.resources, ['accountHistory', 'records', 0]) || this.props.account;
 
-    const user = this.props.user;
-    const patron = this.props.patronGroup;
+    const account = _.get(resources, ['accountHistory', 'records', 0]) || this.props.account;
 
     const columnMapping = {
-      date: intl.formatMessage({ id: 'ui-users.details.columns.date' }),
-      action: intl.formatMessage({ id: 'ui-users.details.columns.action' }),
-      amount: intl.formatMessage({ id: 'ui-users.details.columns.amount' }),
-      balance: intl.formatMessage({ id: 'ui-users.details.columns.balance' }),
-      transactioninfo: intl.formatMessage({ id: 'ui-users.details.columns.transactioninfo' }),
-      created: intl.formatMessage({ id: 'ui-users.details.columns.created' }),
-      source: intl.formatMessage({ id: 'ui-users.details.columns.source' }),
+      date: <FormattedMessage id="ui-users.details.columns.date" />,
+      action: <FormattedMessage id="ui-users.details.columns.action" />,
+      amount: <FormattedMessage id="ui-users.details.columns.amount" />,
+      balance: <FormattedMessage id="ui-users.details.columns.balance" />,
+      transactioninfo: <FormattedMessage id="ui-users.details.columns.transactioninfo" />,
+      created: <FormattedMessage id="ui-users.details.columns.created" />,
+      source: <FormattedMessage id="ui-users.details.columns.source" />,
       comments: (
-        <span>
+        <span className={css.commentsWrapper}>
           <FormattedMessage id="ui-users.details.columns.comments" />
           <Button
-            style={{ float: 'right', marginLeft: '50px' }}
+            buttonClass={css.addCommentBtn}
             onClick={this.comment}
           >
-            +
             <FormattedMessage id="ui-users.accounts.button.new" />
           </Button>
         </span>
@@ -237,6 +252,7 @@ class AccountActionsHistory extends React.Component {
     const remaining = (account.remaining) ? parseFloat(account.remaining).toFixed(2) : '0.00';
     const loanId = account.loanId || '';
     const disabled = (_.get(account, ['status', 'name'], '') === 'Closed');
+    const isAccountId = actions[0] && actions[0].accountId === account.id;
 
     return (
       <Paneset isRoot>
@@ -253,11 +269,39 @@ class AccountActionsHistory extends React.Component {
         >
           <Row>
             <Col xs={12}>
-              <Button disabled={disabled} buttonStyle="primary" onClick={this.pay}><FormattedMessage id="ui-users.accounts.history.button.pay" /></Button>
-              <Button disabled={disabled} buttonStyle="primary" onClick={this.waive}><FormattedMessage id="ui-users.accounts.history.button.waive" /></Button>
-              <Button disabled buttonStyle="primary"><FormattedMessage id="ui-users.accounts.history.button.refund" /></Button>
-              <Button disabled buttonStyle="primary"><FormattedMessage id="ui-users.accounts.history.button.transfer" /></Button>
-              <Button disabled={disabled} buttonStyle="primary" onClick={this.error}><FormattedMessage id="ui-users.accounts.button.error" /></Button>
+              <Button
+                disabled={disabled}
+                buttonStyle="primary"
+                onClick={this.pay}
+              >
+                <FormattedMessage id="ui-users.accounts.history.button.pay" />
+              </Button>
+              <Button
+                disabled={disabled}
+                buttonStyle="primary"
+                onClick={this.waive}
+              >
+                <FormattedMessage id="ui-users.accounts.history.button.waive" />
+              </Button>
+              <Button
+                disabled
+                buttonStyle="primary"
+              >
+                <FormattedMessage id="ui-users.accounts.history.button.refund" />
+              </Button>
+              <Button
+                disabled
+                buttonStyle="primary"
+              >
+                <FormattedMessage id="ui-users.accounts.history.button.transfer" />
+              </Button>
+              <Button
+                disabled={disabled}
+                buttonStyle="primary"
+                onClick={this.error}
+              >
+                <FormattedMessage id="ui-users.accounts.button.error" />
+              </Button>
             </Col>
           </Row>
 
@@ -312,9 +356,16 @@ class AccountActionsHistory extends React.Component {
                 <KeyValue
                   label={<FormattedMessage id="ui-users.details.label.loanDetails" />}
                   value={(
-                    <button style={{ color: '#2b75bb' }} type="button" onClick={(e) => { this.props.onClickViewLoanActionsHistory(e, { id: loanId }); }}>
+                    <button
+                      buttonClass={css.btnView}
+                      type="button"
+                      onClick={(e) => {
+                        onClickViewLoanActionsHistory(e, { id: loanId });
+                      }}
+                    >
                       <FormattedMessage id="ui-users.details.field.loan" />
-                    </button>)}
+                    </button>
+                  )}
                 />
                 :
                 <KeyValue
@@ -340,7 +391,13 @@ class AccountActionsHistory extends React.Component {
             <Col xs={1.5}>
               <KeyValue
                 label={<FormattedMessage id="ui-users.details.field.barcode" />}
-                value={<Link to={`/inventory/view/${_.get(account, ['itemId'], '')}?query=${_.get(account, ['itemId'], '')}`}>{_.get(account, ['barcode'], '-')}</Link>}
+                value={
+                  <Link
+                    to={`/inventory/view/${_.get(account, ['itemId'], '')}?query=${_.get(account, ['itemId'], '')}`}
+                  >
+                    {_.get(account, ['barcode'], '-')}
+                  </Link>
+                }
               />
             </Col>
             <Col xs={1.5}>
@@ -358,13 +415,27 @@ class AccountActionsHistory extends React.Component {
             <Col xs={1.5}>
               <KeyValue
                 label={<FormattedMessage id="ui-users.details.field.duedate" />}
-                value={<FormattedTime value={account.dueDate} day="numeric" month="numeric" year="numeric" /> || '-'}
+                value={
+                  <FormattedTime
+                    value={account.dueDate}
+                    day="numeric"
+                    month="numeric"
+                    year="numeric"
+                  /> || '-'
+                }
               />
             </Col>
             <Col xs={1.5}>
               <KeyValue
                 label={<FormattedMessage id="ui-users.details.field.returnedate" />}
-                value={<FormattedTime value={account.returnedDate} day="numeric" month="numeric" year="numeric" /> || '-'}
+                value={
+                  <FormattedTime
+                    value={account.returnedDate}
+                    day="numeric"
+                    month="numeric"
+                    year="numeric"
+                  /> || '-'
+                }
               />
             </Col>
           </Row>
@@ -373,33 +444,24 @@ class AccountActionsHistory extends React.Component {
             id="list-accountactions"
             formatter={accountActionsFormatter}
             columnMapping={columnMapping}
-            visibleColumns={[
-              'date',
-              'action',
-              'amount',
-              'balance',
-              'transactioninfo',
-              'created',
-              'source',
-              'comments',
-            ]}
-            contentData={(account.id === (actions[0] || {}).accountId) ? actionsSort : []}
+            visibleColumns={columns}
+            contentData={isAccountId ? actionsSort : []}
             fullWidth
-            onHeaderClick={this.onSort}
             sortOrder={sortOrder[0]}
             sortDirection={`${sortDirection[0]}ending`}
-            columnWidths={{ 'action': 250, 'amount': 100, 'balance': 100, 'transactioninfo': 200, 'created': 100, 'source': 200, 'comments': 700 }}
+            columnWidths={columnWidths}
+            onHeaderClick={this.onSort}
           />
           <this.connectedActions
             actions={this.state.actions}
             onChangeActions={this.onChangeActions}
-            user={this.props.user}
-            stripes={this.props.stripes}
+            user={user}
+            stripes={stripes}
             balance={account.remaining || 0}
             accounts={[account]}
             handleEdit={() => {
               this.getAccountActions();
-              this.props.handleAddRecords();
+              handleAddRecords();
             }}
           />
 
@@ -409,4 +471,4 @@ class AccountActionsHistory extends React.Component {
   }
 }
 
-export default injectIntl(AccountActionsHistory);
+export default AccountActionsHistory;

src/AccountsHistory.css
@@ -1,0 +1,16 @@
+@import "@folio/stripes-components/lib/variables";
+
+.commentsWrapper { 
+  display: flex; 
+  align-items: center;
+}
+
+.btnView {
+  color: var(--primary);
+}
+
+.addCommentBtn {
+  float: right;
+  margin-left: 50px;
+  margin-bottom: 0;
+}

src/AccountsHistory.js
@@ -7,6 +7,7 @@ import {
 } from 'react-intl';
 import PropTypes from 'prop-types';
 import queryString from 'query-string';
+
 import {
   Paneset,
   Pane,
@@ -28,7 +29,6 @@ import { getFullName } from './util';
 import { Actions } from './components/Accounts/Actions';
 import { count, handleFilterChange, handleFilterClear } from './components/Accounts/accountFunctions';
 
-
 import {
   Menu,
   Filters,
@@ -39,22 +39,22 @@ import {
 
 const filterConfig = [
   {
-    label: 'Fee/Fine Owner',
+    label: <FormattedMessage id="ui-users.feefines.ownerLabel" />,
     name: 'owner',
     cql: 'feeFineOwner',
     values: [],
   }, {
-    label: 'Payment Status',
+    label: <FormattedMessage id="ui-users.accounts.history.columns.status" />,
     name: 'status',
     cql: 'paymentStatus.name',
     values: [],
   }, {
-    label: 'Fee/Fine Type',
+    label: <FormattedMessage id="ui-users.details.field.feetype" />,
     name: 'type',
     cql: 'feeFineType',
     values: [],
   }, {
-    label: 'Item Type',
+    label: <FormattedMessage id="ui-users.details.field.type" />,
     name: 'material',
     cql: 'materialType',
     values: [],
@@ -74,6 +74,38 @@ const queryFunction = (findAll, queryTemplate, sortMap, fConfig, failOnCondition
     return cql;
   };
 };
+
+const controllableColumns = [
+  'created',
+  'updated',
+  'type',
+  'amount',
+  'remaining',
+  'status',
+  'owner',
+  'title',
+  'barcode',
+  'number',
+  'due',
+  'returned',
+];
+
+const possibleColumns = [
+  '  ',
+  'created',
+  'updated',
+  'type',
+  'amount',
+  'remaining',
+  'status',
+  'owner',
+  'title',
+  'barcode',
+  'number',
+  'due',
+  'returned',
+  ' ',
+];
 
 class AccountsHistory extends React.Component {
   static manifest = Object.freeze({
@@ -148,22 +180,7 @@ class AccountsHistory extends React.Component {
   constructor(props) {
     super(props);
 
-    this.controllableColumns = [
-      'created',
-      'updated',
-      'type',
-      'amount',
-      'remaining',
-      'status',
-      'owner',
-      'title',
-      'barcode',
-      'number',
-      'due',
-      'returned',
-    ];
-
-    const visibleColumns = this.controllableColumns.map(columnName => ({
+    const visibleColumns = controllableColumns.map(columnName => ({
       title: columnName,
       status: true,
     }));
@@ -204,22 +221,6 @@ class AccountsHistory extends React.Component {
     this.handleFilterChange = handleFilterChange.bind(this);
     this.handleFilterClear = handleFilterClear.bind(this);
     this.filterState = filterState.bind(this);
-    this.possibleColumns = [
-      '  ',
-      'created',
-      'updated',
-      'type',
-      'amount',
-      'remaining',
-      'status',
-      'owner',
-      'title',
-      'barcode',
-      'number',
-      'due',
-      'returned',
-      ' '
-    ];
     this.getVisibleColumns = this.getVisibleColumns.bind(this);
     this.renderCheckboxList = this.renderCheckboxList.bind(this);
     this.toggleColumn = this.toggleColumn.bind(this);
@@ -377,7 +378,7 @@ class AccountsHistory extends React.Component {
       return map;
     }, {});
 
-    const columnsToDisplay = this.possibleColumns.filter((e) => visibleColumnsMap[e] === undefined || visibleColumnsMap[e] === true);
+    const columnsToDisplay = possibleColumns.filter((e) => visibleColumnsMap[e] === undefined || visibleColumnsMap[e] === true);
     return columnsToDisplay;
   }
 
@@ -401,12 +402,15 @@ class AccountsHistory extends React.Component {
 
   render() {
     const {
+      onCancel,
+      location,
       user,
       patronGroup,
       resources,
       intl,
     } = this.props;
-    const query = this.props.location.search ? queryString.parse(this.props.location.search) : {};
+
+    const query = location.search ? queryString.parse(location.search) : {};
 
     let accounts = _.get(resources, ['feefineshistory', 'records'], []);
     if (query.loan) {
@@ -422,10 +426,22 @@ class AccountsHistory extends React.Component {
 
     const closeMenu = (
       <PaneMenu>
-        <button onClick={this.props.onCancel} type="button">
+        <button
+          onClick={onCancel}
+          type="button"
+        >
           <Row>
-            <Col><Icon icon="chevron-double-left" size="large" /></Col>
-            <Col><span style={{ fontSize: 'x-large' }}>Back</span></Col>
+            <Col>
+              <Icon
+                icon="chevron-double-left"
+                size="large"
+              />
+            </Col>
+            <Col>
+              <span style={{ fontSize: 'x-large' }}>
+                <FormattedMessage id="ui-users.accounts.cancellation.field.back" />
+              </span>
+            </Col>
           </Row>
         </button>
       </PaneMenu>
@@ -460,7 +476,12 @@ class AccountsHistory extends React.Component {
           group
           pullRight
         >
-          <Button data-role="toggle" bottomMargin2><FormattedMessage id="ui-users.accounts.history.button.select" /></Button>
+          <Button
+            data-role="toggle"
+            bottomMargin2
+          >
+            <FormattedMessage id="ui-users.accounts.history.button.select" />
+          </Button>
           <DropdownMenu data-role="menu">
             <ul>
               {this.renderCheckboxList(columnMapping)}
@@ -498,25 +519,34 @@ class AccountsHistory extends React.Component {
       balance += a.remaining;
     });
 
+    const outstandingBalance = (user.id === (accounts[0] || {}).userId)
+      ? parseFloat(balance || 0).toFixed(2)
+      : '0.00';
+
     const header1 = (
       <Row style={{ width: '80%' }}>
         <Col xs={7}>
-          {' '}
           {closeMenu}
-          {' '}
         </Col>
         <Col xs={4}>
           <Row>
             <Col>
-              <b>{`Fees/Fines - ${getFullName(user)} (${_.upperFirst(patronGroup.group)})`}</b>
+              <b>
+                <FormattedMessage
+                  id="ui-users.accounts.header"
+                  values={{
+                    userName: getFullName(user),
+                    patronGroup: _.upperFirst(patronGroup.group),
+                  }}
+                />
+              </b>
             </Col>
           </Row>
           <Row>
             <Col>
               <div style={{ margin: '5px 5px 5px 40px' }}>
-              Outstanding Balance:
-                {' '}
-                { (user.id === (accounts[0] || {}).userId) ? parseFloat(balance || 0).toFixed(2) : '0.00'}
+                <FormattedMessage id="ui-users.accounts.outstandingBalance" />
+                {outstandingBalance}
               </div>
             </Col>
           </Row>
@@ -530,7 +560,7 @@ class AccountsHistory extends React.Component {
       <Paneset>
         <Pane
           defaultWidth="100%"
-          header={header1}
+          subheader={header1}
         >
           <Paneset>
             <Filters
@@ -545,7 +575,11 @@ class AccountsHistory extends React.Component {
               filters={filters}
               onChangeFilter={(e) => { this.handleFilterChange(e, 'f'); }}
             />
-            <Pane defaultWidth="fill" heigth="80%" header={header}>
+            <Pane
+              defaultWidth="fill"
+              heigth="80%"
+              subheader={header}
+            >
               <Menu
                 user={user}
                 showFilters={this.state.showFilters}
@@ -559,7 +593,6 @@ class AccountsHistory extends React.Component {
                 handleOptionsChange={this.handleOptionsChange}
                 onClickViewChargeFeeFine={this.props.onClickViewChargeFeeFine}
               />
-
               <Row>
                 {(query.layer === 'open-accounts') ?
                   (<this.connectedOpenAccounts

src/Users.js
@@ -23,7 +23,7 @@ const RESULT_COUNT_INCREMENT = 30;
 
 const filterConfig = [
   {
-    label: 'Status',
+    label: <FormattedMessage id="ui-users.status" />,
     name: 'active',
     cql: 'active',
     values: [
@@ -32,7 +32,7 @@ const filterConfig = [
     ],
   },
   {
-    label: 'Patron group',
+    label: <FormattedMessage id="ui-users.information.patronGroup" />,
     name: 'pg',
     cql: 'patronGroup',
     values: [], // will be filled in by componentWillUpdate
@@ -156,7 +156,7 @@ class Users extends React.Component {
 
   componentDidUpdate() {
     const pg = (this.props.resources.patronGroups || {}).records || [];
-    if (pg && pg.length) {
+    if (pg.length) {
       const pgFilterConfig = filterConfig.find(group => group.name === 'pg');
       const oldValuesLength = pgFilterConfig.values.length;
       pgFilterConfig.values = pg.map(rec => ({ name: rec.group, cql: rec.id }));
@@ -223,6 +223,7 @@ class Users extends React.Component {
       browseOnly,
       intl,
     } = this.props;
+
     const patronGroups = (this.props.resources.patronGroups || {}).records || [];
 
     const resultsFormatter = {

commit 8a82d4b2b6dcb0edfd3703320547c47e0032c3c6

always show explantion for renewal failure (#795)
The bug report here was that when a renewal was denied to the item
having an open recall-request against it, the explanation was missing
from the alert box.

This occurred because the error handler in `withRenew` inadvertently
returned a `<FormattedMessage>` with nested props when the error
contained a policy-name, which it does when the loan-policy is the
reason for the denial. When the error is the result of an open recall,
no policy-name is present, hence no nested props.

This fix cleans up the error handling in `withRenew` so the return value
is consistently structured for an error of any type, and it updates the
explanation display accordingly.

This bug was introduced in #600 during internationalization when
the code changed from something like

    message = `could not renew because ${message}`;
    if (policyName) {
            message = `${message}; see ${policyName}`;
    }

to

    message = <FormattedMessage key="foo" values={{message}} />;
    if (policyName) {
        message = <FormattedMessage key="bar" values={{message, policyName}} />;
    }

Reusing the message variable was fine in first case when it always resolves
to a string. After refactoring, it is passed as a string to the first
`<FormattedMessage>` object but then is passed as a `<FormattedMessage>`
to the second `<FormattedMessage>` object, causing the nested props.

Fixes UIU-971

src/components/BulkOverrideDialog/BulkOverrideLoansList.js
@@ -66,7 +66,7 @@ class BulkOverrideLoansList extends Component {
   );
 
   getShortErrorMessage = (errorMessage) => {
-    return get(errorMessage, 'props.values.message.props.values.message', '');
+    return get(errorMessage, 'props.values.message', '');
   };
 
   render() {

src/components/BulkRenewalDialog/BulkRenewedLoansList.js
@@ -72,7 +72,7 @@ const BulkRenewedLoansList = (props) => {
       formatter={{
         renewalStatus: loan => {
           if (failedRenewals.filter(loanObject => loanObject.id === loan.id).length > 0) {
-            const errorMessage = get(errorMessages[loan.id], 'props.values.message.props.values.message', '');
+            const errorMessage = get(errorMessages[loan.id], 'props.values.message', '');
 
             return (errorMessages) ? (
               <div>

src/withRenew.js
@@ -155,8 +155,7 @@ const withRenew = WrappedComponent => class WithRenewComponent extends React.Com
         .then((renewedLoan) => renewSuccess.push(renewedLoan))
         .catch((error) => {
           renewFailure.push(loan);
-          const stringErrorMessage = get(error, 'props.values.message.props.values.message', '');
-
+          const stringErrorMessage = get(error, 'props.values.message', '');
           errorMsg[loan.id] = {
             ...error,
             ...isOverridePossible(stringErrorMessage),
@@ -216,24 +215,19 @@ const withRenew = WrappedComponent => class WithRenewComponent extends React.Com
     if (!errors || !errors.length) return '';
 
     const policyName = this.getPolicyName(errors);
-    let message = errors.reduce((msg, err) => ((msg) ? `${msg}, ${err.message}` : err.message), '');
-    message = (
+    const message = errors.reduce((msg, err) => ((msg) ? `${msg}, ${err.message}` : err.message), '');
+
+    return policyName ? (
+      <FormattedMessage
+        id="ui-users.errors.reviewBeforeRenewal"
+        values={{ message, policyName }}
+      />
+    ) : (
       <FormattedMessage
         id="ui-users.errors.loanNotRenewedReason"
         values={{ message }}
       />
     );
-
-    if (policyName) {
-      message = (
-        <FormattedMessage
-          id="ui-users.errors.reviewBeforeRenewal"
-          values={{ message, policyName }}
-        />
-      );
-    }
-
-    return message;
   };
 
   hideBulkRenewalDialog = () => this.setState({ bulkRenewalDialogOpen: false });