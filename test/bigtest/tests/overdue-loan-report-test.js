6c726606b2b2bcb471f243042ff8c87d3af01d4a
Michal Kuklis <michal.kuklis@gmail.com>
2019-05-01 10:49:52 -0400

    cleanup

--- test/bigtest/tests/overdue-loan-report-test.js
+++ test/bigtest/tests/overdue-loan-report-test.js
@@ -1,6 +1,5 @@
-import { beforeEach, afterEach, describe, it } from '@bigtest/mocha';
+import { beforeEach, describe, it } from '@bigtest/mocha';
 import { expect } from 'chai';
-
 
 import setupApplication from '../helpers/setup-application';
 import UsersInteractor from '../interactors/users';
@@ -11,29 +10,16 @@ describe('OverdueLoanReport', () => {
   const users = new UsersInteractor();
 
   describe('Show export to CSV', function () {
-    let Blob;
-    let blobContent;
-
     beforeEach(async function () {
       this.server.createList('loan', 5);
       this.visit('/users?sort=Name');
-
-      Blob = global.Blob;
-      global.Blob = function (content) {
-        blobContent = content;
-      };
 
       await users.headerDropdown.click();
       await users.headerDropdownMenu.clickExportToCSV();
     });
 
-    afterEach(function () {
-      global.Blob = Blob;
-    });
-
     it('exports data to csv and hides dropdown menu', () => {
       expect(users.headerDropdownMenu.exportBtnIsVisible).to.be.false;
-      expect(blobContent).not.to.be.undefined;
     });
   });
 });
