fb77192164c4b214fa604c171903c8ffeec83c0d
DGB2018 <lmanillah@dgb.unam.mx>
2019-05-02 14:29:51 -0500

    Modify settings-feefine

--- test/bigtest/network/scenarios/settings-feefine.js
+++ test/bigtest/network/scenarios/settings-feefine.js
@@ -3,27 +3,18 @@ import { Response } from '@bigtest/mirage';
 
 export default (server) => {
   server.create('user', { id: '1ad737b0-d847-11e6-bf26-cec0c932ce02' });
-  const owner = server.create('owners', { owner: 'Main Admin0', desc: 'Owner FyF' });
-  server.createList('payments', 7, { ownerId: owner.id });
+  server.create('owners', { owner: 'Main Admin0', desc: 'Owner FyF' });
 
-  const ownerShared = server.create('owners', { owner: 'Shared', desc: 'Owner Shared' });
-  server.createList('payments', 5, { ownerId: ownerShared.id });
+  server.create('owners', { owner: 'Shared', desc: 'Owner Shared' });
 
-  const owner1 = server.create('owners', { owner: 'Main Admin1', desc: 'Owner DGB' });
-  server.createList('payments', 4, { ownerId: owner1.id });
+  server.create('owners', { owner: 'Main Admin1', desc: 'Owner DGB' });
 
-  const otherOwner = server.create('owners', { owner: 'Main Admin2', desc: 'Owner CCH' });
-  server.createList('payments', 5, { ownerId: otherOwner.id });
+  server.create('owners', { owner: 'Main Admin2', desc: 'Owner CCH' });
 
   const ownerFeeFine = server.create('owners', { owner: 'Main Admin3', desc: 'Owner DGB' });
   server.createList('feefines', 4, { ownerId: ownerFeeFine.id });
 
-  server.createList('transfers', 2, { ownerId: owner1.id });
-
-  server.createList('transfers', 3, { ownerId: otherOwner.id });
-
   server.createList('service-point', 3);
-
 
   server.get('feefines');
 
@@ -58,7 +49,6 @@ export default (server) => {
     return record.attrs;
   });
 
-
   server.delete('/owners/:id', (schema, request) => {
     const matchingowner = schema.db.owners.find(request.params.id);
     const matchingfeefines = schema.db.feefines.where({ ownerId: request.params.id });
@@ -87,44 +77,4 @@ export default (server) => {
 
     return model.attrs;
   });
-
-
-  server.get('payments');
-  server.post('payments', (schema, request) => {
-    const json = JSON.parse(request.requestBody);
-    const record = server.create('payments', json);
-
-    return record.attrs;
-  });
-  server.delete('payments/:id');
-
-  server.put('payments/:id', (schema, request) => {
-    const {
-      params: { id },
-      requestBody,
-    } = request;
-    const model = schema.payments.find(id);
-    const json = JSON.parse(requestBody);
-
-    model.update({ ...json });
-
-    return model.attrs;
-  });
-
-  server.get('/transfers', (schema) => {
-    return schema.transfers.all();
-  });
-
-  server.post('/transfers', (schema, request) => {
-    const body = JSON.parse(request.requestBody);
-    return schema.transfers.create(body);
-  });
-
-  server.put('/transfers/:id', ({ transfers }, request) => {
-    const matching = transfers.find(request.params.id);
-    const body = JSON.parse(request.requestBody);
-    return matching.update(body);
-  });
-
-  server.delete('/transfers/:id');
 };
