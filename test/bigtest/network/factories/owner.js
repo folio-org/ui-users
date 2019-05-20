346f5b5bc36e06d9d395dc21404c0dbc2b243427
DGB2018 <lmanillah@dgb.unam.mx>
2019-05-19 22:26:51 -0500

    Update data

--- test/bigtest/network/factories/owner.js
+++ test/bigtest/network/factories/owner.js
@@ -2,7 +2,13 @@ import { Factory, faker } from '@bigtest/mirage';
 
 export default Factory.extend({
   id: () => faker.random.uuid(),
-  owner: () => faker.random.word(),
-  desc: () => faker.random.word(),
+  owner: (i) => 'Main Admin' + i,
+  desc: (i) => 'Main Library Office Desk' + i,
+  metadata: {
+    createdDate: () => '2019-02-05T18:49:20.839+0000',
+    createdByUserId: () => 'ce0e0d5b-b5f3-4ad5-bccb-49c0784298fd',
+    updatedDate: () => '2019-02-05T18:49:20.839+0000',
+    updatedByUserId: () => 'ce0e0d5b-b5f3-4ad5-bccb-49c0784298fd'
+  },
   servicePointOwner: [],
 });
