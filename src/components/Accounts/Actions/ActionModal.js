4c2f4f69f413521a3c7288ef4c32d0aaf05941ca
DGB2018 <lmanillah@dgb.unam.mx>
2019-08-05 19:14:10 -0500

    UIU-932: Clean up duplicate code

---
+++ src/components/Accounts/Actions/ActionModal.js
@@ -0,0 +1,509 @@
+import _ from 'lodash';
+import React from 'react';
+import PropTypes from 'prop-types';
+import { FormattedMessage } from 'react-intl';
+import {
+  Field,
+  reduxForm,
+  change,
+} from 'redux-form';
+import SafeHTMLMessage from '@folio/react-intl-safe-html';
+import {
+  Row,
+  Col,
+  Button,
+  TextArea,
+  Modal,
+  TextField,
+  Checkbox,
+  Select,
+} from '@folio/stripes/components';
+
+import { calculateSelectedAmount } from '../accountFunctions';
+import css from './PayWaive.css';
+
+const validate = (values, props) => {
+  const {
+    accounts,
+    action,
+    commentRequired
+  } = props;
+
+  const selected = calculateSelectedAmount(accounts);
+  const errors = {};
+
+  if (!values.amount) {
+    errors.amount = <FormattedMessage id="ui-users.accounts.error.field" />;
+  }
+  if (values.amount <= 0) {
+    errors.amount = <FormattedMessage id={`ui-users.accounts.${action}.error.amount`} />;
+  }
+  if (!values.method) {
+    errors.method = <FormattedMessage id="ui-users.accounts.error.select" />;
+  }
+  if (commentRequired && !values.comment) {
+    errors.comment = <FormattedMessage id="ui-users.accounts.error.comment" />;
+  }
+  if (parseFloat(values.amount) > parseFloat(selected)) {
+    errors.amount = <FormattedMessage id={`ui-users.accounts.${action}.error.exceeds`} />;
+  }
+
+  return errors;
+};
+
+const onChange = (values, dispatch, props, prevValues) => {
+  if (values.ownerId !== prevValues.ownerId) {
+    dispatch(change('payment-many-modal', 'method', null));
+  }
+};
+
+const asyncValidate = (values, dispatch, props, blurredField) => {
+  if (blurredField === 'amount') {
+    const amount = parseFloat(values.amount || 0).toFixed(2);
+    dispatch(change(props.form, 'amount', amount));
+  }
+  return new Promise(resolve => resolve());
+};
+
+class ActionModal extends React.Component {
+  static propTypes = {
+    onClose: PropTypes.func,
+    handleSubmit: PropTypes.func,
+    open: PropTypes.bool,
+    accounts: PropTypes.arrayOf(PropTypes.object),
+    data: PropTypes.arrayOf(PropTypes.object),
+    balance: PropTypes.number,
+    initialize: PropTypes.func,
+    submitting: PropTypes.bool,
+    invalid: PropTypes.bool,
+    pristine: PropTypes.bool,
+    reset: PropTypes.func,
+    commentRequired: PropTypes.bool,
+    owners: PropTypes.arrayOf(PropTypes.object),
+    feefines: PropTypes.arrayOf(PropTypes.object),
+    label: PropTypes.string,
+    form: PropTypes.string,
+    action: PropTypes.string,
+    intl: PropTypes.object,
+  };
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      amount: '0.00',
+      showNotify: false,
+      notify: false,
+    };
+  }
+
+  shouldComponentUpdate(nextProps, nextState) {
+    const {
+      accounts,
+      feefines,
+      owners,
+      initialize,
+      open,
+      pristine,
+      invalid,
+    } = this.props;
+    if (!_.isEqual(accounts, nextProps.accounts)) {
+      let showNotify = false;
+      let notify = false;
+      const amount = calculateSelectedAmount(nextProps.accounts);
+      nextProps.accounts.forEach(a => {
+        const feefine = feefines.find(f => f.id === a.feeFineId) || {};
+        const owner = owners.find(o => o.id === a.ownerId) || {};
+        if (feefine.actionNoticeId || owner.defaultActionNoticeId) {
+          showNotify = true;
+          notify = true;
+        }
+      });
+      this.setState({
+        amount,
+        showNotify,
+        notify
+      });
+      if (nextProps.form === 'payment-many-modal') {
+        initialize({ amount, notify: true, ownerId: this.loadServicePoints(nextProps) });
+      } else {
+        initialize({ amount, notify: true });
+      }
+    }
+
+    return (accounts !== nextProps.accounts
+      || this.state !== nextState ||
+      open !== nextProps.open ||
+      pristine !== nextProps.pristine ||
+      invalid !== nextProps.invalid);
+  }
+
+  loadServicePoints = (props) => {
+    const servicePoint = props.defaultServicePointId;
+    const servicePoints = props.servicePointsIds;
+    const owners = props.owners || [];
+    let ownerId = 0;
+    if (servicePoint && servicePoint !== '-') {
+      owners.forEach(o => {
+        if (o.servicePointOwner && o.servicePointOwner.find(s => s.value === servicePoint)) {
+          ownerId = o.id;
+          this.onChangeOwner({ target: { value: o.id } });
+        }
+      });
+    } else if (servicePoints.length === 1) {
+      const sp = servicePoints[0];
+      owners.forEach(o => {
+        if (o.servicePointOwner && o.servicePointOwner.find(s => s.value === sp)) {
+          ownerId = o.id;
+          this.onChangeOwner({ target: { value: o.id } });
+        }
+      });
+    } else if (servicePoints.length === 2) {
+      const sp1 = servicePoints[0];
+      const sp2 = servicePoints[1];
+      owners.forEach(o => {
+        if (o.servicePointOwner && o.servicePointOwner.find(s => s.value === sp1) && o.servicePointOwner.find(s => s.value === sp2)) {
+          ownerId = o.id;
+          this.onChangeOwner({ target: { value: o.id } });
+        }
+      });
+    }
+    return ownerId;
+  }
+
+  onChangeAmount = (e) => {
+    this.setState({
+      amount: parseFloat(e.target.value || 0).toFixed(2),
+    });
+  }
+
+  onToggleNotify = () => {
+    this.setState(prevState => ({
+      notify: !prevState.notify,
+    }));
+  }
+
+  onClose = () => {
+    const {
+      accounts,
+      onClose,
+      reset,
+    } = this.props;
+
+    const selected = calculateSelectedAmount(accounts); // Por que?
+    this.setState({ amount: selected }); // Por que?
+    onClose();
+    reset();
+  }
+
+  onChangeOwner = (e) => {
+    this.setState({
+      ownerId: e.target.value,
+    });
+  }
+
+  renderModalLabel() {
+    const {
+      accounts = [],
+      action,
+      intl: { formatMessage },
+    } = this.props;
+    const { amount } = this.state;
+
+    const selected = calculateSelectedAmount(accounts);
+    const type = amount < selected
+      ? formatMessage({ id: `ui-users.accounts.${action}.summary.partially` })
+      : formatMessage({ id: `ui-users.accounts.${action}.summary.fully` });
+
+    return (
+      <SafeHTMLMessage
+        id="ui-users.accounts.summary"
+        values={{
+          count: accounts.length,
+          amount,
+          type,
+        }}
+      />
+    );
+  }
+
+  renderTransactionInfo = () => {
+    const { accounts } = this.props;
+    return (
+      <Col xs={4}>
+        {accounts.length > 1 ? <br /> : ''}
+        <Row>
+          <Col xs>
+            <FormattedMessage id="ui-users.accounts.payment.field.transactionInfo" />
+          </Col>
+        </Row>
+        <Row>
+          <Col xs>
+            <FormattedMessage id="ui-users.accounts.payment.transaction.placeholder">
+              {placeholder => (
+                <Field
+                  name="transaction"
+                  component={TextField}
+                  placeholder={placeholder}
+                />
+              )}
+            </FormattedMessage>
+          </Col>
+        </Row>
+      </Col>
+    );
+  }
+
+  renderMethod = (options) => {
+    const { action } = this.props;
+    return (
+      <Col xs={action === 'payment' ? 3 : 7}>
+        <Row>
+          <Col xs>
+            <FormattedMessage id={`ui-users.accounts.${action}.method`} />
+            {'*'}
+          </Col>
+        </Row>
+        <Row>
+          <Col xs id="action-selection">
+            <FormattedMessage id={`ui-users.accounts.${action}.method.placeholder`}>
+              {placeholder => (
+                <Field
+                  name="method"
+                  component={Select}
+                  dataOptions={options}
+                  placeholder={placeholder}
+                />
+              )}
+            </FormattedMessage>
+          </Col>
+        </Row>
+      </Col>
+    );
+  }
+
+  render() {
+    const {
+      accounts,
+      action,
+      balance,
+      commentRequired,
+      data,
+      handleSubmit,
+      intl: { formatMessage },
+      invalid,
+      label,
+      open,
+      owners,
+      pristine,
+      submitting,
+    } = this.props;
+
+    const {
+      amount,
+      ownerId,
+      showNotify,
+      notify
+    } = this.state;
+
+    const selected = calculateSelectedAmount(accounts);
+    const remaining = parseFloat(balance - amount).toFixed(2);
+    const staffPlaceholder = formatMessage({
+      id: 'ui-users.accounts.placeholder.additional'
+    },
+    {
+      action: formatMessage({ id: `ui-users.accounts.actions.${action}` }),
+      type: formatMessage({ id: `ui-users.accounts.${(commentRequired) ? 'required' : 'optional'}` })
+    });
+
+    const ownerOptions = owners.filter(o => o.owner !== 'Shared').map(o => ({ value: o.id, label: o.owner }));
+
+    let options = (action === 'payment') ? data.filter(d => (d.ownerId === (accounts.length > 1 ? ownerId : (accounts[0] || {}).ownerId))) : data;
+    options = _.uniqBy(options.map(o => ({ id: o.id, label: o[label] })), 'label');
+
+    return (
+      <Modal
+        id={`${action}-modal`}
+        open={open}
+        label={<FormattedMessage id={`ui-users.accounts.${action}.modalLabel`} />}
+        onClose={this.onClose}
+        size="medium"
+        dismissible
+      >
+        <form>
+          <Row>
+            <Col xs>{this.renderModalLabel()}</Col>
+          </Row>
+          <br />
+          <Row>
+            <Col xs={5}>
+              <Row end="xs">
+                <Col xs={7}>
+                  <FormattedMessage id="ui-users.accounts.totalOwed" />
+                  {':'}
+                </Col>
+                <Col xs={4}>
+                  {balance}
+                </Col>
+              </Row>
+              <Row end="xs">
+                <Col xs={7}>
+                  <FormattedMessage id="ui-users.accounts.selectedAmount" />
+                  {':'}
+                </Col>
+                <Col xs={4}>
+                  {selected}
+                </Col>
+              </Row>
+              <Row end="xs">
+                <Col xs={7}>
+                  <b>
+                    <FormattedMessage id={`ui-users.accounts.${action}.amount`} />
+                    {'*:'}
+                  </b>
+                </Col>
+                <Col xs={4} className={css.customCol}>
+                  <div>
+                    <Field
+                      id="amount"
+                      name="amount"
+                      component={TextField}
+                      onChange={this.onChangeAmount}
+                      hasClearIcon={false}
+                      fullWidth
+                      marginBottom0
+                      autoFocus
+                      required
+                    />
+                  </div>
+                </Col>
+              </Row>
+              <Row end="xs">
+                <Col xs={7}>
+                  <FormattedMessage id="ui-users.accounts.remainingAmount" />
+                  {':'}
+                </Col>
+                <Col xs={4}>
+                  {remaining}
+                </Col>
+              </Row>
+            </Col>
+            {(action === 'payment' && accounts.length > 1) &&
+              <Col xs={4}>
+                <Row>
+                  <Col xs>
+                    <FormattedMessage id="ui-users.accounts.payment.field.ownerDesk" />
+                    {'*'}
+                  </Col>
+                </Row>
+                <Row>
+                  <Col xs>
+                    <FormattedMessage id="ui-users.accounts.payment.owner.placeholder">
+                      {placeholder => (
+                        <Field
+                          id="ownerId"
+                          name="ownerId"
+                          component={Select}
+                          dataOptions={ownerOptions}
+                          placeholder={placeholder}
+                          onChange={this.onChangeOwner}
+                        />
+                      )}
+                    </FormattedMessage>
+                  </Col>
+                </Row>
+              </Col>
+            }
+            {this.renderMethod(options)}
+            {action === 'payment' && this.renderTransactionInfo()}
+          </Row>
+          <br />
+          {(action === 'paymany' && accounts.length > 1) &&
+            <Row>
+              {this.renderTransactionInfo()}
+            </Row>
+          }
+          <Row>
+            <Col xs>
+              <FormattedMessage id="ui-users.accounts.commentStaff" />
+              {(commentRequired) ? '*' : ''}
+            </Col>
+          </Row>
+          <br />
+          <Row>
+            <Col xs>
+              <Field
+                id="comments"
+                name="comment"
+                component={TextArea}
+                placeholder={staffPlaceholder}
+              />
+            </Col>
+          </Row>
+          {showNotify &&
+            <div>
+              <Row>
+                <Col xs>
+                  <Field
+                    id="notify"
+                    name="notify"
+                    component={Checkbox}
+                    checked={notify}
+                    onChange={this.onToggleNotify}
+                    inline
+                  />
+                  <FormattedMessage id="ui-users.accounts.notifyPatron" />
+                </Col>
+              </Row>
+            </div>
+          }
+          <br />
+          {(notify && showNotify) &&
+            <div>
+              <Row>
+                <Col xs>
+                  <FormattedMessage id="ui-users.accounts.infoPatron" />
+                </Col>
+              </Row>
+              <br />
+              <Row>
+                <Col xs>
+                  <Field
+                    id="patronInfo"
+                    name="patronInfo"
+                    component={TextArea}
+                  />
+                </Col>
+              </Row>
+            </div>
+          }
+          <Row end="xs">
+            <Col xs>
+              <Button
+                id="cancel-button"
+                onClick={this.onClose}
+              >
+                <FormattedMessage id="ui-users.cancel" />
+              </Button>
+              <Button
+                id="submit-button"
+                buttonStyle="primary"
+                onClick={handleSubmit}
+                disabled={pristine || submitting || invalid}
+              >
+                <FormattedMessage id={`ui-users.accounts.${action}`} />
+              </Button>
+            </Col>
+          </Row>
+        </form>
+      </Modal>
+    );
+  }
+}
+
+export default reduxForm({
+  asyncBlurFields: ['amount'],
+  asyncValidate,
+  onChange,
+  validate,
+})(ActionModal);
