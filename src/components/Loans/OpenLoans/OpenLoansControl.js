886abd1a00344a5e99f74e5974b8c4bc160eee4b
Zak Burke <zburke@cornell.edu>
2019-09-17 10:51:07 -0400

    fix 'policy' typo

--- src/components/Loans/OpenLoans/OpenLoansControl.js
+++ src/components/Loans/OpenLoans/OpenLoansControl.js
@@ -143,7 +143,7 @@ class OpenLoansControl extends React.Component {
     }));
   };
 
-  getLoanPolicie = (policieId) => {
+  getLoanPolicy = (policieId) => {
     const { loanPolicies } = this.props;
 
     return loanPolicies[policieId];
@@ -407,7 +407,7 @@ class OpenLoansControl extends React.Component {
           isLoanChecked={this.isLoanChecked}
           requestRecords={(resources.requests || {}).records || []}
           resources={resources}
-          getLoanPolicie={this.getLoanPolicie}
+          getLoanPolicy={this.getLoanPolicy}
           handleOptionsChange={this.handleOptionsChange}
           possibleColumns={this.possibleColumns}
           hideChangeDueDateDialog={this.hideChangeDueDateDialog}
