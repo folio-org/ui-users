c1426fc8372170836cf81790a2840d27bc5de7c2
Zak Burke <zburke@cornell.edu>
2020-01-16 13:00:55 -0500

    FOLIO-2097 eliminate setupApplication in before
    `before` blocks are utterly completely hoisted to the top of everything
    and run before _every_ test. This means that in the change-due-date-test
    script, we'd be running user-status-filter-test's `before` block and all
    other tests' `before` blocks. That could lead to lots of unnecessary app
    mounting and unmounting and general madness.
    
    The findings of FOLIO-2360 notwithstanding and #1057 notwithstanding, it
    seems that a better approach than begin blocks may be to nest `describe`
    blocks. This allows us to make sure `setupApplication` is run once at
    the top level of each test script and guarantees it finishes executing
    before nested describe blocks, which seems to be the problem #1057
    alludes too.
    
    Refs FOLIO-2097, FOLIO-2360

--- test/bigtest/tests/permissions-assign-test.js
+++ test/bigtest/tests/permissions-assign-test.js
@@ -1,5 +1,4 @@
 import {
-  before,
   beforeEach,
   describe,
   it,
@@ -15,66 +14,65 @@ describe('Permissions assign', () => {
   const permissionsAmount = 10;
   const permissionSetsAmount = 1;
 
-  before(function () {
-    setupApplication({ permissions: { 'perms.users.get': true } });
-  });
+  setupApplication({ permissions: { 'perms.users.get': true } });
 
-
-  beforeEach(async function () {
-    this.server.createList('permissions', permissionsAmount);
-    this.server.createList('permissions', permissionSetsAmount, { mutable: true });
-    const user = this.server.create('user');
-
-    this.visit(`/users/preview/${user.id}`);
-    await InstanceViewPage.whenLoaded();
-  });
-
-  it('edit button is present', () => {
-    expect(InstanceViewPage.editButtonPresent).to.be.true;
-  });
-
-  describe('entering edit mode', () => {
+  describe('visit user details', () => {
     beforeEach(async function () {
-      await InstanceViewPage.clickEditButton();
-      await UserFormPage.whenLoaded();
+      this.server.createList('permissions', permissionsAmount);
+      this.server.createList('permissions', permissionSetsAmount, { mutable: true });
+      const user = this.server.create('user');
+
+      this.visit(`/users/preview/${user.id}`);
+      await InstanceViewPage.whenLoaded();
     });
 
-    describe('add permission button', () => {
-      beforeEach(async () => {
-        await UserFormPage.togglePermissionAccordionButton.click();
+    it('edit button is present', () => {
+      expect(InstanceViewPage.editButtonPresent).to.be.true;
+    });
+
+    describe('entering edit mode', () => {
+      beforeEach(async function () {
+        await InstanceViewPage.clickEditButton();
+        await UserFormPage.whenLoaded();
       });
 
-      it('should be displayed', () => {
-        expect(UserFormPage.addPermissionButton.isPresent).to.be.true;
-      });
-
-      it('should be 0 permissions', () => {
-        expect(UserFormPage.permissions().length).to.equal(0);
-      });
-
-      describe('add permission button click', () => {
-        beforeEach(async function () {
-          await UserFormPage.addPermissionButton.click();
+      describe('add permission button', () => {
+        beforeEach(async () => {
+          await UserFormPage.togglePermissionAccordionButton.click();
         });
 
-        describe('permissions modal', () => {
-          it('should be displayed', () => {
-            expect(UserFormPage.permissionsModal.isPresent).to.be.true;
+        it('should be displayed', () => {
+          expect(UserFormPage.addPermissionButton.isPresent).to.be.true;
+        });
+
+        it('should be 0 permissions', () => {
+          expect(UserFormPage.permissions().length).to.equal(0);
+        });
+
+        describe('add permission button click', () => {
+          beforeEach(async function () {
+            await UserFormPage.addPermissionButton.click();
           });
 
-          describe('assign all permissions', () => {
-            beforeEach(async function () {
-              await PermissionSetForm.permissionsModal.permissionsList.selectAllPermissions.click();
+          describe('permissions modal', () => {
+            it('should be displayed', () => {
+              expect(UserFormPage.permissionsModal.isPresent).to.be.true;
             });
 
-            describe('submit button click', () => {
+            describe('assign all permissions', () => {
               beforeEach(async function () {
-                await UserFormPage.permissionsModal.saveButton.click();
-                await UserFormPage.submitButton.click();
+                await PermissionSetForm.permissionsModal.permissionsList.selectAllPermissions.click();
               });
 
-              it('user view page should be displayed', () => {
-                expect(InstanceViewPage.isPresent).to.be.true;
+              describe('submit button click', () => {
+                beforeEach(async function () {
+                  await UserFormPage.permissionsModal.saveButton.click();
+                  await UserFormPage.submitButton.click();
+                });
+
+                it('user view page should be displayed', () => {
+                  expect(InstanceViewPage.isPresent).to.be.true;
+                });
               });
             });
           });
--- test/bigtest/tests/user-edit-page-test.js
+++ test/bigtest/tests/user-edit-page-test.js
@@ -1,5 +1,4 @@
 import {
-  before,
   beforeEach,
   describe,
   it,
@@ -16,157 +15,157 @@ describe('User Edit Page', () => {
   let user1;
   let user2;
 
-  before(function () {
-    setupApplication();
-  });
+  setupApplication();
 
-  beforeEach(async function () {
-    user1 = this.server.create('user');
-    user2 = this.server.create('user');
-    this.server.create('requestPreferences', {
-      userId: user1.id,
-      delivery: true,
-      defaultServicePointId: 'servicepointId1',
-      defaultDeliveryAddressTypeId: 'Type1',
-      fulfillment: 'Delivery',
-    });
-
-    this.visit(`/users/preview/${user1.id}`);
-    await InstanceViewPage.whenLoaded();
-  });
-
-  it('edit button is present', () => {
-    expect(InstanceViewPage.editButtonPresent).to.be.true;
-  });
-
-  describe('visiting the edit user page', () => {
+  describe('visit users-details', () => {
     beforeEach(async function () {
-      await InstanceViewPage.clickEditButton();
-      await UserFormPage.whenLoaded();
+      user1 = this.server.create('user');
+      user2 = this.server.create('user');
+      this.server.create('requestPreferences', {
+        userId: user1.id,
+        delivery: true,
+        defaultServicePointId: 'servicepointId1',
+        defaultDeliveryAddressTypeId: 'Type1',
+        fulfillment: 'Delivery',
+      });
+
+      this.visit(`/users/preview/${user1.id}`);
+      await InstanceViewPage.whenLoaded();
     });
 
-    it('displays the title in the pane header', () => {
-      expect(UserFormPage.title).to.equal(user1.username);
+    it('edit button is present', () => {
+      expect(InstanceViewPage.editButtonPresent).to.be.true;
     });
 
-    describe('validating user barcode', () => {
+    describe('visiting the edit user page', () => {
       beforeEach(async function () {
-        await UserFormPage.barcodeField.fillAndBlur(user2.barcode);
-        await UserFormPage.submitButton.click();
+        await InstanceViewPage.clickEditButton();
+        await UserFormPage.whenLoaded();
       });
 
-      it('should display validation error', () => {
-        expect(UserFormPage.feedbackError).to.equal('This barcode has already been taken');
-      });
-    });
-
-    describe('validating empty user barcode', () => {
-      beforeEach(async function () {
-        await UserFormPage.barcodeField.fillAndBlur('');
-        await UserFormPage.submitButton.click();
+      it('displays the title in the pane header', () => {
+        expect(UserFormPage.title).to.equal(user1.username);
       });
 
-      it('should show user detail view', () => {
-        expect(InstanceViewPage.isVisible).to.equal(true);
-      });
-    });
-
-    describe('validating username', () => {
-      beforeEach(async function () {
-        await UserFormPage.usernameField.fillAndBlur(user2.username);
-        await UserFormPage.submitButton.click();
-      });
-
-      it('should display validation error', () => {
-        expect(UserFormPage.feedbackError).to.equal('This username already exists');
-      });
-    });
-
-    describe('pane header menu', () => {
-      beforeEach(async () => {
-        await UserFormPage.cancelButton.click();
-      });
-
-      it('should redirect to view users page after click', () => {
-        expect(users.$root).to.exist;
-      });
-    });
-
-    describe('request preferences', () => {
-      it('should display "hold shelf" checkbox as checked', () => {
-        expect(UserFormPage.holdShelfCheckboxIsChecked).to.be.true;
-      });
-
-      it('should display "hold shelf" checkbox as disabled', () => {
-        expect(UserFormPage.holdShelfCheckboxIsDisabled).to.be.true;
-      });
-
-      describe('when delivery is activated', () => {
-        it('should display "delivery" checkbox as checked', () => {
-          expect(UserFormPage.deliveryCheckboxIsChecked).to.be.true;
+      describe('validating user barcode', () => {
+        beforeEach(async function () {
+          await UserFormPage.barcodeField.fillAndBlur(user2.barcode);
+          await UserFormPage.submitButton.click();
         });
 
-        it('should display selected "Fulfillment preference"', () => {
-          expect(UserFormPage.fulfillmentPreference.value).to.equal('Delivery');
+        it('should display validation error', () => {
+          expect(UserFormPage.feedbackError).to.equal('This barcode has already been taken');
+        });
+      });
+
+      describe('validating empty user barcode', () => {
+        beforeEach(async function () {
+          await UserFormPage.barcodeField.fillAndBlur('');
+          await UserFormPage.submitButton.click();
         });
 
-        it('should display selected default address type', () => {
-          const CLAIM_ADDRESS_TYPE_VALUE = 'Type1';
-          expect(UserFormPage.defaultAddressTypeField.value).to.equal(CLAIM_ADDRESS_TYPE_VALUE);
+        it('should show user detail view', () => {
+          expect(InstanceViewPage.isVisible).to.equal(true);
+        });
+      });
+
+      describe('validating username', () => {
+        beforeEach(async function () {
+          await UserFormPage.usernameField.fillAndBlur(user2.username);
+          await UserFormPage.submitButton.click();
         });
 
-        describe('and selected default address type was changed', () => {
-          beforeEach(async () => {
-            await UserFormPage.firstAddressTypeField.selectAndBlur('Home');
-            await UserFormPage.defaultAddressTypeField.selectAndBlur('Home');
-            await UserFormPage.firstAddressTypeField.selectAndBlur('Order');
-          });
+        it('should display validation error', () => {
+          expect(UserFormPage.feedbackError).to.equal('This username already exists');
+        });
+      });
 
-          it('should reset default address type to empty value', () => {
-            expect(UserFormPage.defaultAddressTypeField.value).to.equal('');
-          });
-
-          it('should display validation message of "Default delivery address field"', () => {
-            expect(UserFormPage.defaultAddressTypeValidationMessage).to.equal('Please fill this in to continue');
-          });
+      describe('pane header menu', () => {
+        beforeEach(async () => {
+          await UserFormPage.cancelButton.click();
         });
 
-        describe('and selected default address type was deleted', () => {
-          beforeEach(async () => {
-            await UserFormPage.clickAddAddressButton();
-            await UserFormPage.firstAddressTypeField.selectAndBlur('Home');
-            await UserFormPage.defaultAddressTypeField.selectAndBlur('Home');
-            await UserFormPage.deleteAddressType();
-          });
+        it('should redirect to view users page after click', () => {
+          expect(users.$root).to.exist;
+        });
+      });
 
-          it('should reset default address type to empty value', () => {
-            expect(UserFormPage.defaultAddressTypeField.value).to.equal('');
-          });
-
-          it('should display validation message of "Default delivery address field"', () => {
-            expect(UserFormPage.defaultAddressTypeValidationMessage)
-              .to.equal('Please, add at least one address inside "Addresses" section');
-          });
+      describe('request preferences', () => {
+        it('should display "hold shelf" checkbox as checked', () => {
+          expect(UserFormPage.holdShelfCheckboxIsChecked).to.be.true;
         });
 
-        describe('and all address types were deleted', () => {
-          beforeEach(async () => {
-            await UserFormPage.clickAddAddressButton();
-            await UserFormPage.clickAddAddressButton();
-            await UserFormPage.firstAddressTypeField.selectAndBlur('Home');
-            await UserFormPage.secondAddressTypeField.selectAndBlur('Order');
-            await UserFormPage.defaultAddressTypeField.selectAndBlur('Order');
-            await UserFormPage.deleteAddressType();
-            await UserFormPage.deleteAddressType();
+        it('should display "hold shelf" checkbox as disabled', () => {
+          expect(UserFormPage.holdShelfCheckboxIsDisabled).to.be.true;
+        });
+
+        describe('when delivery is activated', () => {
+          it('should display "delivery" checkbox as checked', () => {
+            expect(UserFormPage.deliveryCheckboxIsChecked).to.be.true;
           });
 
-          it('should reset default address type to empty value', () => {
-            expect(UserFormPage.defaultAddressTypeField.value).to.equal('');
+          it('should display selected "Fulfillment preference"', () => {
+            expect(UserFormPage.fulfillmentPreference.value).to.equal('Delivery');
           });
 
-          it('should display validation message of "Default delivery address field"', () => {
-            expect(UserFormPage.defaultAddressTypeValidationMessage)
-              .to.equal('Please, add at least one address inside "Addresses" section');
+          it('should display selected default address type', () => {
+            const CLAIM_ADDRESS_TYPE_VALUE = 'Type1';
+            expect(UserFormPage.defaultAddressTypeField.value).to.equal(CLAIM_ADDRESS_TYPE_VALUE);
+          });
+
+          describe('and selected default address type was changed', () => {
+            beforeEach(async () => {
+              await UserFormPage.firstAddressTypeField.selectAndBlur('Home');
+              await UserFormPage.defaultAddressTypeField.selectAndBlur('Home');
+              await UserFormPage.firstAddressTypeField.selectAndBlur('Order');
+            });
+
+            it('should reset default address type to empty value', () => {
+              expect(UserFormPage.defaultAddressTypeField.value).to.equal('');
+            });
+
+            it('should display validation message of "Default delivery address field"', () => {
+              expect(UserFormPage.defaultAddressTypeValidationMessage).to.equal('Please fill this in to continue');
+            });
+          });
+
+          describe('and selected default address type was deleted', () => {
+            beforeEach(async () => {
+              await UserFormPage.clickAddAddressButton();
+              await UserFormPage.firstAddressTypeField.selectAndBlur('Home');
+              await UserFormPage.defaultAddressTypeField.selectAndBlur('Home');
+              await UserFormPage.deleteAddressType();
+            });
+
+            it('should reset default address type to empty value', () => {
+              expect(UserFormPage.defaultAddressTypeField.value).to.equal('');
+            });
+
+            it('should display validation message of "Default delivery address field"', () => {
+              expect(UserFormPage.defaultAddressTypeValidationMessage)
+                .to.equal('Please, add at least one address inside "Addresses" section');
+            });
+          });
+
+          describe('and all address types were deleted', () => {
+            beforeEach(async () => {
+              await UserFormPage.clickAddAddressButton();
+              await UserFormPage.clickAddAddressButton();
+              await UserFormPage.firstAddressTypeField.selectAndBlur('Home');
+              await UserFormPage.secondAddressTypeField.selectAndBlur('Order');
+              await UserFormPage.defaultAddressTypeField.selectAndBlur('Order');
+              await UserFormPage.deleteAddressType();
+              await UserFormPage.deleteAddressType();
+            });
+
+            it('should reset default address type to empty value', () => {
+              expect(UserFormPage.defaultAddressTypeField.value).to.equal('');
+            });
+
+            it('should display validation message of "Default delivery address field"', () => {
+              expect(UserFormPage.defaultAddressTypeValidationMessage)
+                .to.equal('Please, add at least one address inside "Addresses" section');
+            });
           });
         });
       });
