55cd29579972ce2a473ab9f3811096e466c0cbe6
Anna_Melnyk <Anna_Melnyk@epam.com>
2020-02-20 16:08:26 +0200

    UIU-1347: Remove useless sorter

--- src/components/Loans/OpenLoans/helpers/getListDataFormatter.js
+++ src/components/Loans/OpenLoans/helpers/getListDataFormatter.js
@@ -83,7 +83,6 @@ export default function getListDataFormatter(
       key:'callNumber',
       view: formatMessage({ id: 'ui-users.loans.details.effectiveCallNumber' }),
       formatter: loan => (<div data-test-list-call-numbers>{ effectiveCallNumber(loan) }</div>),
-      sorter: loan => effectiveCallNumber(loan),
     },
     'loanPolicy': {
       key:'loanPolicy',
