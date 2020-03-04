e582fb6c68f8cc6e621a2508118baa9719fbad5c
Denys Bohdan <Denys_Bohdan@epam.com>
2020-03-04 17:46:58 +0200

    UIU-1492 Passed down permission props from module to Custom Fields

--- src/routes/EditCustomFields.js
+++ src/routes/EditCustomFields.js
@@ -1,19 +1,30 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+import { stripesConnect } from '@folio/stripes-core';
 import { EditCustomFieldsSettings } from '@folio/stripes/smart-components';
 
 const propTypes = {
   history: PropTypes.shape({
     push: PropTypes.func.isRequired,
   }).isRequired,
+  stripes: PropTypes.shape({
+    hasPerm: PropTypes.func.isRequired,
+  }).isRequired,
 };
 
 const EditCustomFields = ({
   history,
+  stripes,
 }) => {
   const redirectToView = () => {
     history.push('/settings/users/custom-fields');
+  };
+
+  const permissions = {
+    canView: stripes.hasPerm('ui-users.settings.customfields.view'),
+    canEdit: stripes.hasPerm('ui-users.settings.customfields.edit'),
+    canDelete: stripes.hasPerm('ui-users.stripes.customfields.delete'),
   };
 
   return (
@@ -21,10 +32,11 @@ const EditCustomFields = ({
       backendModuleName="users"
       entityType="user"
       redirectToView={redirectToView}
+      permissions={permissions}
     />
   );
 };
 
 EditCustomFields.propTypes = propTypes;
 
-export default EditCustomFields;
+export default stripesConnect(EditCustomFields);
--- src/settings/CustomFieldsSettings.js
+++ src/settings/CustomFieldsSettings.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+
+import { stripesConnect } from '@folio/stripes-core';
 import { ViewCustomFieldsSettings } from '@folio/stripes/smart-components';
 
 const propTypes = {
+  stripes: PropTypes.shape({
+    hasPerm: PropTypes.func.isRequired,
+  }).isRequired,
   history: PropTypes.shape({
     push: PropTypes.func.isRequired,
   }).isRequired,
@@ -10,9 +15,16 @@ const propTypes = {
 
 const CustomFieldsSettings = ({
   history,
+  stripes,
 }) => {
   const redirectToEdit = () => {
     history.push('/users/custom-fields/edit');
+  };
+
+  const permissions = {
+    canView: stripes.hasPerm('ui-users.settings.customfields.view'),
+    canEdit: stripes.hasPerm('ui-users.settings.customfields.edit'),
+    canDelete: stripes.hasPerm('ui-users.stripes.customfields.delete'),
   };
 
   return (
@@ -20,10 +32,11 @@ const CustomFieldsSettings = ({
       backendModuleName="users"
       entityType="user"
       redirectToEdit={redirectToEdit}
+      permissions={permissions}
     />
   );
 };
 
 CustomFieldsSettings.propTypes = propTypes;
 
-export default CustomFieldsSettings;
+export default stripesConnect(CustomFieldsSettings);
