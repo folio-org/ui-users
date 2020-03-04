16b8ecb68ba64c9451029c563e67f770bbeb912f
Denys Bohdan <Denys_Bohdan@epam.com>
2020-03-04 14:26:19 +0200

    UIU-1492 Added permission tests for custom fields

--- test/bigtest/interactors/settings-custom-fields.js
+++ test/bigtest/interactors/settings-custom-fields.js
@@ -2,19 +2,22 @@ import {
   interactor,
   isPresent,
   clickable,
+  count
 } from '@bigtest/interactor';
 
 import { AccordionSetInteractor } from '@folio/stripes-components/lib/Accordion/tests/interactor'; // eslint-disable-line import/no-extraneous-dependencies
 
 @interactor class CustomFieldsInteractor {
-  customFieldsPaneIsPresent = isPresent('#custom-fields-pane');
+  editCustomFieldsPaneIsPresent = isPresent('#edit-custom-fields-pane');
+  viewCustomFieldsPaneIsPresent = isPresent('#custom-fields-pane');
   editFieldsButtonPresent = isPresent('[data-test-custom-fields-edit-button]');
   clickEditFieldsButton = clickable('[data-test-custom-fields-edit-button]');
   noCustomFieldsMessagePresent = isPresent('[data-test-custom-fields-no-fields-message]');
   customFieldsList = new AccordionSetInteractor('#custom-fields-list');
+  deleteCustomFieldButtonsCount = count('[type="button"][data-test-custom-field-delete-button]');
 
   whenLoaded() {
-    return this.when(() => this.editFieldsButtonPresent).timeout(10000);
+    return this.when(() => this.isPresent).timeout(1000);
   }
 
   whenCustomFieldsLoaded() {
--- test/bigtest/tests/settings-custom-fields-test.js
+++ test/bigtest/tests/settings-custom-fields-test.js
@@ -1,6 +1,7 @@
 import { expect } from 'chai';
 
 import {
+  before,
   beforeEach,
   describe,
   it,
@@ -107,24 +108,179 @@ describe('Settings custom fields', () => {
     });
   });
 
-  describe('when user does not have view permissions', () => {
-    beforeEach(async function () {
-      this.visit('/settings/users/custom-fields');
+  describe('permissions', () => {
+    const customFields = [
+      {
+        id: 'daf98472-7311-487d-a018-65fb2496e3e4',
+        name: 'Custom field 1',
+        refId: 'custom-field-_1',
+        type: 'TEXTBOX_LONG',
+        entityType: 'user',
+        visible: false,
+        required: false,
+        order: 1,
+        helpText: 'Very helpful text',
+        metadata: {
+          createdDate: '2020-02-25T09:06:16.182+0000',
+          createdByUserId: '6be4382b-cfa9-5571-a7ed-cb89536de85b',
+          createdByUsername: 'diku_admin',
+          updatedDate: '2020-02-25T09:06:16.182+0000',
+          updatedByUserId: '6be4382b-cfa9-5571-a7ed-cb89536de85b'
+        }
+      },
+      {
+        id: '4e8f8e16-8fd2-4d69-8d64-3ee8c0bb385f',
+        name: 'Custom field 2',
+        refId: 'custom-field-_2',
+        type: 'TEXTBOX_SHORT',
+        entityType: 'user',
+        visible: true,
+        required: true,
+        order: 2,
+        helpText: '',
+        metadata: {
+          createdDate: '2020-02-25T09:06:16.182+0000',
+          createdByUserId: '6be4382b-cfa9-5571-a7ed-cb89536de85b',
+          createdByUsername: 'diku_admin',
+          updatedDate: '2020-02-25T09:06:16.182+0000',
+          updatedByUserId: '6be4382b-cfa9-5571-a7ed-cb89536de85b'
+        }
+      },
+    ];
+
+    beforeEach(function () {
+      this.server.get('/custom-fields', () => ({
+        'customFields': customFields,
+        'totalRecords': 2
+      }));
     });
 
-    it('should not display Custom fields pane', () => {
-      expect(CustomFieldsInteractor.customFieldsPaneIsPresent).to.be.false;
+    describe('when user does not have view permissions', () => {
+      beforeEach(async function () {
+        this.visit('/settings/users/custom-fields');
+      });
+  
+      it('should not display Custom fields pane', () => {
+        expect(CustomFieldsInteractor.viewCustomFieldsPaneIsPresent).to.be.false;
+      });
     });
-  });
-
-  describe('when user does have view permissions', () => {
-    beforeEach(async function () {
-      this.visit('/settings/users/custom-fields');
-      await CustomFieldsInteractor.whenLoaded();
+  
+    describe('when user does have view permissions', () => {
+      before(() => {
+        setupApplication({
+          permissions: ['ui-users.settings.customfields.view'],
+        });
+      });
+  
+      beforeEach(async function () {
+        this.visit('/settings/users/custom-fields');
+        await CustomFieldsInteractor.whenLoaded();
+      });
+  
+      it('should display Custom fields pane', () => {
+        expect(CustomFieldsInteractor.viewCustomFieldsPaneIsPresent).to.be.true;
+      });
     });
 
-    it('should display Custom fields pane', () => {
-      expect(CustomFieldsInteractor.customFieldsPaneIsPresent).to.be.true;
+    describe('when user does not have edit permissions', () => {
+      before(() => {
+        setupApplication({
+          permissions: ['ui-users.settings.customfields.view'],
+        });
+      });
+
+      describe('and visits Settings > Users > Custom Fields page', () => {
+        beforeEach(async function () {
+          this.visit('/settings/users/custom-fields');
+          await CustomFieldsInteractor.whenLoaded();
+        });
+
+        it('should not render Edit button', () => {
+          expect(CustomFieldsInteractor.editFieldsButtonPresent).to.be.false;
+        });
+      });
+
+      describe('and visits Edit Custom Fields page', () => {
+        beforeEach(async function () {
+          this.visit('/user/custom-fields/edit');
+          await CustomFieldsInteractor.whenLoaded();
+        });
+
+        it('should not display edit custom fields form', () => {
+          expect(CustomFieldsInteractor.editCustomFieldsPaneIsPresent).to.be.false;
+        });
+      });
+    });
+
+    describe('when user has edit permissions', () => {
+      before(() => {
+        setupApplication({
+          permissions: ['ui-users.settings.customfields.edit'],
+        });
+      });
+
+      describe('and visits Settings > Users > Custom Fields page', () => {
+        beforeEach(async function () {
+          this.visit('/settings/users/custom-fields');
+          await CustomFieldsInteractor.whenLoaded();
+        });
+
+        it('should render Edit button', () => {
+          expect(CustomFieldsInteractor.editFieldsButtonPresent).to.be.true;
+        });
+      });
+
+      describe('and visits Edit Custom Fields page', () => {
+        beforeEach(async function () {
+          this.visit('/settings/users/custom-fields');
+          await CustomFieldsInteractor.whenLoaded();
+          await CustomFieldsInteractor.clickEditFieldsButton();
+        });
+
+        it('should display edit custom fields form', () => {
+          expect(CustomFieldsInteractor.editCustomFieldsPaneIsPresent).to.be.true;
+        });
+      });
+    });
+
+    describe('when user does not have delete permissions', () => {
+      before(() => {
+        setupApplication({
+          permissions: ['ui-users.settings.customfields.edit'],
+        });
+      });
+
+      describe('and visits Edit Custom Fields page', () => {
+        beforeEach(async function () {
+          this.visit('/settings/users/custom-fields');
+          await CustomFieldsInteractor.whenLoaded();
+          await CustomFieldsInteractor.clickEditFieldsButton();
+        });
+
+        it('should not render delete buttons', () => {
+          expect(CustomFieldsInteractor.deleteCustomFieldButtonsCount).to.equal(0);
+        });
+      });
+    });
+
+    describe('when user does have delete permissions', () => {
+      before(() => {
+        setupApplication({
+          permissions: ['ui-users.settings.customfields.delete'],
+        });
+      });
+
+      describe('and visits Edit Custom Fields page', () => {
+        beforeEach(async function () {
+          this.visit('/settings/users/custom-fields');
+          await CustomFieldsInteractor.whenLoaded();
+          await CustomFieldsInteractor.clickEditFieldsButton();
+        });
+
+        it('should render all delete buttons', () => {
+          expect(CustomFieldsInteractor.deleteCustomFieldButtonsCount).to.equal(customFields.length);
+        });
+      });
     });
   });
 });
