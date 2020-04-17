23a1efda6491844c36cf77709286b2a9807254a0
Michał Kuklis <michal.kuklis@gmail.com>
2020-04-17 09:19:10 -0400

    UIU-1596: Build filter string by using ids instead of names (#1258)

--- src/views/UserSearch/Filters.js
+++ src/views/UserSearch/Filters.js
@@ -42,8 +42,7 @@ export default class Filters extends React.Component {
 
   getValuesFromResources = (type, key) => {
     const items = get(this.props.resources, `${type}.records`, [])
-      .map(item => ({ label: item[key], value: item[key] }));
-
+      .map(item => ({ label: item[key], value: item.id }));
     return sortBy(items, 'label');
   };
 
