a53d4a5799f8ce5e27ef973957460010cb8453ec
Matthew Connolly <mjc12@cornell.edu>
2020-05-22 20:09:21 -0400

    Display bulk claim returned results modal

--- src/components/Loans/OpenLoans/components/BulkClaimReturnedModal/BulkClaimReturnedModal.js
+++ src/components/Loans/OpenLoans/components/BulkClaimReturnedModal/BulkClaimReturnedModal.js
@@ -26,6 +26,7 @@ class BulkClaimReturnedModal extends React.Component {
     claimReturned: {
       type: 'okapi',
       fetch: false,
+      throwErrors: false,
       POST: {
         path: 'circulation/loans/%{loanId}/claim-item-returned',
       },
@@ -48,11 +49,13 @@ class BulkClaimReturnedModal extends React.Component {
     super(props);
 
     this.state = {
-      additionalInfo: '',
+      additionalInfo: '',     // The 'additional information' text included as part of a claim returned POST
+      operationResults: [],   // An array of results returned from the server after a POST
+      operationState: 'pre',  // Whether we're at the start ('pre') or end ('post') of a transaction
     };
 
-    this.claimAllReturned = this.claimAllReturned.bind(this);
-    this.handleAdditionalInfoChange = this.handleAdditionalInfoChange.bind(this)
+   // this.claimAllReturned = this.claimAllReturned.bind(this);
+  //  this.handleAdditionalInfoChange = this.handleAdditionalInfoChange.bind(this)
   }
 
   // Return the number of requests for item @id
@@ -68,7 +71,7 @@ class BulkClaimReturnedModal extends React.Component {
   claimItemReturned = (loan) => {
     if (!loan) return null;
 
-    this.props.mutator.loanId.replace(loan.id);
+    this.props.mutator.loanId.replace('abc');
     return this.props.mutator.claimReturned.POST(
       {
         itemClaimedReturnedDateTime: moment().format(),
@@ -78,25 +81,53 @@ class BulkClaimReturnedModal extends React.Component {
   }
 
   claimAllReturned = () => {
-    const promises = Object.values(this.props.checkedLoansIndex).map(loan => this.claimItemReturned(loan).catch(e => e));
+    this.setState({ operationResults: [] });
+    const promises = Object
+                      .values(this.props.checkedLoansIndex)
+                      .map(loan => this.claimItemReturned(loan).catch(e => e));
     Promise.all(promises)
       .then(results => this.finishClaims(results));
   }
 
   finishClaims = (results) => {
-    console.log("got results!", results)
+    this.setState({
+      operationResults: results,
+      operationState: 'post',
+    });
+  }
+
+  // @results: array of results of the claim-returned operation for each loan
+  // Each item in the array will either be a simple object that represents a
+  // successful change (including an id value) or a larger object representing
+  // a CORS error (identifiable by the properties ok: false and url).
+  //
+  // @return array of loan IDs for which the operation returned an error.
+  getUnchangedLoansList = (results) => {
+    return results.filter(r => r.ok === false).map(r => r.url?.match(/^.*\/(.*)\/claim-item-returned/)[1]);
+  }
+
+  onCancel = () => {
+    this.setState({ operationState: 'pre' });
+    this.props.onCancel();
   }
 
   render() {
-    const { checkedLoansIndex, open, onCancel } = this.props;
-    const { additionalInfo } = this.state;
+    const { checkedLoansIndex, open } = this.props;
+    const {
+      additionalInfo,
+      operationResults,
+      operationState,
+    } = this.state;
     const loans = checkedLoansIndex ? Object.values(checkedLoansIndex) : [];
+    // List of loans for which the claim returned operation failed
+    const unchangedLoans = this.getUnchangedLoansList(operationResults);
+    console.log("unchangedloans", unchangedLoans)
 
     const footer = (
       <ModalFooter>
         <Layout className="textRight">
           <Button
-            onClick={onCancel}
+            onClick={this.onCancel}
           >
             <FormattedMessage id="ui-users.cancel" />
           </Button>
@@ -113,8 +144,14 @@ class BulkClaimReturnedModal extends React.Component {
     );
 
     const columns = ['title', 'dueDate', 'requests', 'barcode', 'callNumber', 'loanPolicy'];
+    if (operationState === 'post') { columns.unshift('status'); }
+    const statuses = {
+      OK: 'Item successfully claimed returned',
+      NOT_OK: 'Item could not be claimed returned',
+    };
 
 console.log("loans list", loans)
+console.log("results", operationResults)
     const loansList =
       <MultiColumnList
         interactive={false}
@@ -122,6 +159,7 @@ console.log("loans list", loans)
         contentData={loans}
         visibleColumns={columns}
         columnMapping={{
+          status: 'Claim returned status',
           title: <FormattedMessage id="ui-users.bulkClaimReturned.item.title" />,
           dueDate: <FormattedMessage id="ui-users.dueDate" />,
           requests: <FormattedMessage id="ui-users.loans.details.requests" />,
@@ -130,6 +168,7 @@ console.log("loans list", loans)
           loanPolicy: <FormattedMessage id="ui-users.loans.details.loanPolicy" />,
         }}
         formatter={{
+          status: loan => unchangedLoans.includes(loan.id) ? statuses.NOT_OK : statuses.OK,
           title: loan => loan?.item?.title,
           dueDate: loan => loan?.dueDate,
           requests: loan => this.getRequestCountForItem(loan?.item?.id),
@@ -143,9 +182,20 @@ console.log("loans list", loans)
         // }}
       />
 
+      let modalHeader, statusMessage;
+      if (operationState === 'pre') {
+        modalHeader = "Confirm claimed returned";
+        statusMessage = <span>{loans.length} item(s) will be claimed returned.</span>;
+      }
+      else {
+        const numSuccessfulOperations = loans.length - unchangedLoans.length;
+        modalHeader = "Claim returned confirmation";
+        statusMessage = <span>{unchangedLoans.length} item(s) <strong>not</strong> claimed returned. {numSuccessfulOperations} item(s) successfully claimed returned.</span>;
+      }
+
       const modalContent =
         <>
-          <span>{loans.length} item(s) will be claimed returned.</span>
+          {statusMessage}
           {loansList}
           <Col sm={12} className={css.additionalInformation}>
             <TextArea
@@ -158,7 +208,7 @@ console.log("loans list", loans)
 
     return (
       <Modal
-        label="Confirm claimed returned"
+        label={modalHeader}
         dismissible
         open={open}
         footer={footer}
