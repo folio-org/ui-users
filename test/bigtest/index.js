7a40ef845c971b5f396db19f6209307f580340d3
Michal Kuklis <michal.kuklis@gmail.com>
2020-05-26 13:58:28 -0400

    UIU-1523: Capitalize user status when displayed

--- test/bigtest/index.js
+++ test/bigtest/index.js
@@ -1,7 +1,11 @@
 import 'core-js/stable';
 import 'regenerator-runtime/runtime';
 
+import turnOffWarnings from './helpers/turn-off-warnings';
 import './helpers/monkey-patch-run';
+
+
+turnOffWarnings();
 
 // require all modules ending in "-test" from the current directory and
 // all subdirectories
