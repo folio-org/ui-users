f993bdb4df16b7c11450caa81fc8d0a83c7264e1
ISELA-DGB <57374564+iselagb@users.noreply.github.com>
2020-05-26 15:25:56 -0500

    UIU-1398: Charge fee/fine: Created at must be Service Point in order … (#1266)
    * UIU-1398: Charge fee/fine: Created at must be Service Point in order to balance fees/fines

--- src/views/AccountDetails/AccountDetails.js
+++ src/views/AccountDetails/AccountDetails.js
@@ -71,6 +71,7 @@ class AccountDetails extends React.Component {
     match: PropTypes.object,
     patronGroup: PropTypes.object,
     itemDetails: PropTypes.object,
+    okapi: PropTypes.object,
   };
 
   static defaultProps = {
@@ -254,7 +255,7 @@ class AccountDetails extends React.Component {
       amount: action => (action.amountAction > 0 ? parseFloat(action.amountAction).toFixed(2) : '-'),
       balance: action => (action.balance > 0 ? parseFloat(action.balance).toFixed(2) : '-'),
       transactioninfo: action => action.transactionInformation || '-',
-      created: action => action.createdAt,
+      created: action => (this.props.okapi.currentUser.servicePoints.find(sp => sp.id === action.createdAt) || {}).name,
       source: action => action.source,
       comments: action => (action.comments ? (<div>{action.comments.split('\n').map(c => (<Row><Col>{c}</Col></Row>))}</div>) : ''),
     };
