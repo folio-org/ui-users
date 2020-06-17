7409166984ce57fa2451216ee83fd254fdf3ac21
Maksym Dryha <Maksym_Dryha@epam.com>
2020-06-17 11:12:43 +0300

    remove unneeded test after changes to custom fields feature

--- test/bigtest/interactors/settings-custom-fields.js
+++ test/bigtest/interactors/settings-custom-fields.js
@@ -12,7 +12,6 @@ import { AccordionSetInteractor } from '@folio/stripes-components/lib/Accordion/
   viewCustomFieldsPaneIsPresent = isPresent('#custom-fields-pane');
   editFieldsButtonPresent = isPresent('[data-test-custom-fields-edit-button]');
   clickEditFieldsButton = clickable('[data-test-custom-fields-edit-button]');
-  noCustomFieldsMessagePresent = isPresent('[data-test-custom-fields-no-fields-message]');
   customFieldsList = new AccordionSetInteractor('#custom-fields-list');
   deleteCustomFieldButtonsCount = count('[type="button"][data-test-custom-field-delete-button]');
 
