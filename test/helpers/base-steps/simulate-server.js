2ca43c26add75713b850fbf3709681b5dbe4b3f3
minkimcello <minkimcello@gmail.com>
2020-09-12 23:08:07 -0400

    Add user argument to server

--- test/helpers/base-steps/simulate-server.js
+++ test/helpers/base-steps/simulate-server.js
@@ -1,3 +1,4 @@
+/* eslint-disable import/no-extraneous-dependencies */
 import { test } from '@bigtest/suite';
 import { bigtestGlobals } from '@bigtest/globals';
 import localforage from 'localforage';
@@ -5,22 +6,21 @@ import { start } from '../server';
 
 bigtestGlobals.defaultInteractorTimeout = 12_000;
 
-export default function simulateServer(name, { permissions = [] } = { permissions: [] }) {
+export default function simulateServer(name, { permissions = [] } = { permissions: [] }, user) {
   return test(name)
     .step('set up localforage', async () => {
       localforage.clear();
       localforage.setItem('okapiSess', {
         token: 'test',
-        user: {
-          id: 'test',
+        user: { id: 'test',
           username: 'testuser',
           firstName: 'Test',
           lastName: 'User',
           email: 'user@folio.org',
           addresses: [],
           servicePoints: [],
-          curServicePoint: { id: null }
-        },
+          curServicePoint: { id: null },
+          ...user },
         perms: permissions.reduce((memo, perm) => ({ ...memo, [perm]: true }), {})
       });
     })
