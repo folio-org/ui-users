f357649ffd4e1e2def648a5551a88f731cb64863
minkimcello <minkimcello@gmail.com>
2020-09-22 17:00:28 -0400

    fee-fine details AND history what

--- test/interactors/Link.js
+++ test/interactors/Link.js
@@ -2,6 +2,12 @@ import { createInteractor, perform } from '@bigtest/interactor';
 
 export default createInteractor('link')({
   selector: 'a',
+  locators: {
+    findById: element => element.id
+  },
+  filters: {
+    value: element => element.textContent
+  },
   actions: {
     click: perform((element) => {
       element.click();
--- test/interactors/Section.js
+++ test/interactors/Section.js
@@ -2,5 +2,8 @@ import { createInteractor } from '@bigtest/interactor';
 
 export default createInteractor('section')({
   selector: 'section',
-  defaultLocator: element => element.id
+  defaultLocator: element => element.id,
+  locators: {
+    findByAttribute: element => element.getAttributeNames().find(attr => /data-test/.exec(attr))
+  }
 });
