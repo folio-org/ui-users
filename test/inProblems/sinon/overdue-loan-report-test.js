06c7de9ef7e6027f292acdbc8e0f0d9dbbda3cd0
minkimcello <minkimcello@gmail.com>
2020-09-23 19:34:00 -0400

    Sinon part 2

---
+++ test/inProblems/sinon/overdue-loan-report-test.js
@@ -0,0 +1,63 @@
+import { beforeEach, afterEach, describe, it } from '@bigtest/mocha';
+import { expect } from 'chai';
+import sinon from 'sinon';
+
+import setupApplication from '../helpers/setup-application';
+import UsersInteractor from '../interactors/users';
+
+describe('OverdueLoanReport', () => {
+  const users = new UsersInteractor();
+  let xhr;
+  let requests = [];
+
+  setupApplication();
+
+  describe('visit user-list', () => {
+    beforeEach(async function () {
+      this.server.createList('loan', 5, 'borrower');
+      this.visit('/users?sort=Name');
+      await users.whenLoaded();
+    });
+
+    describe('Show export to CSV', function () {
+      beforeEach(async function () {
+        xhr = sinon.useFakeXMLHttpRequest();
+        requests = [];
+        xhr.onCreate = function (req) { requests.push(req); };
+        await users.headerDropdown.click();
+        await users.headerDropdownMenu.clickExportToCSV();
+      });
+
+      afterEach(async function () {
+        await xhr.restore();
+      });
+
+      it('hides dropdown menu', () => {
+        expect(users.headerDropdownMenu.exportBtnIsVisible).to.be.false;
+      });
+
+      it('requests data', () => {
+        expect(requests.length).to.equal(1);
+      });
+    });
+
+    describe('Double-clicking the item does not download twice/clicking while download in progress', function () {
+      beforeEach(async function () {
+        xhr = sinon.useFakeXMLHttpRequest();
+        requests = [];
+        xhr.onCreate = function (req) { requests.push(req); };
+        await users.headerDropdown.click();
+        await users.headerDropdownMenu.clickExportToCSV();
+        await users.headerDropdownMenu.clickExportToCSV();
+      });
+
+      afterEach(async function () {
+        await xhr.restore();
+      });
+
+      it('request only happens once', () => {
+        expect(requests.length).to.equal(1);
+      });
+    });
+  });
+});
