a50d1410f1bdfad76415b6cc59a7e13319243b0a
minkimcello <minkimcello@gmail.com>
2020-09-24 13:27:42 -0400

    Overdue loan report test

--- test/integration/overdue-loan-report.test.js
+++ test/integration/overdue-loan-report.test.js
@@ -1,15 +1,17 @@
 import { App } from '@bigtest/interactor';
-import sinon from 'sinon';
 import test from '../helpers/base-steps/simulate-server';
 import { store } from '../helpers/server';
+
+import {
+  Button,
+  Div
+} from '../interactors';
 
 export default test('overdue loan report')
   .step('seed data', async () => {
     store.createList('loan', 5, 'borrower');
   })
   .step(App.visit('/users'))
-  // .step('sinon', async () => {
-  //   const xhr = sinon.useFakeXMLHttpRequest();
-  //   const requests = [];
-  //   xhr.onCreate = function (req) { requests.push(req); };
-  // })
+  .step(Button('Actions').click())
+  .step(Button('Overdue loans report (CSV)').click())
+  .assertion(Div.findByAttribute('data-test-callout-element').exists());
