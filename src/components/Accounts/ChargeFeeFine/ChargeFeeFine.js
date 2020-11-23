141dd11ad35468611a25fde8b0b6d43a39ff624e
Anna_Melnyk <anna_melnyk@epam.com>
2020-11-23 20:57:32 +0200

    UIU-1975: Fix empty payments select

--- src/components/Accounts/ChargeFeeFine/ChargeFeeFine.js
+++ src/components/Accounts/ChargeFeeFine/ChargeFeeFine.js
@@ -427,7 +427,6 @@ class ChargeFeeFine extends React.Component {
       }
     });
     const feefines = _.get(resources, ['allfeefines', 'records'], []);
-    const payments = _.get(resources, ['payments', 'records'], []).filter(p => p.ownerId === this.state.ownerId);
     const accounts = _.get(resources, ['accounts', 'records'], []);
     const settings = _.get(resources, ['commentRequired', 'records', 0], {});
     const barcode = _.get(resources, 'activeRecord.barcode');
@@ -464,6 +463,10 @@ class ChargeFeeFine extends React.Component {
     const selectedFeeFine = feefines.find(f => f.id === feeFineTypeId);
     const currentOwnerFeeFineTypes = feefines.filter(f => f.ownerId === resources.activeRecord.ownerId);
     const selectedOwner = owners.find(o => o.id === initialOwnerId);
+    const payments = _.get(resources, ['payments', 'records'], []).filter(
+      p => p.ownerId === this.state.ownerId || resources.activeRecord.ownerId
+    );
+
     const initialChargeValues = {
       ownerId: resources.activeRecord.ownerId || '',
       notify: !!(selectedFeeFine?.chargeNoticeId || selectedOwner?.defaultChargeNoticeId),
