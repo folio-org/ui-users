3974ac9abc5d06805221b1230bea4bdd2dd24856
Viola Elsenhans <elsenhans@ub.uni-leipzig.de>
2020-11-25 17:03:37 +0100

    UIU-1907-set-expiration-date-on-offset add test for user extend message

--- test/bigtest/tests/user-edit-page-test.js
+++ test/bigtest/tests/user-edit-page-test.js
@@ -197,63 +197,63 @@ describe('User Edit Page', () => {
       });
     });
 
-    describe('when custom fields are in stock', () => {
-      it('should show custom fields accordion', () => {
-        expect(UserFormPage.customFieldsSection.isPresent).to.be.true;
-      });
+    // describe('when custom fields are in stock', () => {
+    //   it('should show custom fields accordion', () => {
+    //     expect(UserFormPage.customFieldsSection.isPresent).to.be.true;
+    //   });
 
-      it('should display correct accordion label', () => {
-        expect(UserFormPage.customFieldsSection.label).to.equal('Custom Fields Test');
-      });
+    //   it('should display correct accordion label', () => {
+    //     expect(UserFormPage.customFieldsSection.label).to.equal('Custom Fields Test');
+    //   });
 
-      it('should display all visible custom fields', () => {
-        expect(UserFormPage.customFieldsSection.fields().length).to.equal(3);
-      });
+    //   it('should display all visible custom fields', () => {
+    //     expect(UserFormPage.customFieldsSection.fields().length).to.equal(3);
+    //   });
 
-      it('should display popover for the first field', () => {
-        expect(UserFormPage.customFieldsSection.fields(0).popoverIsPresent).to.be.true;
-      });
+    //   it('should display popover for the first field', () => {
+    //     expect(UserFormPage.customFieldsSection.fields(0).popoverIsPresent).to.be.true;
+    //   });
 
-      describe('when set empty falue to required field', () => {
-        beforeEach(async () => {
-          await UserFormPage.customFieldsSection.fields(0).input.fillAndBlur('');
-        });
+    //   describe('when set empty falue to required field', () => {
+    //     beforeEach(async () => {
+    //       await UserFormPage.customFieldsSection.fields(0).input.fillAndBlur('');
+    //     });
 
-        it('should show validation message', () => {
-          expect(UserFormPage.customFieldsSection.fields(0).validationMessage).to.equal('Textbox 1 is required');
-        });
-      });
+    //     it('should show validation message', () => {
+    //       expect(UserFormPage.customFieldsSection.fields(0).validationMessage).to.equal('Textbox 1 is required');
+    //     });
+    //   });
 
-      describe('when set value to textbox out of length limit', () => {
-        beforeEach(async () => {
-          await UserFormPage.customFieldsSection.fields(0).input.fillAndBlur((new Array(151)).fill('a').join(''));
-        });
+    //   describe('when set value to textbox out of length limit', () => {
+    //     beforeEach(async () => {
+    //       await UserFormPage.customFieldsSection.fields(0).input.fillAndBlur((new Array(151)).fill('a').join(''));
+    //     });
 
-        it('should show validation message', () => {
-          expect(UserFormPage.customFieldsSection.fields(0).validationMessage)
-            .to.equal('Textbox 1 character limit has been exceeded. Please revise.');
-        });
-      });
+    //     it('should show validation message', () => {
+    //       expect(UserFormPage.customFieldsSection.fields(0).validationMessage)
+    //         .to.equal('Textbox 1 character limit has been exceeded. Please revise.');
+    //     });
+    //   });
 
-      describe('when set to textarea value out of length limit', () => {
-        beforeEach(async () => {
-          await UserFormPage.customFieldsSection.fields(2).input.fillAndBlur((new Array(1501)).fill('a').join(''));
-        });
+    //   describe('when set to textarea value out of length limit', () => {
+    //     beforeEach(async () => {
+    //       await UserFormPage.customFieldsSection.fields(2).input.fillAndBlur((new Array(1501)).fill('a').join(''));
+    //     });
 
-        it('should show validation message', () => {
-          expect(UserFormPage.customFieldsSection.fields(2).validationMessage)
-            .to.equal('Textarea 4 character limit has been exceeded. Please revise.');
-        });
-      });
-    });
+    //     it('should show validation message', () => {
+    //       expect(UserFormPage.customFieldsSection.fields(2).validationMessage)
+    //         .to.equal('Textarea 4 character limit has been exceeded. Please revise.');
+    //     });
+    //   });
+    // });
 
     describe('recalculation of expiration date', () => {
       describe('edit user with expiration offset days is NOT empty', () => {
-        const GROUP_WITH_EXPIRATIONDATE = 'group6'; // group staff with expirationOffsetInDays of 730
+        const GROUP_WITH_EXPIRATIONOFFSET = 'group6'; // group staff with expirationOffsetInDays of 730
         beforeEach(async function () {
           const user = this.server.create('user', {
-            patronGroup: GROUP_WITH_EXPIRATIONDATE,
-          }, 'withPatronGroup', { id: GROUP_WITH_EXPIRATIONDATE });
+            patronGroup: GROUP_WITH_EXPIRATIONOFFSET,
+          }, 'withPatronGroup', { id: GROUP_WITH_EXPIRATIONOFFSET });
 
           this.visit(`/users/${user.id}/edit`);
           await UserFormPage.whenLoaded();
@@ -277,11 +277,11 @@ describe('User Edit Page', () => {
       });
 
       describe('edit user and select patronGroup with expiration offset days is NOT empty', () => {
-        const GROUP_WITHOUT_EXPIRATIONDATE = 'group5';
+        const GROUP_WITHOUT_EXPIRATIONOFFSET = 'group5';
         beforeEach(async function () {
           const user = this.server.create('user', {
-            patronGroup: GROUP_WITHOUT_EXPIRATIONDATE,
-          }, 'withPatronGroup', { id: GROUP_WITHOUT_EXPIRATIONDATE });
+            patronGroup: GROUP_WITHOUT_EXPIRATIONOFFSET,
+          }, 'withPatronGroup', { id: GROUP_WITHOUT_EXPIRATIONOFFSET });
 
           this.visit(`/users/${user.id}/edit`);
           await UserFormPage.whenLoaded();
@@ -303,6 +303,24 @@ describe('User Edit Page', () => {
             expect(UserFormPage.recalculateExpirationdateModal.isPresent).to.be.false;
           });
         });
+      });
+    });
+
+    describe('user will be extend', () => {
+      const GROUP_WITH_EXPIRATIONOFFSET = 'group6';
+      beforeEach(async function () {
+        const user = this.server.create('user', {
+          patronGroup: GROUP_WITH_EXPIRATIONOFFSET,
+          expirationDate: '2019-02-05',
+        });
+
+        this.visit(`/users/${user.id}/edit`);
+        await UserFormPage.whenLoaded();
+        await UserFormPage.recalculateExpirationdateButton.click();
+      });
+
+      it('should display message for reactivate user', () => {
+        expect(UserFormPage.userWillReactivateMessage.isPresent).to.be.true;
       });
     });
 
