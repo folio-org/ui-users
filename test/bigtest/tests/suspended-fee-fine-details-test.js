acc7ece80a61873448678e4982e364e9181403cd
iselagb <igarciab@dgb.unam.mx>
2021-01-13 18:18:54 -0600

    UIU-1166: Modify indentation

--- test/bigtest/tests/suspended-fee-fine-details-test.js
+++ test/bigtest/tests/suspended-fee-fine-details-test.js
@@ -1,7 +1,7 @@
 import {
-    beforeEach,
-    describe,
-    it,
+  beforeEach,
+  describe,
+  it,
 } from '@bigtest/mocha';
 import { expect } from 'chai';
 
@@ -11,29 +11,29 @@ import FeeFineHistoryInteractor from '../interactors/fee-fine-history';
 import FeeFineDetails from '../interactors/fee-fine-details';
 import { refundClaimReturned } from '../../../src/constants';
 
-describe.only('Test Suspended Fee/Fine details', () => {
-    setupApplication({
-        scenarios: ['suspended-fee-fine-details'],
-        currentUser: {
-            curServicePoint: { id: 1 },
-        },
+describe('Test Suspended Fee/Fine details', () => {
+  setupApplication({
+    scenarios: ['suspended-fee-fine-details'],
+    currentUser: {
+      curServicePoint: { id: 1 },
+    },
+  });
+  describe('visit Fee/fine details', () => {
+    beforeEach(async function () {
+      this.visit('/users/preview/ce0e0d5b-b5f3-4ad5-bccb-49c0784298fd');
+      await FeeFineHistoryInteractor.whenSectionLoaded();
+      await FeeFineHistoryInteractor.sectionFeesFinesSection.click();
+      await FeeFineHistoryInteractor.openAccounts.click();
+      await FeeFineHistoryInteractor.rows(3).click();
     });
-    describe('visit Fee/fine details', () => {
-        beforeEach(async function () {
-            this.visit('/users/preview/ce0e0d5b-b5f3-4ad5-bccb-49c0784298fd');
-            await FeeFineHistoryInteractor.whenSectionLoaded();
-            await FeeFineHistoryInteractor.sectionFeesFinesSection.click();
-            await FeeFineHistoryInteractor.openAccounts.click();
-            await FeeFineHistoryInteractor.rows(3).click();
-        });
 
-        it('displays account actions section', () => {
-            expect(FeeFineDetails.isPresent).to.be.true;
-        });
-
-        it('displays fee/fine details latest payment status', () => {
-            expect(FeeFineDetails.latestPaymentStatus.label.text).to.equal(translations['details.field.latest']);
-            expect(FeeFineDetails.latestPaymentStatus.value.text).to.string(refundClaimReturned.PAYMENT_STATUS);
-        });
+    it('displays account actions section', () => {
+      expect(FeeFineDetails.isPresent).to.be.true;
     });
+
+    it('displays fee/fine details latest payment status', () => {
+      expect(FeeFineDetails.latestPaymentStatus.label.text).to.equal(translations['details.field.latest']);
+      expect(FeeFineDetails.latestPaymentStatus.value.text).to.string(refundClaimReturned.PAYMENT_STATUS);
+    });
+  });
 });
