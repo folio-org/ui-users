55c5f1145a87448dad285a388ecb43766456e958
Viola Elsenhans <elsenhans@ub.uni-leipzig.de>
2021-02-18 08:51:03 +0100

    UIU-1916-add-dropdown-show-shortcuts reduce duplicated lines

--- src/commandsGeneral.js
+++ src/commandsGeneral.js
@@ -3,32 +3,26 @@ import { FormattedMessage } from 'react-intl';
 
 const commandsGeneral = [
   {
-    name: 'expandOrCollapseSection',
     label: (<FormattedMessage id="ui-users.shortcut.expandCollapse" />),
     shortcut: 'spacebar'
   },
   {
-    name: 'closeModal',
     label: (<FormattedMessage id="ui-users.shortcut.closeModal" />),
     shortcut: 'esc'
   },
   {
-    name: 'copy',
     label: (<FormattedMessage id="ui-users.shortcut.copy" />),
     shortcut: 'mod + c'
   },
   {
-    name: 'cut',
     label: (<FormattedMessage id="ui-users.shortcut.cut" />),
     shortcut: 'mod + x'
   },
   {
-    name: 'paste',
     label: (<FormattedMessage id="ui-users.shortcut.paste" />),
     shortcut: 'mod + v'
   },
   {
-    name: 'find',
     label: (<FormattedMessage id="ui-users.shortcut.find" />),
     shortcut: 'mod + f'
   }
