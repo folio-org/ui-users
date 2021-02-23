989068d3e776ad5ea194c4806518a04fa94481ef
Viola Elsenhans <elsenhans@ub.uni-leipzig.de>
2021-02-23 16:52:38 +0100

    UIU-1916-add-dropdown-show-shortcuts change class component in functional component

--- src/components/KeyboardShortcutsModal/KeyboardShortcutsModal.js
+++ src/components/KeyboardShortcutsModal/KeyboardShortcutsModal.js
@@ -12,74 +12,74 @@ import {
 import commands from '../../commands';
 import commandsGeneral from '../../commandsGeneral';
 
-class KeyboardShortcutsModal extends React.Component {
-  static propTypes = {
-    onClose: PropTypes.func.isRequired,
+function KeyboardShortcutsModal(props) {
+  const { onClose } = props;
+
+  const footer = (
+    <ModalFooter>
+      <Button
+        id="keyboard-shortcuts-modal-close"
+        onClick={onClose}
+      >
+        <FormattedMessage id="ui-users.blocks.closeButton" />
+      </Button>
+    </ModalFooter>
+  );
+
+  const allCommands = commands.concat(commandsGeneral);
+  const platform = window.navigator.platform;
+  const shortcutsData = [];
+  allCommands.forEach(key => {
+    let value = {};
+
+    if (platform.includes('Mac')) {
+      value = {
+        'action': key.label,
+        'shortcut': upperFirst(key.shortcut.replace('mod', 'cmd').replace('alt', 'Option')),
+      };
+    } else {
+      value = {
+        'action': key.label,
+        'shortcut': upperFirst(key.shortcut.replace('mod', 'ctrl')),
+      };
+    }
+    shortcutsData.push(value);
+  });
+
+  const columnMapping = {
+    action: <FormattedMessage id="ui-users.shortcut.action" />,
+    shortcut: <FormattedMessage id="ui-users.shortcut.shortcut" />,
   };
 
-  render() {
-    const footer = (
-      <ModalFooter>
-        <Button
-          id="keyboard-shortcuts-modal-close"
-          onClick={this.props.onClose}
-        >
-          <FormattedMessage id="ui-users.blocks.closeButton" />
-        </Button>
-      </ModalFooter>
-    );
+  const columnWidths = {
+    'action': '50%',
+    'shortcut': '50%',
+  };
 
-    const allCommands = commands.concat(commandsGeneral);
-    const platform = window.navigator.platform;
-    const shortcutsData = [];
-    allCommands.forEach(key => {
-      let value = {};
-
-      if (platform.includes('Mac')) {
-        value = {
-          'action': key.label,
-          'shortcut': upperFirst(key.shortcut.replace('mod', 'cmd').replace('alt', 'Option')),
-        };
-      } else {
-        value = {
-          'action': key.label,
-          'shortcut': upperFirst(key.shortcut.replace('mod', 'ctrl')),
-        };
-      }
-      shortcutsData.push(value);
-    });
-
-    const columnMapping = {
-      action: <FormattedMessage id="ui-users.shortcut.action" />,
-      shortcut: <FormattedMessage id="ui-users.shortcut.shortcut" />,
-    };
-
-    const columnWidths = {
-      'action': '50%',
-      'shortcut': '50%',
-    };
-
-    return (
-      <Modal
-        autosize
-        dismissible
-        footer={footer}
-        id="keyboard-shortcuts-modal"
-        label={<FormattedMessage id="ui-users.appMenu.keyboardShortcuts" />}
-        onClose={this.props.onClose}
-        open
-      >
-        <div>
-          <MultiColumnList
-            columnMapping={columnMapping}
-            columnWidths={columnWidths}
-            contentData={shortcutsData}
-            interactive={false}
-          />
-        </div>
-      </Modal>
-    );
-  }
+  return (
+    <Modal
+      autosize
+      dismissible
+      footer={footer}
+      id="keyboard-shortcuts-modal"
+      label={<FormattedMessage id="ui-users.appMenu.keyboardShortcuts" />}
+      onClose={onClose}
+      open
+    >
+      <div>
+        <MultiColumnList
+          columnMapping={columnMapping}
+          columnWidths={columnWidths}
+          contentData={shortcutsData}
+          interactive={false}
+        />
+      </div>
+    </Modal>
+  );
 }
+
+KeyboardShortcutsModal.propTypes = {
+  onClose: PropTypes.func.isRequired,
+};
 
 export default KeyboardShortcutsModal;
