84ad350cd069a587c062d6ead1d9744306d22d61
Mikita Siadykh <mikita_siadykh@epam.com>
2021-03-30 13:44:17 +0400

    update karma

--- karma.conf.js
+++ karma.conf.js
@@ -3,12 +3,12 @@ module.exports = config => config.set({
   client: {
     captureConsole: false,
     mocha: {
-      timeout: 20000
+      timeout: 100000
     },
   },
-  browserDisconnectTimeout: 20000,
+  browserDisconnectTimeout: 100000,
   browserDisconnectTolerance: 10,
-  browserNoActivityTimeout: 20000,
+  browserNoActivityTimeout: 100000,
   flags: [
     '--disable-gpu',
     '--no-sandbox'
