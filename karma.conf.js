89630932c65e1a4b22e2cd0082110bd4e5883873
Zak Burke <zburke@cornell.edu>
2021-05-21 15:49:10 -0400

    restore original test config
    BigTest bombs in CI as described in detail in UIIN-1461. Instead of
    trying to fix it, we're giving up and moving on with Jest/RTL. This PR
    will now be limited to functional changes and will include no test
    config changes.

--- karma.conf.js
+++ karma.conf.js
@@ -1,16 +1,6 @@
 /** @param {import('karma').Config} config */
 module.exports = config => config.set({
   client: {
-    captureConsole: false,
-    mocha: {
-      timeout: 100000
-    },
+    captureConsole: false
   },
-  browserDisconnectTimeout: 100000,
-  browserDisconnectTolerance: 10,
-  browserNoActivityTimeout: 100000,
-  flags: [
-    '--disable-gpu',
-    '--no-sandbox'
-  ],
 });
