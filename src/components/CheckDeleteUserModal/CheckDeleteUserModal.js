6d912d664ac83c51f53adaebde7460351e7a52fb
Viola Elsenhans <elsenhans@ub.uni-leipzig.de>
2021-05-31 18:37:46 +0200

    UIU-1971-delete-user-with-transactions-check change function to class

--- src/components/CheckDeleteUserModal/CheckDeleteUserModal.js
+++ src/components/CheckDeleteUserModal/CheckDeleteUserModal.js
@@ -7,60 +7,58 @@ import {
   ModalFooter,
 } from '@folio/stripes/components';
 
-function CheckDeleteUserModal({
-  onCloseModal,
-  open,
-  username,
-}) {
-  // const [showConfirmDelete, setShowConfirmDelete] = useState(false);
-  const [showCloseModal, setShowCloseModal] = useState(false);
+class CheckDeleteUserModal extends React.Component {
+  static propTypes = {
+    onCloseModal: PropTypes.func.isRequired,
+    open: PropTypes.bool,
+    username: PropTypes.string,
+  };
 
-  // const handleCloseModal = () => {
-  //   setShowCloseModal(false);
-  // };
+  render() {
+    const { onCloseModal, open, username } = this.props;
+    // const [showConfirmDelete, setShowConfirmDelete] = useState(false);
+    // const [showCloseModal, setShowCloseModal] = useState(false);
 
-  return (
-    <>
-      <Modal
-        id="delete-user-modal"
-        data-test-delete-user-modal
-        open={open}
-        label={<FormattedMessage id="ui-users.checkDelete" />}
-        footer={
-          <ModalFooter>
-            <Button
-              buttonStyle="danger"
-              id="delete-user-button"
-              // onClick={}
-            >
-              <FormattedMessage id="ui-users.yes" />
-            </Button>
-            <Button
-              id="close-delete-user-button"
-              onClick={() => {
-                setShowCloseModal(false);
-                onCloseModal();
-              }}
-            >
-              <FormattedMessage id="ui-users.no" />
-            </Button>
-          </ModalFooter>
-        }
-      >
-        <FormattedMessage
-          id="ui-users.checkDelete.confirmation"
-          values={{ name: username }}
-        />
-      </Modal>
-    </>
-  );
+    // const handleCloseModal = () => {
+    //   setShowCloseModal(false);
+    // };
+
+    return (
+      <>
+        <Modal
+          id="delete-user-modal"
+          data-test-delete-user-modal
+          open={open}
+          label={<FormattedMessage id="ui-users.checkDelete" />}
+          footer={
+            <ModalFooter>
+              <Button
+                buttonStyle="danger"
+                id="delete-user-button"
+                // onClick={}
+              >
+                <FormattedMessage id="ui-users.yes" />
+              </Button>
+              <Button
+                id="close-delete-user-button"
+                onClick={() => {
+                  // setShowCloseModal(false);
+                  onCloseModal();
+                }}
+              >
+                <FormattedMessage id="ui-users.no" />
+              </Button>
+            </ModalFooter>
+          }
+        >
+          <FormattedMessage
+            id="ui-users.checkDelete.confirmation"
+            values={{ name: username }}
+          />
+        </Modal>
+      </>
+    );
+  }
 }
-
-CheckDeleteUserModal.propTypes = {
-  // handlers: PropTypes.shape({}).isRequired,
-  onCloseModal: PropTypes.func.isRequired,
-  open: PropTypes.bool,
-  username: PropTypes.string,
-};
 
 export default injectIntl(CheckDeleteUserModal);
