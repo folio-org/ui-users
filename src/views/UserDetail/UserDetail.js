be1139f113e9b69a25feab961bd849a3febb05b6
Mariia Sychova <mariia_sychova@epam.com>
2021-07-13 18:07:43 +0300

    UIU-2210 Pass custom fields labels as props to ViewCustomFieldsRecord

--- src/views/UserDetail/UserDetail.js
+++ src/views/UserDetail/UserDetail.js
@@ -680,6 +680,8 @@ class UserDetail extends React.Component {
                   backendModuleName="users"
                   entityType="user"
                   customFieldsValues={customFields}
+                  customFieldsLabel="ui-users.custom.customFields"
+                  noCustomFieldsFoundLabel="ui-users.custom.noCustomFieldsFound"
                 />
                 <IfPermission perm="proxiesfor.collection.get">
                   <ProxyPermissions
