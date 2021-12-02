45afb86453be88cb87773c9cf8de7749eb21610a
Mathan Sivavel <89525723+Mathansivavelmurugesan@users.noreply.github.com>
2021-12-02 12:02:32 +0530

    UIU-2408
    * Initial commit
    
    * Added test case

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -80,6 +80,7 @@
 * Create Jest/RTL test for `PermissionsList`. Refs UIU-2374.
 * Create Jest/RTL test for `ProxyPermissions`. Refs UIU-2275.
 * Create Jest/RTL test for `UserPermissions`. Refs UIU-2303.
+* Create Jest/RTL test for `Owners`. Refs UIU-2408.
 
 ## [6.1.0](https://github.com/folio-org/ui-users/tree/v6.1.0) (2021-06-18)
 [Full Changelog](https://github.com/folio-org/ui-users/compare/v6.0.0...v6.1.0)
---
+++ src/settings/FeeFinesTable/Owners.test.js
@@ -0,0 +1,51 @@
+import renderWithRouter from 'helpers/renderWithRouter';
+import Owners from './Owners';
+
+jest.unmock('@folio/stripes/components');
+
+const renderOwners = (props) => renderWithRouter(<Owners {...props} />);
+
+describe('Owners component', () => {
+  it('Component must be rendered', () => {
+    const props = {
+      onChange: jest.fn(),
+      filterShared: true,
+      dataOptions: [
+        {
+          id: 'test1234',
+          owner: 'test'
+        },
+        {
+          id: 'test1256',
+          owner: 'Shared'
+        }
+      ]
+    };
+    renderOwners(props);
+    expect(document.querySelector('[ value="test1256"]')).toBeFalsy();
+    expect(document.querySelector('[ value="test1234"]')).toBeTruthy();
+  });
+  it('Checking Shared Owners', () => {
+    const props = {
+      onChange: jest.fn(),
+      filterShared: false,
+      dataOptions: [
+        {
+          id: 'test1256',
+          owner: 'Shared'
+        }
+      ]
+    };
+    renderOwners(props);
+    expect(document.querySelector('[ value="test1256"]')).toBeTruthy();
+  });
+  it('Check for empty data', () => {
+    const props = {
+      onChange: jest.fn(),
+      filterShared: false,
+      dataOptions: [],
+    };
+    renderOwners(props);
+    expect(renderOwners(props)).toBeTruthy();
+  });
+});
