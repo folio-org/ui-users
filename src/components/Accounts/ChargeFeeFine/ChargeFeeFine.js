36fe6a7180dd28a8fed462ba19ddae41869621d9
iselagb <igarciab@dgb.unam.mx>
2021-12-14 12:06:49 -0600

    UIU-1160 and UIU-2001 Modify how default Fee/Fine Owner is set on New Fee/Fine page and pay Fee/Fine modal

--- src/components/Accounts/ChargeFeeFine/ChargeFeeFine.js
+++ src/components/Accounts/ChargeFeeFine/ChargeFeeFine.js
@@ -427,6 +427,11 @@ class ChargeFeeFine extends React.Component {
       match: {
         params: { loanid },
       },
+      okapi: {
+        currentUser: {
+          curServicePoint,
+        }
+      },
     } = this.props;
     const {
       ownerId,
@@ -449,7 +454,7 @@ class ChargeFeeFine extends React.Component {
     const accounts = _.get(resources, ['accounts', 'records'], []);
     const settings = _.get(resources, ['commentRequired', 'records', 0], {});
     const barcode = _.get(resources, 'activeRecord.barcode');
-    const defaultServicePointId = _.get(resources, ['curUserServicePoint', 'records', 0, 'defaultServicePointId'], '-');
+    const defaultServicePointId = curServicePoint.id;
     const servicePointsIds = _.get(resources, ['curUserServicePoint', 'records', 0, 'servicePointsIds'], []);
     let selected = parseFloat(0);
     accounts.forEach(a => {
@@ -480,11 +485,11 @@ class ChargeFeeFine extends React.Component {
     const servicePointOwnerId = loadServicePoints({ owners: (shared ? owners : list), defaultServicePointId, servicePointsIds });
     const initialOwnerId = ownerId !== '0' ? ownerId : servicePointOwnerId;
     const selectedFeeFine = feefines.find(f => f.id === feeFineTypeId);
-    const currentOwnerFeeFineTypes = feefines.filter(f => f.ownerId === resources.activeRecord.ownerId || f.ownerId === resources.activeRecord.shared);
+    const currentOwnerFeeFineTypes = feefines.filter(f => f.ownerId === initialOwnerId || f.ownerId === resources.activeRecord.shared);
     const selectedOwner = owners.find(o => o.id === initialOwnerId);
 
     const initialChargeValues = {
-      ownerId: resources.activeRecord.ownerId || '',
+      ownerId: initialOwnerId,
       notify: !!(selectedFeeFine?.chargeNoticeId || selectedOwner?.defaultChargeNoticeId),
       feeFineId: '',
       amount: ''
