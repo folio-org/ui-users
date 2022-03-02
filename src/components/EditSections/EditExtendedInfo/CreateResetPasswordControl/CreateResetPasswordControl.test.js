5511a3a85e29a1b5adab780edf2062db705a2470
Mathan <mathansivavel_murugesan@epam.com>
2022-03-02 18:16:23 +0530

    Updated Test Case

--- src/components/EditSections/EditExtendedInfo/CreateResetPasswordControl/CreateResetPasswordControl.test.js
+++ src/components/EditSections/EditExtendedInfo/CreateResetPasswordControl/CreateResetPasswordControl.test.js
@@ -34,21 +34,22 @@ describe('CreateResetPasswordControl component', () => {
     renderCreateResetPasswordControl(propData(mockFunc));
     expect(screen.getByText('ui-users.extended.sendResetPassword')).toBeInTheDocument();
   });
-  it('If reset password renders on click', async () => {
+  it('If reset password must be open with copy link', async () => {
     const mockFunc = jest.fn(() => new Promise((resolve, _) => {
       resolve({ ok: true, link: 'bl-users/password-reset/link' });
     }));
     await waitFor(() => renderCreateResetPasswordControl(propData(mockFunc)));
-    await userEvent.click(screen.getByText('ui-users.extended.sendResetPassword'));
-    expect(screen.getByText('testemail@email.com')).toBeInTheDocument();
+    await waitFor(() => userEvent.click(screen.getByText('ui-users.extended.sendResetPassword')));
+    await waitFor(() => expect(screen.getByText('ui-users.extended.copyLink')).toBeInTheDocument());
   });
-  it('If it redirects after POST fails', async () => {
-    const mockFunc = jest.fn(() => new Promise((_, reject) => {
-      const error = 'Something Went Wrong';
-      reject(error);
-    }));
-    await waitFor(() => renderCreateResetPasswordControl(propData(mockFunc)));
-    await userEvent.click(screen.getByText('ui-users.extended.sendResetPassword'));
-    await screen.debug();
-  });
+  /* Can be uncommented after the  createResetpasswordControl modal logic is reworked. Should add an assertion at the end after the results */
+  // it('If it redirects after POST fails', async () => {
+  //   const mockFunc = jest.fn(() => new Promise((_, reject) => {
+  //     const error = 'Something Went Wrong';
+  //     reject(error);
+  //   }));
+  //   await waitFor(() => renderCreateResetPasswordControl(propData(mockFunc)));
+  //   await userEvent.click(screen.getByText('ui-users.extended.sendResetPassword'));
+  //   await screen.debug();
+  // });
 });
