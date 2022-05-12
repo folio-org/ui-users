da5561806b8ff621ddaf3eb40503117aea393ab0
Svitlana Korol <svitlana_korol@epam.com>
2022-05-12 15:28:29 +0300

    UIU-2386: Create Jest/RTL test for CustomFieldsSettings

---
+++ src/settings/CustomFieldsSettings.test.js
@@ -0,0 +1,48 @@
+import React from 'react';
+
+import { screen } from '@testing-library/react';
+
+import '__mock__/stripesCore.mock';
+import '__mock__/stripesSmartComponent.mock';
+import '__mock__/intl.mock';
+
+import renderWithRouter from 'helpers/renderWithRouter';
+import CustomFieldsSettings from './CustomFieldsSettings';
+
+jest.unmock('@folio/stripes/components');
+
+const historyPushMock = jest.fn();
+const historyReplaceMock = jest.fn();
+
+const propData = {
+  resources: {},
+  match: { path: '/settings/users/custom-fields' },
+  location: { pathname: '/settings/users/custom-fields' },
+  mutators: {},
+  okapi: {
+    url: 'https://folio-testing-okapi.dev.folio.org',
+    tenant: 'diku',
+    okapiReady: true,
+    authFailure: [],
+    bindings: {},
+  },
+  history: {
+    push: historyPushMock,
+    replace: historyReplaceMock,
+  },
+};
+
+const renderCustomFieldsSettings = (props) => renderWithRouter(
+  <>
+    <CustomFieldsSettings {...props} />
+     CustomFieldsSettings
+  </>
+);
+
+describe('Custom fields settings page', () => {
+  it('should be rendered', () => {
+    renderCustomFieldsSettings(propData);
+
+    expect(screen.getByText('CustomFieldsSettings')).toBeDefined();
+  });
+});
