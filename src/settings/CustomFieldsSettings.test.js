13e5ad032a70c103883ea058529766b160ea8aa3
Svitlana Korol <svitlana_korol@epam.com>
2022-05-13 15:18:49 +0300

    UIU-2386: added some tests

--- src/settings/CustomFieldsSettings.test.js
+++ src/settings/CustomFieldsSettings.test.js
@@ -1,8 +1,11 @@
 import React from 'react';
 
-import { screen } from '@testing-library/react';
+import '../../test/jest/__mock__';
 
-import renderWithRouter from 'helpers/renderWithRouter';
+import { screen } from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom';
+import renderWithRouter from '../../test/jest/helpers/renderWithRouter';
+
 import CustomFieldsSettings from './CustomFieldsSettings';
 
 jest.unmock('@folio/stripes/components');
@@ -10,9 +13,17 @@ jest.unmock('@folio/stripes/components');
 const historyPushMock = jest.fn();
 const historyReplaceMock = jest.fn();
 
+jest.mock('@folio/stripes/smart-components', () => ({
+  ViewCustomFieldsSettings: () => <div>ViewCustomFieldsSettings</div>,
+}));
+
+jest.mock('@folio/stripes/smart-components', () => ({
+  EditCustomFieldsSettings: () => <div>EditCustomFieldsSettings</div>,
+}));
+
 const propData = {
   resources: {},
-  match: { path: '/settings/users/custom-fields' },
+  match: { params: { }, path: '/settings/users/custom-fields', url: '/' },
   location: { pathname: '/settings/users/custom-fields' },
   mutators: {},
   okapi: {
@@ -22,23 +33,41 @@ const propData = {
     authFailure: [],
     bindings: {},
   },
+  stripes: {
+    hasPerm: jest.fn(() => true),
+    connect: jest.fn((component) => component),
+    logger: {
+      log: jest.fn(),
+    },
+  },
   history: {
     push: historyPushMock,
     replace: historyReplaceMock,
   },
 };
 
-const renderCustomFieldsSettings = (props) => renderWithRouter(
-  <>
-    <CustomFieldsSettings {...props} />
-     CustomFieldsSettings
-  </>
-);
+const renderCustomFieldsSettings = ({ initialEntries }) => {
+  renderWithRouter(
+    <MemoryRouter initialEntries={initialEntries}>
+      <CustomFieldsSettings
+        {...propData}
+      />
+    </MemoryRouter>,
+  );
+};
 
 describe('Custom fields settings page', () => {
   it('should be rendered', () => {
-    renderCustomFieldsSettings(propData);
+    renderCustomFieldsSettings('/settings/users/custom-fields');
+  });
 
-    expect(screen.getByText('CustomFieldsSettings')).toBeDefined();
+  it('should redirect on viewCustomFieldsSettings route', async () => {
+    renderCustomFieldsSettings(['/settings/users/custom-fields']);
+
+    const viewCustomFieldsSettings = await screen.findByText('/settings/users/custom-fields');
+    const viewCustomFieldsSettingsText = await screen.findByText(/ViewCustomFieldsSettings/);
+
+    expect(viewCustomFieldsSettings).toHaveLength(1);
+    expect(viewCustomFieldsSettingsText).toBeVisible();
   });
 });
