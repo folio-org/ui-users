ced9c76c90cba2fa0f8dcf79df2e2b85406cfdac
Priyanka <priyanka_terala@epam.com>
2022-05-26 19:35:22 +0530

    typo corrected

--- src/components/EditSections/EditExtendedInfo/DepartmentsNameEdit/DepartmentsNameEdit.test.js
+++ src/components/EditSections/EditExtendedInfo/DepartmentsNameEdit/DepartmentsNameEdit.test.js
@@ -58,7 +58,7 @@ describe('Given DepartmentsNameEdit', () => {
     expect(screen.getByText('ui-users.extended.department.add')).toBeInTheDocument();
   });
 
-  it('should render select compoenent after clicking "Add Department" button', () => {
+  it('should render select component after clicking "Add Department" button', () => {
     userEvent.click(screen.getByText('ui-users.extended.department.add'));
     expect(screen.queryByPlaceholderText(/ui-users.extended.department.default/i));
   });
