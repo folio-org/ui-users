0b82a96eef82730c5da20c4bd9457efc3066f5b0
VSnehalatha <snehalathav@ebsco.com>
2022-09-30 14:07:20 +0530

    PR changes

--- src/components/Accounts/ChargeFeeFine/ItemLookup.test.js
+++ src/components/Accounts/ChargeFeeFine/ItemLookup.test.js
@@ -1,24 +1,22 @@
-import { screen, cleanup } from '@testing-library/react';
+import { screen, render } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
-
-import renderWithRouter from 'helpers/renderWithRouter';
-import ItemLookup from './ItemLookup';
 
 import '__mock__/stripesCore.mock';
 import '__mock__/stripesSmartComponent.mock';
+
+import ItemLookup from './ItemLookup';
 
 jest.unmock('@folio/stripes/components');
 jest.unmock('@folio/stripes/smart-components');
 
 const onCloseMock = jest.fn();
 const onChangeItemMock = jest.fn();
-const onConfirmMock = jest.fn();
 
-const renderItemLookup = (props) => renderWithRouter(<ItemLookup {...props} />);
+const renderItemLookup = (props) => render(<ItemLookup {...props} />);
 
 const propData = {
   open: true,
-  items:[],
+  items:[{ id:'test1', barcode:'test', title:'test' }],
   onClose: onCloseMock,
   onChangeItem: onChangeItemMock,
 };
@@ -28,14 +26,22 @@ describe('Item Lookup component', () => {
     renderItemLookup(propData);
     expect(screen.getByText('ui-users.charge.itemLookup.modalLabel')).toBeInTheDocument();
   });
+
   it('Onclose modal check', () => {
     renderItemLookup(propData);
     userEvent.click(screen.getByText('ui-users.charge.itemLookup.cancel'));
     expect(onCloseMock).toHaveBeenCalled();
   });
-  it('OnConfirm modal check', () => {
+
+  it('Confirm is disabled until a row is selcted', () => {
     renderItemLookup(propData);
+    expect(screen.getByRole('button', { name: /confirm/ })).toBeDisabled();
+  });
+
+  it('Checking Confirm button click after selecting row', () => {
+    renderItemLookup(propData);
+    userEvent.click(document.querySelector('[data-row-inner="0"]'));
     userEvent.click(screen.getByText('ui-users.charge.itemLookup.confirm'));
-    expect(onConfirmMock).not.toHaveBeenCalled();
+    expect(onChangeItemMock).toHaveBeenCalled();
   });
 });
