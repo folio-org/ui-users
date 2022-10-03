648eafdcc9e50180d6427c09173e2fbccd1ef2a3
KetineniM <53168935+KetineniM@users.noreply.github.com>
2022-10-03 02:25:38 -0400

    fixing the lint issues and test cases in proxyviewlist

--- src/components/ProxyGroup/ProxyViewList/ProxyViewList.test.js
+++ src/components/ProxyGroup/ProxyViewList/ProxyViewList.test.js
@@ -1,5 +1,5 @@
 import { screen } from '@testing-library/react';
-import userEvent from '@testing-library/user-event';
+// import userEvent from '@testing-library/user-event';
 
 import renderWithRouter from 'helpers/renderWithRouter';
 import ProxyViewList from './ProxyViewList';
@@ -11,36 +11,39 @@ jest.unmock('@folio/stripes/components');
 
 const renderProxyViewList = (props) => renderWithRouter(<ProxyViewList {...props} />);
 const STRIPES = {
-    config: {},
-    hasPerm: jest.fn().mockReturnValue(true),
-  };
-const props = (data, name) => {
+  config: {},
+  hasPerm: jest.fn().mockReturnValue(true),
+};
+
+const props = (data, itemname) => {
   return {
     records: data,
-    itemComponent: jest.fn(),
-    name: name,
+    itemComponent: ({ record }) => <div>{record.name}</div>,
+    name: itemname,
     label: <div>Label</div>,
     stripes: STRIPES,
   };
 };
 
-  describe('Checking ProxyViewList ', () => {
-    it('if it renders', () => {
-      const data = [];
-      renderProxyViewList(props(data,""));
-      expect(screen.getByText('ui-users.permissions.noProxiesFound')).toBeTruthy();
-    });
-    it('checking sponsers', () => {
-        const data = [];
-        renderProxyViewList(props(data,"sponsors"));
-        expect(screen.getByText('ui-users.permissions.noSponsorsFound')).toBeTruthy();
-      });
-      it('checking non sponsers', () => {
-        const data = [];
-        renderProxyViewList(props(data,"sponsors1"));
-        expect(screen.getByText('ui-users.permissions.noProxiesFound')).toBeTruthy();
-      });
-      
-    
+describe('Checking ProxyViewList ', () => {
+  it('if it renders', () => {
+    const data = [];
+    renderProxyViewList(props(data, ''));
+    expect(screen.getByText('ui-users.permissions.noProxiesFound')).toBeTruthy();
   });
-
+  it('checking sponsors', () => {
+    const data = [];
+    renderProxyViewList(props(data, 'sponsors'));
+    expect(screen.getByText('ui-users.permissions.noSponsorsFound')).toBeTruthy();
+  });
+  it('checking non sponsors', () => {
+    const data = [];
+    renderProxyViewList(props(data, 'sponsors1'));
+    expect(screen.getByText('ui-users.permissions.noProxiesFound')).toBeTruthy();
+  });
+  it('Displaying items when items are not empty', () => {
+    const data = [{ name: 'apple' }, { name: 'banana' }];
+    renderProxyViewList(props(data, ''));
+    expect(screen.getByText('apple')).toBeInTheDocument();
+  });
+});
