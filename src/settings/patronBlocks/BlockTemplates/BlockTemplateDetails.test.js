2afcc798189410a2f5c95dd367db21c96646457b
cchitneedi <cchitneedi@ebsco.com>
2022-10-06 20:13:52 +0530

    lint errors cleared

--- src/settings/patronBlocks/BlockTemplates/BlockTemplateDetails.test.js
+++ src/settings/patronBlocks/BlockTemplates/BlockTemplateDetails.test.js
@@ -1,68 +1,54 @@
-import { screen, render } from "@testing-library/dom";
+import { screen, render } from '@testing-library/dom';
 import renderWithRouter from 'helpers/renderWithRouter';
 import { Form } from 'react-final-form';
+
 import { fireEvent } from '@testing-library/react';
-import BlockTemplateDetails from "./BlockTemplateDetails";
+import BlockTemplateDetails from './BlockTemplateDetails';
 
 jest.unmock('@folio/stripes/components');
+
 const onSubmit = jest.fn();
 
 const renderBlockTemplateDetails = (props) => renderWithRouter(<BlockTemplateDetails {...props} />);
 
+const props = { initialValues: { metadata: { id: 'mock-match-params-id', createdDate: '2021-11-22T03:38:19.279+00:00' }, renewals: 'true', name: 'blockTemplate Test', code: 'components' } };
 
+const props1 = { initialValues: { metadata: { id: 'mock-match-params-id' }, name: 'blockTemplate Test', code: 'components' } };
 
-
-const props = {
-  initialValues: 
-  {metadata: { id: 'mock-match-params-id', createdDate: '2021-11-22T03:38:19.279+00:00' } , renewals: 'true', name: 'blockTemplate Test', code: 'components'} 
-}
-
-const props1 = {
-  initialValues: {metadata: { id: 'mock-match-params-id'} , name: 'blockTemplate Test', code: 'components'} 
-}
-
-
-
-  
 describe('Render BlockTemplateDetails component with', () => {
-    it('Check for Template Information Accordian for created date', () => {
-      renderBlockTemplateDetails(props);
-      expect(screen.getByText('ViewMetaData')).toBeInTheDocument();
-    });
+  it('Check for Template Information Accordian for created date', () => {
+    renderBlockTemplateDetails(props);
+    expect(screen.getByText('ViewMetaData')).toBeInTheDocument();
+  });
 
-    it('Check for Template Information Accordian if date not present', () => {
-      renderBlockTemplateDetails(props1);
-      expect(screen.queryByText('ViewMetaData')).not.toBeInTheDocument();
-    });
+  it('Check for Template Information Accordian if date not present', () => {
+    renderBlockTemplateDetails(props1);
+    expect(screen.queryByText('ViewMetaData')).not.toBeInTheDocument();
+  });
 
-    it('Render BlockTemplate Name', () => {
-       renderBlockTemplateDetails(props);
-      
-      expect(screen.getByText('blockTemplate Test')).toBeInTheDocument();
-    });
+  it('Render BlockTemplate Name', () => {
+    renderBlockTemplateDetails(props);
+    expect(screen.getByText('blockTemplate Test')).toBeInTheDocument();
+  });
 
-    it('Render BlockTemplate Code', () => {
-     renderBlockTemplateDetails(props);
-      expect(screen.getByText('components')).toBeInTheDocument();
-    });
-    
-    it('Component must be rendered', () => {
-      renderBlockTemplateDetails(props);
-      expect(renderBlockTemplateDetails(props)).toBeTruthy();
-    });
+  it('Render BlockTemplate Code', () => {
+    renderBlockTemplateDetails(props);
+    expect(screen.getByText('components')).toBeInTheDocument();
+  });
 
-    it('Expand Button for template information', () => {
-      renderBlockTemplateDetails(props);
-      fireEvent.click(document.querySelector('[id="accordion-toggle-button-templateInformation"]'));
-      expect(renderBlockTemplateDetails(props)).toBeTruthy();
+  it('Component must be rendered', () => {
+    renderBlockTemplateDetails(props);
+    expect(renderBlockTemplateDetails(props)).toBeTruthy();
+  });
 
-    });
-    it('Component must be rendered', () => {
-      renderBlockTemplateDetails(props);
-      expect(renderBlockTemplateDetails(props)).toBeTruthy();
-    });
-    
+  it('Expand Button for template information', () => {
+    renderBlockTemplateDetails(props);
+    fireEvent.click(document.querySelector('[id="accordion-toggle-button-templateInformation"]'));
+    expect(renderBlockTemplateDetails(props)).toBeTruthy();
+  });
 
+  it('Component must be rendered', () => {
+    renderBlockTemplateDetails(props);
+    expect(renderBlockTemplateDetails(props)).toBeTruthy();
+  });
 });
-
-
