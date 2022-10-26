52cedfe31cd9a1a4f43eb5d742cdbca772b997d5
VSnehalatha <snehalathav@ebsco.com>
2022-10-26 13:13:16 +0530

    Lint corrections

--- src/views/UserSearch/Filters.test.js
+++ src/views/UserSearch/Filters.test.js
@@ -1,14 +1,8 @@
 import React from 'react';
 import { fireEvent, render, screen } from '@testing-library/react';
-import userEvent from '@testing-library/user-event';
-
-import { waitFor } from '@testing-library/react';
 import '../../../test/jest/__mock__/matchMedia.mock';
 
-import { statusFilter } from '../../constants';
-
 import Filters from './Filters';
-import { Select } from '@folio/stripes-components';
 
 jest.unmock('@folio/stripes/components');
 jest.unmock('@folio/stripes/smart-components');
@@ -23,7 +17,7 @@ jest.mock('@folio/stripes/smart-components', () => {
   };
 });
 
-const stateMock=jest.fn();
+const stateMock = jest.fn();
 const filterHandlers = {
   state: stateMock,
   checkbox: () => {},
@@ -32,7 +26,7 @@ const filterHandlers = {
   reset: () => {},
 };
 
-const onChangeMock=jest.fn();
+const onChangeMock = jest.fn();
 const renderFilters = (props) => render(<Filters {...props} />);
 const initialProps = {
   onChange: onChangeMock,
@@ -41,39 +35,28 @@ const initialProps = {
     formatMessage: jest.fn(),
   },
   resources: {
-    patronGroups:{ records: [{ group: 'grouptest', id: 'idtest'}]},
-    departments:{ records: [{ name: 'nametest', id: 'idtest1'}]},
-    tags: { records: [{ label: 'labeltest'}]},
+    patronGroups:{ records: [{ group: 'grouptest', id: 'idtest' }] },
+    departments:{ records: [{ name: 'nametest', id: 'idtest1' }] },
+    tags: { records: [{ label: 'labeltest' }] },
   },
   onChangeHandlers: filterHandlers,
   resultOffset: {
     replace: jest.fn(),
   },
 };
-  
+
 describe('Filters', () => {
-    /*beforeEach(() => {
-      render(
-        <Filters
-          {...initialProps}
-        />
-      );
-    });*/
-  
-  describe('Loss type filter', () => {
-    it('should render loss type accordion', () => {
-      renderFilters(initialProps);
-      //screen.debug(undefined, Infinity);
-      expect(screen.getByText('ui-users.status')).toBeTruthy();
-    });
+  it('Component render testing', () => {
+    renderFilters(initialProps);
+    expect(screen.getByText('ui-users.status')).toBeTruthy();
   });
 
   it('Checking on change handlers', () => {
     renderFilters(initialProps);
     fireEvent.click(screen.getByText('ui-users.filters.status.inactive'));
-    //screen.debug(undefined, Infinity);
     expect(stateMock).toHaveBeenCalled();
   });
+
   it('Checking clear Group', () => {
     renderFilters(initialProps);
     fireEvent.click(screen.getAllByLabelText('Clear selected filters for "ui-users.departments"')[0]);
@@ -95,4 +78,3 @@ describe('Filters', () => {
     expect(screen.getByText('ui-users.departments')).toBeInTheDocument();
   });
 });
-  
