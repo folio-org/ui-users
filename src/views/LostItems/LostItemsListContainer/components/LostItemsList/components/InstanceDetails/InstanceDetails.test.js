a0e76c70b1ceabb023b2e9a20d2168929a7cbffa
Artem_Blazhko <artem_blazhko@epam.com>
2022-11-23 11:42:50 +0200

    Increase code coverage of InstanceDetails.js

--- src/views/LostItems/LostItemsListContainer/components/LostItemsList/components/InstanceDetails/InstanceDetails.test.js
+++ src/views/LostItems/LostItemsListContainer/components/LostItemsList/components/InstanceDetails/InstanceDetails.test.js
@@ -3,42 +3,115 @@ import {
   render,
   screen,
 } from '@testing-library/react';
+import { effectiveCallNumber } from '@folio/stripes/util';
 
 import InstanceDetails, {
-    getISBN,
-    getEffectiveCallNumber,
+  getISBN,
+  renderISBN,
+  getEffectiveCallNumber,
 } from './InstanceDetails';
+import ShowLongContentInPopover from './components';
 import { ISBN_ID } from '../../../../../constants';
 
-describe('getISBN', () => {
-  const notISBNIdentifier = {
-    identifierTypeId: 'not_ISBN_ID',
-    identifierType: 'identifierTypeForISBN',
-    value: 'valueForISBN',
-  };
-  const ISBNIdentifier = {
-    identifierTypeId: ISBN_ID,
-    identifierType: 'identifierTypeForISBN',
-    value: 'valueForISBN',
-  };
+jest.mock('@folio/stripes/util', () => ({
+  effectiveCallNumber: jest.fn(),
+}));
+jest.mock('./components', () => jest.fn(() => null));
 
-  it('should not return identifier(s) when instanceIdentifiers are absence', () => {
-    expect(getISBN({})).toEqual([]);
+describe('InstanceDetails', () => {
+  describe('getISBN', () => {
+    const notISBNIdentifier = {
+      identifierTypeId: 'not_ISBN_ID',
+      identifierType: 'identifierTypeForISBN',
+      value: 'valueForISBN',
+    };
+    const ISBNIdentifier = {
+      identifierTypeId: ISBN_ID,
+      identifierType: 'identifierTypeForISBN',
+      value: 'valueForISBN',
+    };
+
+    it('should not return identifier(s) when instanceIdentifiers are absence', () => {
+      expect(getISBN({})).toEqual([]);
+    });
+
+    it('should not return identifier(s) when identifierTypeId does not match ISBN_ID', () => {
+      expect(getISBN({
+        instance: {
+          identifiers: [notISBNIdentifier],
+        },
+      })).toEqual([]);
+    });
+
+    it('should return identifier(s) when identifierTypeId match ISBN_ID', () => {
+      expect(getISBN({
+        instance: {
+          identifiers: [notISBNIdentifier, ISBNIdentifier],
+        },
+      })).toEqual([ISBNIdentifier]);
+    });
   });
 
-  it('should not return identifier(s) when identifierTypeId does not match ISBN_ID', () => {
-    expect(getISBN({
-      instance: {
-        identifiers: [notISBNIdentifier],
-      },
-    })).toEqual([]);
+  describe('renderISBN', () => {
+    it('should return correct data', () => {
+      const actualCostRecord = {
+        instance: {
+          identifiers: [{
+            identifierTypeId: ISBN_ID,
+            value: 'value',
+          }],
+        },
+      };
+      const result = renderISBN(actualCostRecord);
+
+      render(result);
+
+      expect(screen.getByText(/value/)).toBeInTheDocument();
+    });
   });
 
-  it('should return identifier(s) when identifierTypeId match ISBN_ID', () => {
-    expect(getISBN({
+  describe('getEffectiveCallNumber', () => {
+    it('should trigger "effectiveCallNumber" with correct data', () => {
+      const actualCostRecord = {
+        item: 'item',
+      };
+
+      getEffectiveCallNumber(actualCostRecord);
+
+      expect(effectiveCallNumber).toHaveBeenCalledWith(actualCostRecord.item);
+    });
+  });
+
+  describe('InstanceDetails component', () => {
+    const actualCostRecord = {
       instance: {
-        identifiers: [notISBNIdentifier, ISBNIdentifier],
+        title: 'title',
       },
-    })).toEqual([ISBNIdentifier]);
+      item: {
+        materialType: 'materialType',
+        loanType: 'loanType',
+      },
+    };
+
+    beforeEach(() => {
+      render(
+        <InstanceDetails
+          actualCostRecord={actualCostRecord}
+        />
+      );
+    });
+
+    it('should render "ShowLongContentInPopover" with correct props', () => {
+      const expectedProps = {
+        text: actualCostRecord.instance.title,
+        additionalText: actualCostRecord.item.materialType,
+      };
+
+      expect(ShowLongContentInPopover).toHaveBeenCalledWith(expect.objectContaining(expectedProps), {});
+    });
+
+    it('should render loan type information', () => {
+      expect(screen.getByText(/loanType/)).toBeInTheDocument();
+    });
   });
 });
