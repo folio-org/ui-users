c22accf33ba8d87c05adcc9296b6eaf273cd1e48
cchitneedi <cchitneedi@ebsco.com>
2022-11-29 18:24:30 +0530

    create Jest/RTL test for RequestPreferencesEdit.js

---
+++ src/components/EditSections/EditExtendedInfo/RequestPreferencesEdit/RequestPreferencesEdit.test.js
@@ -0,0 +1,99 @@
+import { render, screen, fireEvent, waitFor } from '@testing-library/react';
+import { Form } from 'react-final-form';
+import userEvent from '@testing-library/user-event';
+import PropTypes from 'prop-types';
+import { StripesContext } from '@folio/stripes-core/src/StripesContext';
+
+import '__mock__/stripesComponents.mock';
+
+import renderWithRouter from 'helpers/renderWithRouter';
+import { nullOrStringIsRequiredTypeValidator } from '../../../../customTypeValidators';
+
+import RequestPreferencesEdit from './RequestPreferencesEdit';
+
+jest.mock('react-redux', () => ({
+  connect: () => (ReactComponent) => ReactComponent,
+}));
+jest.unmock('@folio/stripes/components');
+jest.unmock('@folio/stripes/smart-components');
+
+const onSubmit = jest.fn();
+const setFieldMock = jest.fn();
+
+const renderRequestPreferencesEdit = (props) => {
+  const component = () => (
+    <>
+      <RequestPreferencesEdit {...props} />
+    </>
+  );
+  renderWithRouter(
+    <Form
+      id="form-user"
+      onSubmit={onSubmit}
+      render={component}
+    />
+  );
+};
+
+const props = {
+  accordionId: 'EditContactInfo',
+  expanded: true,
+  onToggle: jest.fn(),
+  setFieldValue: setFieldMock,
+  deliveryAvailable: true,
+  intl: { formatMessage : jest.fn() },
+  servicePoints: [
+    {
+      name: 'servicePointsName',
+      id: 'd003c876-90f1-40da-90bc-a5dd3dedb9c6a',
+      name2: 'servicenameDefault',
+      id2: ''
+    }
+  ],
+  addresses: [{
+    addressType: 'Industrial Zone'
+  }],
+  addressTypes: [{
+    addressType: 'Home',
+    id: '123123'
+  },
+  {
+    addressType: 'Work',
+    id: '123123132'
+  }],
+  id: '111999',
+  name: 'servicePointPickUp'
+};
+
+const renderRequestPreferencesEdit1 = () => renderWithRouter(<RequestPreferencesEdit {...props} />);
+
+describe('request preference point', () => {
+  it('Must be rendered request preferences', () => {
+    renderRequestPreferencesEdit(props);
+    screen.debug(undefined, Infinity);
+    expect(screen.getByText('ui-users.requests.preferences')).toBeTruthy();
+  });
+
+  it('Must be rendered', () => {
+    renderRequestPreferencesEdit(props);
+    expect(screen.getByText('ui-users.requests.selectDeliveryAddress')).toBeInTheDocument();
+  });
+
+  it('Must be rendered request null value', () => {
+    renderRequestPreferencesEdit(props);
+    screen.debug(undefined, Infinity);
+    expect(document.querySelector('ui-users.errors.missingRequiredField')).toBeNull();
+  });
+
+  it('Checkbox selection', async () => {
+    renderRequestPreferencesEdit(props);
+    userEvent.click(document.querySelector('[name="requestPreferences.holdShelf"]'));
+    userEvent.selectOptions(document.querySelector('[data-test-fulfillment-preference="true"]'), 'Hold Shelf');
+    expect(screen.findByDisplayValue('Hold Shelf')).toBeTruthy();
+  });
+
+  it('Service PointsWith Pickup Location', () => {
+    renderRequestPreferencesEdit(props);
+    expect(screen.findByLabelText('folio_users_service_points.records"')).toBeTruthy();
+  });
+});
