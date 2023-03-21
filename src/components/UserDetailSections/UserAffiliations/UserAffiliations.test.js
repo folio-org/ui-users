0da681f9fdf7e2fd5863af795485816d0fd0b70e
Yury Saukou <yury_saukou@epam.com>
2023-03-21 18:23:24 +0400

    UIU-2821 View a users affiliations accordion in the details pane (#2366)

---
+++ src/components/UserDetailSections/UserAffiliations/UserAffiliations.test.js
@@ -0,0 +1,51 @@
+import { screen } from '@testing-library/react';
+
+import renderWithRouter from 'helpers/renderWithRouter';
+
+import affiliations from '../../../../test/jest/fixtures/affiliations';
+import { useUserAffiliations } from '../../../hooks';
+import UserAffiliations from './UserAffiliations';
+
+jest.unmock('@folio/stripes/components');
+jest.mock(
+  '../../../hooks/useUserAffiliations',
+  () => jest.fn(() => ({ affiliations: [], totalRecords: 0 })),
+);
+
+const defaultProps = {
+  accordionId: 'affiliations',
+  expanded: true,
+  onToggle: jest.fn(),
+  userId: 'userId',
+};
+
+const renderPatronBlock = (props = {}) => renderWithRouter(
+  <UserAffiliations
+    {...defaultProps}
+    {...props}
+  />
+);
+
+describe('render UserAffiliations accordion component', () => {
+  beforeEach(() => {
+    useUserAffiliations
+      .mockClear()
+      .mockReturnValue({ affiliations, totalRecords: affiliations.length });
+  });
+
+  it('should render a list of user affiliations', () => {
+    renderPatronBlock();
+
+    affiliations.map(({ tenantName }) => {
+      return expect(screen.getByText(tenantName)).toBeInTheDocument();
+    });
+  });
+
+  it('should render primary affiliation list item with \'primary\' class', () => {
+    renderPatronBlock();
+
+    const primaryTenantListItem = screen.getByText(affiliations.find(({ isPrimary }) => isPrimary).tenantName);
+
+    expect(primaryTenantListItem).toHaveClass('primary');
+  });
+});
