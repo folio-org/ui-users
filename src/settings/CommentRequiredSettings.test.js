64471ff725b657f0cc307401817d272d2714488e
cchitneedi <100114810+cchitneedi@users.noreply.github.com>
2023-04-11 09:37:56 +0530

    Save button method changed

--- src/settings/CommentRequiredSettings.test.js
+++ src/settings/CommentRequiredSettings.test.js
@@ -1,14 +1,11 @@
 import React from 'react';
-import { screen } from '@testing-library/react';
-import '__mock__/stripesCore.mock';
-import renderWithRouter from 'helpers/renderWithRouter';
-
+import { screen, waitFor } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
-
+import renderWithRouter from 'helpers/renderWithRouter';
 import CommentRequiredSettings from './CommentRequiredSettings';
 
 jest.unmock('@folio/stripes/components');
-
+const mockPUT = jest.fn();
 const props = {
   mutator: {
     record: {
@@ -16,32 +13,37 @@ const props = {
     },
     commentRequired: {
       POST: jest.fn(),
-      PUT: jest.fn(),
+      PUT: mockPUT,
       GET: jest.fn().mockResolvedValue([{ id: 1 }])
     }
   },
   resources: {
     commentRequired: {
-      records: [{ paid: false,
-        waived: false,
-        refunded: false,
-        transferredManually: true }],
+      records: [
+        {
+          paid: false,
+          waived: false,
+          refunded: false,
+          transferredManually: true
+        }
+      ],
       hasLoaded: true,
-
     }
   }
 };
 
 const renderCommentRequiredSettings = () => renderWithRouter(<CommentRequiredSettings {...props} />);
 describe('CommentRequiredSettings', () => {
-  it('renders', () => {
+  it('Component should render correctly', () => {
     renderCommentRequiredSettings();
     expect(screen.getByText('ui-users.comment.title')).toBeInTheDocument();
-    const refunDropdown = screen.getAllByText('ui-users.comment.refunded');
-    expect(refunDropdown).toHaveLength(2);
+  });
+  it('PUT function to called on clicking save button', async () => {
+    renderCommentRequiredSettings();
     userEvent.selectOptions(screen.getByRole('combobox', { name: 'ui-users.comment.transferred' }), 'ui-users.no');
     userEvent.click(screen.getByRole('button', { name: 'ui-users.comment.save' }));
-    screen.debug();
-    expect(screen.getByRole('button', { name: 'ui-users.comment.save' })).toBeInTheDocument();
+    await waitFor(() => {
+      expect(mockPUT).toHaveBeenCalled();
+    });
   });
 });
