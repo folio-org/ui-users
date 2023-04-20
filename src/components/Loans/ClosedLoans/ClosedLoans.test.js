b0742ffcd84ca07ca606047852fc0ac0bb08df26
Priyanka <priyanka_terala@epam.com>
2023-04-20 18:25:55 +0530

    UIU-1631 - fix lint issue

--- src/components/Loans/ClosedLoans/ClosedLoans.test.js
+++ src/components/Loans/ClosedLoans/ClosedLoans.test.js
@@ -1,3 +1,4 @@
+/* eslint-disable import/no-extraneous-dependencies */
 import React from 'react';
 import { render, screen, fireEvent, waitFor } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
