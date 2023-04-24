5fb899ebbe82e367c3aae1cd2a67c4940d812ad6
Yury Saukou <yury_saukou@epam.com>
2023-04-24 16:45:42 +0400

    UIU-2800 enable useConsortium hook when consortia interface is enabled

--- src/hooks/useConsortium/useConsortium.js
+++ src/hooks/useConsortium/useConsortium.js
@@ -3,13 +3,19 @@ import { useQuery } from 'react-query';
 import {
   useNamespace,
   useOkapiKy,
+  useStripes,
 } from '@folio/stripes/core';
 
 import { CONSORTIA_API } from '../../constants';
 
 const useConsortium = () => {
+  const stripes = useStripes();
   const ky = useOkapiKy();
   const [namespace] = useNamespace({ key: 'consortium' });
+
+  const enabled = Boolean(
+    stripes.hasInterface('consortia') && stripes.hasPerm('consortia.consortium.collection.get')
+  );
 
   const { isLoading, data } = useQuery(
     [namespace],
@@ -23,6 +29,9 @@ const useConsortium = () => {
       }
 
       return Promise.resolve();
+    },
+    {
+      enabled,
     },
   );
 
