ab832c34cd09d3b1e6ea7c51f61d44378eee81d5
Priyanka <priyanka_terala@epam.com>
2023-05-05 18:55:04 +0530

    UIU-2860 - fix code smells

--- src/routes/UserSearchContainer.js
+++ src/routes/UserSearchContainer.js
@@ -208,7 +208,7 @@ class UserSearchContainer extends React.Component {
   }
 
   componentDidUpdate() {
-    const pg = (this?.props?.resources?.patronGroups || {}).records || [];
+    const pg = (this?.props?.resources?.patronGroups || {})?.records || [];
     if (pg.length) {
       const pgFilterConfig = filterConfig.find(group => group.name === 'pg');
       const oldValuesLength = pgFilterConfig.values.length;
