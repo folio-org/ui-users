88bf8e8a53c797646b8bd3afe056442c24148e3d
Zak Burke <zburke@ebsco.com>
2023-05-15 23:42:19 -0400

    test clean-up (#2458)
    * omit `screen.debug`
    * omit private paths to translations in test harnesses

--- test/jest/helpers/renderWithRouter.js
+++ test/jest/helpers/renderWithRouter.js
@@ -4,24 +4,6 @@ import { CalloutContext } from '@folio/stripes/core';
 import { Router } from 'react-router-dom';
 import { render } from '@testing-library/react';
 import { createMemoryHistory } from 'history';
-import componentsTranslations from '@folio/stripes-components/translations/stripes-components/en';
-import stripesCoreTranslations from '@folio/stripes-core/translations/stripes-core/en';
-
-const prefixKeys = (translations, prefix) => {
-  return Object
-    .keys(translations)
-    .reduce((acc, key) => (
-      {
-        ...acc,
-        [`${prefix}.${key}`]: translations[key],
-      }
-    ), {});
-};
-
-const translations = {
-  ...prefixKeys(componentsTranslations, 'stripes-components'),
-  ...prefixKeys(stripesCoreTranslations, 'stripes-core'),
-};
 
 let rtlApi;
 
@@ -33,7 +15,7 @@ const renderWithRouter = (children, options = {}) => {
       <CalloutContext.Provider value={{ sendCallout: () => { } }}>
         <IntlProvider
           locale="en"
-          messages={translations}
+          messages={{}}
         >
           {children}
         </IntlProvider>
