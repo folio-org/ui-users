3ce60f528860ed74586970abaa5f8bed72899f84
Mike Taylor <mike@indexdata.com>
2023-06-15 12:23:21 +0100

    UIU-2893 Use new WSAPI for adding patron/staff notes to loans (#2490)
    * Require `add-info` interface (from mod-circulation)
    * Major version bump due to new interface dependency

--- src/components/ModalContent/ModalContent.js
+++ src/components/ModalContent/ModalContent.js
@@ -49,15 +49,15 @@ class ModalContent extends React.Component {
     patronInfo: {
       type: 'okapi',
       fetch: false,
-      PUT: {
-        path: 'circulation/loans/!{loan.id}',
+      POST: {
+        path: 'circulation/loans/!{loan.id}/add-info',
       },
     },
     staffInfo: {
       type: 'okapi',
       fetch: false,
-      PUT: {
-        path: 'circulation/loans/!{loan.id}',
+      POST: {
+        path: 'circulation/loans/!{loan.id}/add-info',
       },
     },
     cancel: {
@@ -88,10 +88,10 @@ class ModalContent extends React.Component {
         POST: PropTypes.func.isRequired,
       }).isRequired,
       patronInfo: PropTypes.shape({
-        PUT: PropTypes.func.isRequired,
+        POST: PropTypes.func.isRequired,
       }).isRequired,
       staffInfo: PropTypes.shape({
-        PUT: PropTypes.func.isRequired,
+        POST: PropTypes.func.isRequired,
       }).isRequired,
       cancel: PropTypes.shape({
         POST: PropTypes.func.isRequired,
@@ -142,7 +142,6 @@ class ModalContent extends React.Component {
     const { additionalInfo } = this.state;
 
     const {
-      loan,
       loanAction,
       stripes: {
         user: {
@@ -159,9 +158,9 @@ class ModalContent extends React.Component {
     } = this.props;
 
     const isInfo = (loanAction === 'patronInfo' || loanAction === 'staffInfo');
-    const mutatorFunction = this.props.mutator[loanAction][isInfo ? 'PUT' : 'POST'];
+    const mutatorFunction = this.props.mutator[loanAction].POST;
     const requestData = isInfo ?
-      { ...loan, action: loanAction, actionComment: additionalInfo } :
+      { action: loanAction === 'patronInfo' ? 'patronInfoAdded' : 'staffInfoAdded', actionComment: additionalInfo } :
       { comment: additionalInfo };
 
     if (loanAction === loanActionMutators.CLAIMED_RETURNED) {
