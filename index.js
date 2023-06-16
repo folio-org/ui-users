81571c52a10380d16c2bdcdcd9a4b66983ba0d97
Yury Saukou <yury_saukou@epam.com>
2023-06-16 14:25:46 +0400

    Expose `<RenderPermissions>`, `<PermissionSetDetails>` and `<PermissionSetForm>` components for reuse

---
+++ index.js
@@ -0,0 +1,7 @@
+// app root
+export { default } from './src';
+
+// components
+export { default as RenderPermissions } from './src/components/RenderPermissions';
+export { default as PermissionSetDetails } from './src/settings/permissions/PermissionSetDetails';
+export { default as PermissionSetForm } from './src/settings/permissions/PermissionSetForm';
