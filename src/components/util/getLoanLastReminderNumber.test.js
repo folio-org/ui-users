ac0a1249a61982df119b582fdb4bcca6114e844c
Michal Kuklis <michal.kuklis@gmail.com>
2023-07-10 23:03:42 -0400

    UIU-2591: Add info about reminder fees to loan details screen

---
+++ src/components/util/getLoanLastReminderNumber.test.js
@@ -0,0 +1,28 @@
+import { FormattedMessage } from 'react-intl';
+import getLoanLastReminderNumber from './getLoanLastReminderNumber';
+
+describe('getLoanLastReminderNumber', () => {
+  it('should return null if lastReminderNumber is not present', () => {
+    const loan = {};
+    const result = getLoanLastReminderNumber(loan);
+    expect(result).toBeNull();
+  });
+
+  it('should return JSX containing info about the last reminder fee if lastReminderNumber is present', () => {
+    const loan = {
+      reminders: {
+        lastFeeBilled: {
+          number: 2,
+        },
+      },
+    };
+    const result = getLoanLastReminderNumber(loan);
+    const expected = (
+      <>
+        <br />
+        (<FormattedMessage id="ui-users.loans.lastReminderNumber" values={{ count: 2 }} />)
+      </>
+    );
+    expect(result).toEqual(expected);
+  });
+});
