c4f1632496eaf694ff23c992a9272d8cc9ae88f4
Alisher Musurmonov <alisher_musurmonov1@epam.com>
2023-07-24 17:30:40 +0500

    tests: add test case

--- src/components/AssignedMembers/hooks/useAssignedUsers/useAssignedUsers.test.js
+++ src/components/AssignedMembers/hooks/useAssignedUsers/useAssignedUsers.test.js
@@ -70,8 +70,35 @@ describe('useAssignedUsers', () => {
     useOkapiKy.mockClear().mockReturnValue(kyMock);
   });
 
+  it('should call useOkapiKy with the correct headers', () => {
+    const grantedToIds = [1, 2, 3];
+    const tenantId = 'tenant-123';
+    renderHook(() => useAssignedUsers({ grantedToIds, tenantId }));
+
+    expect(useOkapiKy).toHaveBeenCalled();
+    expect(kyMock.extend).toHaveBeenCalledWith({
+      hooks: {
+        beforeRequest: [expect.any(Function)],
+      },
+    });
+
+    const [beforeRequestHook] = kyMock.extend.mock.calls[0][0].hooks.beforeRequest;
+    const requestHeaders = {
+      set: jest.fn(),
+    };
+    const requestMock = {
+      headers: requestHeaders,
+    };
+
+    beforeRequestHook(requestMock);
+
+    expect(requestHeaders.set).toHaveBeenCalledWith('X-Okapi-Tenant', tenantId);
+  });
+
   it('should fetch assigned users', async () => {
     batchRequest.mockClear()
+      .mockImplementationOnce(() => mockGrantedToIds)
+      .mockImplementationOnce(() => mockData.data)
       .mockReturnValueOnce([{ userId: '1' }, { userId: '2' }])
       .mockReturnValueOnce([{ id: '1', patronGroup: 'pg1', personal: { firstName: 'firstName1', lastName: 'lastName1' } }, { id: '2', patronGroup: 'pg2', personal: { firstName: 'firstName2', lastName: 'lastName2' } }])
       .mockReturnValueOnce([{ id: 'pg1', group: 'group1' }, { id: 'pg2', group: 'group2' }]);
