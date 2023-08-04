2e1e507e2f2509e31dd8850332e88aebfd42b7e3
Alisher Musurmonov <alisher_musurmonov1@epam.com>
2023-08-04 12:36:23 +0500

    refactor: code snippet for finding added and removed users list

--- src/components/AssignedUsers/utils.js
+++ src/components/AssignedUsers/utils.js
@@ -1,26 +1,10 @@
 /* eslint-disable no-prototype-builtins */
-import { keyBy } from 'lodash';
+import { differenceBy } from 'lodash';
 
 // eslint-disable-next-line import/prefer-default-export
 export function findObjectDifferences(prevUsers = [], newUsers = []) {
-  const oldObject = keyBy(prevUsers, 'id');
-  const newObject = keyBy(newUsers, 'id');
-  const added = {};
-  const removed = {};
+  const added = differenceBy(newUsers, prevUsers, 'id');
+  const removed = differenceBy(prevUsers, newUsers, 'id');
 
-  // Check for added and modified properties
-  for (const key in newObject) {
-    if (!oldObject.hasOwnProperty(key)) {
-      added[key] = newObject[key];
-    }
-  }
-
-  // Check for removed properties
-  for (const key in oldObject) {
-    if (!newObject.hasOwnProperty(key)) {
-      removed[key] = oldObject[key];
-    }
-  }
-
-  return { added: Object.values(added), removed: Object.values(removed) };
+  return { added, removed };
 }
