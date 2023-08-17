01c5d8b69d354fe02c086db17dfb78dd4290820d
Priyanka <priyanka_terala@epam.com>
2023-08-17 12:25:33 +0530

    UIU-2921 - Fix failed tests in ConditionsSettings.test.js

--- src/settings/OwnerSettings.test.js
+++ src/settings/OwnerSettings.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { act, screen, waitFor } from '@folio/jest-config-stripes/testing-library/react';
+import { screen, waitFor } from '@folio/jest-config-stripes/testing-library/react';
 import userEvent from '@folio/jest-config-stripes/testing-library/user-event';
 
 import renderWithRouter from 'helpers/renderWithRouter';
