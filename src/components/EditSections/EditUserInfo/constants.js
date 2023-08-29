9cd2832238e2ceb301e76d99a6dcdcaacd8d0da7
Alisher Musurmonov <alisher_musurmonov1@epam.com>
2023-08-29 14:26:03 +0500

    refactor: remove unused constants file

--- src/components/EditSections/EditUserInfo/constants.js
+++
@@ -1,9 +0,0 @@
-export const USER_TYPES = {
-  staff: 'staff',
-  patron: 'patron',
-  shadow: 'shadow',
-};
-
-export const checkIfConsortiumEnabled = stripes => {
-  return stripes.hasInterface('consortia');
-};
