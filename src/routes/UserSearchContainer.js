6d7c9d1a195b25cf6b18ac04a5c5b365abcf372c
Priyanka <priyanka_terala@epam.com>
2023-11-09 11:30:54 +0530

    UIU-2995 - Update resourceData and queryParams in UserSearchContainer.js to escape special characters in tags filter

--- src/routes/UserSearchContainer.js
+++ src/routes/UserSearchContainer.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import {
+  cloneDeep,
   get,
   template,
 } from 'lodash';
@@ -34,8 +35,30 @@ const searchFields = [
 ];
 const compileQuery = template(`(${searchFields.join(' or ')})`, { interpolate: /%{([\s\S]+?)}/g });
 
+const escapeSpecialCharactersInTagFilters = (queryParams, resourceData) => {
+  const newResourceData = cloneDeep(resourceData);
+  let escapedFilters;
+
+  if (resourceData.query.filters) {
+    const filterArr = resourceData.query.filters.split(',');
+    escapedFilters = filterArr.map(f => {
+      let newF = f;
+      if (f.startsWith('tags.')) {
+        newF = f.replace(/["^*?\\]/g, c => '\\' + c);
+      }
+      return newF;
+    });
+    escapedFilters = escapedFilters.join(',');
+
+    newResourceData.query.filters = escapedFilters;
+    queryParams.filters = escapedFilters;
+  }
+  return newResourceData;
+};
+
 export function buildQuery(queryParams, pathComponents, resourceData, logger, props) {
   const customFilterConfig = buildFilterConfig(queryParams.filters);
+  const newResourceData = escapeSpecialCharactersInTagFilters(queryParams, resourceData);
 
   return makeQueryFunction(
     'cql.allRecords=1',
@@ -53,7 +76,7 @@ export function buildQuery(queryParams, pathComponents, resourceData, logger, pr
     },
     [...filterConfig, ...customFilterConfig],
     2,
-  )(queryParams, pathComponents, resourceData, logger, props);
+  )(queryParams, pathComponents, newResourceData, logger, props);
 }
 
 class UserSearchContainer extends React.Component {
