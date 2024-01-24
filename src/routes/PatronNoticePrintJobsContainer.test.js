c7e98e3508668799d5d656fb078512f4de6fea72
Michal Kuklis <michal.kuklis@gmail.com>
2024-01-23 21:06:23 -0500

    Add more tests

--- src/routes/PatronNoticePrintJobsContainer.test.js
+++ src/routes/PatronNoticePrintJobsContainer.test.js
@@ -1,17 +1,23 @@
-import React from 'react';
+import { waitFor, fireEvent } from '@folio/jest-config-stripes/testing-library/react';
 
 import renderWithRouter from 'helpers/renderWithRouter';
 import PatronNoticePrintJobsContainer from './PatronNoticePrintJobsContainer';
 
+
 jest.mock('../views/PatronNoticePrintJobs', () => {
-  return jest.fn(() => <div />);
+  return jest.fn(({ onClose }) => (<button type="button" data-testid="close-button" onClick={onClose}>Close</button>));
 });
 
-const mockResources = {
-  entries: {
-    records: [],
-  },
-};
+jest.mock('history', () => {
+  return {
+    createMemoryHistory: jest.fn(() => ({
+      push: jest.fn(),
+      location: {},
+      listen: jest.fn(),
+      goBack: jest.fn(),
+    })),
+  };
+});
 
 const mockMutator = {
   printingJob: {
@@ -20,16 +26,44 @@ const mockMutator = {
   },
 };
 
-const props = {
-  resources: mockResources,
-  mutator: mockMutator
+const mockResources = {
+  entries: {
+    records: [],
+  },
 };
 
-const renderPatronNoticePrintJobsContainer = () => renderWithRouter(<PatronNoticePrintJobsContainer {...props} />);
+const renderPatronNoticePrintJobsContainer = (extraProps) => renderWithRouter(
+  <PatronNoticePrintJobsContainer mutator={mockMutator} resources={mockResources} {...extraProps} />
+);
 
 describe('PatronNoticePrintJobsContainer', () => {
-  it('renders without crashing', () => {
+  it('should render PatronNoticePrintJobs', () => {
     const { container } = renderPatronNoticePrintJobsContainer();
     expect(container).toBeInTheDocument();
+  });
+
+  it('should go back if location state exists', async () => {
+    const { getByTestId, history } = renderPatronNoticePrintJobsContainer({
+      location: { state: { from: '/previous-page' } }
+    });
+
+    fireEvent.click(getByTestId('close-button'));
+
+    await waitFor(() => {
+      expect(history.goBack).toHaveBeenCalled();
+    });
+  });
+
+
+  it('should redirect to /users?sort=name if location state does not exist', async () => {
+    const { getByTestId, history } = renderPatronNoticePrintJobsContainer({
+      location: {}
+    });
+
+    fireEvent.click(getByTestId('close-button'));
+
+    await waitFor(() => {
+      expect(history.push).toHaveBeenCalled();
+    });
   });
 });
