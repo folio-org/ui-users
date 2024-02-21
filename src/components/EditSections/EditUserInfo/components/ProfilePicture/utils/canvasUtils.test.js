9d2bcc40ae24934ebd7e8bdd617bf0c2e7b83ed9
Priyanka <priyanka_terala@epam.com>
2024-02-21 14:35:34 +0530

    UIU-2974 - add unit tests

--- src/components/EditSections/EditUserInfo/components/ProfilePicture/utils/canvasUtils.test.js
+++ src/components/EditSections/EditUserInfo/components/ProfilePicture/utils/canvasUtils.test.js
@@ -1,4 +1,5 @@
-import { getRadianAngle, rotateSize } from './canvasUtils';
+import { getRadianAngle, rotateSize, getCroppedImg, getRotatedImage } from './canvasUtils';
+import { imageSrc } from './data/imageSrc';
 
 describe('canvasUtils', () => {
   test('getRadianAngle', () => {
@@ -7,5 +8,23 @@ describe('canvasUtils', () => {
 
   test('rotateSize', () => {
     expect(rotateSize(10, 10, 10)).toEqual({ width: 11.584559306791382, height: 11.584559306791382 });
+  });
+
+  test('getRotatedImage', async () => {
+    await expect(getRotatedImage(imageSrc, 0)).resolves.toBe(imageSrc);
+  });
+
+  test('getCroppedImg', async () => {
+    const pixelCrop = {
+      'width': 474,
+      'height': 474,
+      'x': 0,
+      'y': 9
+    };
+    const result = await getCroppedImg(imageSrc, pixelCrop, 45);
+    expect(result).toBe(expect.objectContaining({
+      size: 16660,
+      type: 'image/jpeg'
+    }));
   });
 });
