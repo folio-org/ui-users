75596e2706e29a89a5b96fd722eca196db6179b3
Priyanka <priyanka_terala@epam.com>
2024-02-21 15:14:57 +0530

    UIU-2974 - update unit tests

--- src/components/EditSections/EditUserInfo/components/ProfilePicture/utils/canvasUtils.test.js
+++ src/components/EditSections/EditUserInfo/components/ProfilePicture/utils/canvasUtils.test.js
@@ -10,19 +10,19 @@ describe('canvasUtils', () => {
     expect(rotateSize(10, 10, 10)).toEqual({ width: 11.584559306791382, height: 11.584559306791382 });
   });
 
-  test('getRotatedImage', async () => {
-    await expect(getRotatedImage(imageSrc, 0)).resolves.toBe(imageSrc);
+  test('getRotatedImage', () => {
+    expect(getRotatedImage(imageSrc, 0)).resolves.toBe(imageSrc);
   });
 
-  test('getCroppedImg', async () => {
+  test('getCroppedImg', () => {
     const pixelCrop = {
       'width': 474,
       'height': 474,
       'x': 0,
       'y': 9
     };
-    const result = await getCroppedImg(imageSrc, pixelCrop, 45);
-    expect(result).toBe(expect.objectContaining({
+
+    expect(getCroppedImg(imageSrc, pixelCrop, 45)).resolves.toBe(expect.objectContaining({
       size: 16660,
       type: 'image/jpeg'
     }));
