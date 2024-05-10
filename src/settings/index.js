8fdb72fc2403f88180a1dac12c25e2f69bc5c2af
aidynoJ <azhakizhanov@ebsco.com>
2024-05-10 16:53:19 +0500

    UIU-3105: hide permissionSets from settings/users nav pane if roles interface is present

--- src/settings/index.js
+++ src/settings/index.js
@@ -1,15 +1,24 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+import { useStripes } from '@folio/stripes/core';
 import sections from './sections';
 import SettingsPage from './components/SettingsPage';
 
 const Settings = ({ children, match }) => {
   const { path } = match;
+  const stripes = useStripes();
+
+  const [settingsGeneral, ...otherSections] = sections;
+
+  const getSettingSections = () => {
+    if (!stripes.hasInterface('roles')) return sections;
+    return [{ ...settingsGeneral, pages:settingsGeneral.pages.filter(page => page.route !== 'perms') }, ...otherSections];
+  };
 
   return (
     <>
-      <SettingsPage path={path} sections={sections} />
+      <SettingsPage path={path} sections={getSettingSections()} />
       {children}
     </>
   );
