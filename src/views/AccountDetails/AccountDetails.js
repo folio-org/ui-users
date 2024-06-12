ee8bedf6a9d2cd53d5b7f7814989724cad6e95d2
Artem Blazhko <artem_blazhko@epam.com>
2024-06-12 18:04:38 +0300

    Make unblocked fee-fine actions if item is claimed returned and loan is closed (#2705)

--- src/views/AccountDetails/AccountDetails.js
+++ src/views/AccountDetails/AccountDetails.js
@@ -43,6 +43,7 @@ import FeeFineReport from '../../components/data/reports/FeeFineReport';
 import {
   itemStatuses,
   refundClaimReturned,
+  loanStatuses,
 } from '../../constants';
 
 import css from './AccountDetails.css';
@@ -325,13 +326,14 @@ feeFineActions
       resources,
       itemDetails
     } = this.props;
-
+    const relatedLoan = (resources?.loans?.records || []).filter((loan) => loan.id === account.loanId);
     const feeFineActions = resources?.feefineactions?.records || [];
     const isAccountsPending = resources?.accounts?.isPending ?? true;
     const isActionsPending = resources?.accountActions?.isPending ?? true;
     const allFeeFineActions = resources?.feefineactions?.records || [];
-    const isClaimReturnedItem = (itemDetails?.statusItemName === itemStatuses.CLAIMED_RETURNED);
-
+    const isClaimReturnedItem = itemDetails?.statusItemName === itemStatuses.CLAIMED_RETURNED;
+    const isLoanOpened = relatedLoan[0]?.status?.name === loanStatuses.OPEN;
+    const isMenuButtonDisabled = isActionsPending || isAccountsPending || (isClaimReturnedItem && isLoanOpened);
     const disabled = account.remaining === 0;
     const buttonDisabled = !this.props.stripes.hasPerm('ui-users.feesfines.actions.all');
     const refundAllowed = isRefundAllowed(account, feeFineActions);
@@ -345,7 +347,7 @@ feeFineActions
           <Button
             id="payAccountActionsHistory"
             buttonStyle="dropdownItem"
-            disabled={disabled || buttonDisabled || isActionsPending || isAccountsPending || isClaimReturnedItem}
+            disabled={disabled || buttonDisabled || isMenuButtonDisabled}
             onClick={this.pay}
           >
             <Icon icon="cart">
@@ -355,7 +357,7 @@ feeFineActions
           <Button
             id="waiveAccountActionsHistory"
             buttonStyle="dropdownItem"
-            disabled={disabled || buttonDisabled || isActionsPending || isAccountsPending || isClaimReturnedItem}
+            disabled={disabled || buttonDisabled || isMenuButtonDisabled}
             onClick={this.waive}
           >
             <Icon icon="cancel">
@@ -365,7 +367,7 @@ feeFineActions
           <Button
             id="refundAccountActionsHistory"
             buttonStyle="dropdownItem"
-            disabled={!refundAllowed || buttonDisabled || isActionsPending || isAccountsPending || isClaimReturnedItem}
+            disabled={!refundAllowed || buttonDisabled || isMenuButtonDisabled}
             onClick={this.refund}
           >
             <Icon icon="replace">
@@ -375,7 +377,7 @@ feeFineActions
           <Button
             id="transferAccountActionsHistory"
             buttonStyle="dropdownItem"
-            disabled={disabled || buttonDisabled || isActionsPending || isAccountsPending || isClaimReturnedItem}
+            disabled={disabled || buttonDisabled || isMenuButtonDisabled}
             onClick={this.transfer}
           >
             <Icon icon="transfer">
@@ -385,7 +387,7 @@ feeFineActions
           <Button
             id="errorAccountActionsHistory"
             buttonStyle="dropdownItem"
-            disabled={disabled || buttonDisabled || isActionsPending || isAccountsPending || !cancelAllowed || isClaimReturnedItem}
+            disabled={disabled || buttonDisabled || !cancelAllowed || isMenuButtonDisabled}
             onClick={this.error}
           >
             <Icon icon="trash">
