3ecc8531ef13b10252ee1108b295d0d13e48a22a
Priyanka Terala <104053200+Terala-Priyanka@users.noreply.github.com>
2024-07-09 18:05:19 +0530

    UIU-3163 - Export user details for library card printing (#2715)
    * UIU-3159 - Add the 'Print Library Card' action in the Action Menu of User app
    
    * UIU-3159 - update change log md file
    
    * UIU-3163 - Export user details for library card printing
    
    * UIU-3163 - refine and add error handling
    
    * UIU-3163 - fix lint error and refine

--- src/components/PrintLibraryCardButton/PrintLibraryCardButton.test.js
+++ src/components/PrintLibraryCardButton/PrintLibraryCardButton.test.js
@@ -1,12 +1,40 @@
 import { screen, render } from '@folio/jest-config-stripes/testing-library/react';
+import userEvent from '@folio/jest-config-stripes/testing-library/user-event';
+
+import { exportToCsv } from '@folio/stripes/components';
+
 import PrintLibraryCardButton from './PrintLibraryCardButton';
+
+jest.mock('@folio/stripes/components', () => ({
+  ...jest.requireActual('@folio/stripes/components'),
+  exportToCsv: jest.fn(),
+  Icon: jest.fn((props) => (props && props.children ? props.children : <span />)),
+}));
 
 describe('PrintLibraryCard', () => {
   beforeEach(() => {
-    render(<PrintLibraryCardButton />);
+    const props = {
+      user: {
+        expirationDate: '2024-07-09T23:59:59.000+00:00',
+        personal: {
+          firstName: 'firstName',
+          lastName: 'lastName',
+        },
+        patronGroup: 'patronGroupId'
+      },
+      patronGroup: 'patronGroup',
+    };
+    render(<PrintLibraryCardButton {...props} />);
   });
 
   it('should display print library card button', () => {
     expect(screen.getByText('ui-users.printLibraryCard')).toBeInTheDocument();
+  });
+
+  it('should export CSV file', async () => {
+    const printLibraryCardButton = screen.getByTestId('print-library-card');
+    await userEvent.click(printLibraryCardButton);
+
+    expect(exportToCsv).toHaveBeenCalled();
   });
 });
