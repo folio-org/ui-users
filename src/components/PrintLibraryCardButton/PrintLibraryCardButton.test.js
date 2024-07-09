83183e6edfeabb52d9fdf365a3a1043d4956fc0d
Priyanka Terala <priyanka_terala@epam.com>
2024-07-09 17:19:17 +0530

    UIU-3160 - Export profile picture for library card printing

--- src/components/PrintLibraryCardButton/PrintLibraryCardButton.test.js
+++ src/components/PrintLibraryCardButton/PrintLibraryCardButton.test.js
@@ -1,7 +1,9 @@
-import { screen, render } from '@folio/jest-config-stripes/testing-library/react';
+import { screen, render, waitFor } from '@folio/jest-config-stripes/testing-library/react';
 import userEvent from '@folio/jest-config-stripes/testing-library/user-event';
+import ppData from 'fixtures/userProfilePicture';
 
 import { exportToCsv } from '@folio/stripes/components';
+import { useProfilePicture } from '@folio/stripes/smart-components';
 
 import PrintLibraryCardButton from './PrintLibraryCardButton';
 
@@ -11,30 +13,192 @@ jest.mock('@folio/stripes/components', () => ({
   Icon: jest.fn((props) => (props && props.children ? props.children : <span />)),
 }));
 
+jest.mock('@folio/stripes/smart-components', () => ({
+  ...jest.requireActual('@folio/stripes/smart-components'),
+  useProfilePicture: jest.fn(),
+}));
+
 describe('PrintLibraryCard', () => {
-  beforeEach(() => {
-    const props = {
-      user: {
-        expirationDate: '2024-07-09T23:59:59.000+00:00',
-        personal: {
-          firstName: 'firstName',
-          lastName: 'lastName',
+  describe('when user is active and type is patron or staff', () => {
+    describe('when user do not have profile picture', () => {
+      beforeEach(() => {
+        useProfilePicture
+          .mockClear()
+          .mockReturnValue({ profilePictureData: 'profilePictureData' });
+
+        const props = {
+          user: {
+            expirationDate: '2024-07-09T23:59:59.000+00:00',
+            active: true,
+            type: 'staff',
+            personal: {
+              firstName: 'firstName',
+              lastName: 'lastName',
+            },
+            patronGroup: 'patronGroupId'
+          },
+          patronGroup: 'patronGroup',
+        };
+        render(<PrintLibraryCardButton {...props} />);
+      });
+
+      it('should display print library card button', () => {
+        expect(screen.getByText('ui-users.printLibraryCard')).toBeInTheDocument();
+      });
+
+      it('should export CSV file', async () => {
+        const printLibraryCardButton = screen.getByTestId('print-library-card');
+        await userEvent.click(printLibraryCardButton);
+
+        await waitFor(() => expect(exportToCsv).toHaveBeenCalled());
+      });
+    });
+
+    describe('when user has profile picture - uuid for profile picture link in user personal details', () => {
+      beforeEach(() => {
+        useProfilePicture
+          .mockClear()
+          .mockReturnValue({ profilePictureData: ppData.ppbase64String });
+
+        const props = {
+          user: {
+            expirationDate: '2024-07-09T23:59:59.000+00:00',
+            active: true,
+            type: 'staff',
+            personal: {
+              firstName: 'firstName',
+              lastName: 'lastName',
+              profilePictureLink: '02128bad-5852-4907-8714-3ca780124a21',
+            },
+            patronGroup: 'patronGroupId'
+          },
+          patronGroup: 'patronGroup',
+        };
+        render(<PrintLibraryCardButton {...props} />);
+      });
+
+      it('should display print library card button', () => {
+        expect(screen.getByText('ui-users.printLibraryCard')).toBeInTheDocument();
+      });
+
+      it('should export CSV file', async () => {
+        const printLibraryCardButton = screen.getByTestId('print-library-card');
+        await userEvent.click(printLibraryCardButton);
+
+        await waitFor(() => expect(exportToCsv).toHaveBeenCalled());
+      });
+
+      it('should export user pp jpg file', async () => {
+        global.URL.createObjectURL = jest.fn();
+        global.Blob = jest.fn();
+        global.navigator.msSaveBlob = jest.fn();
+        const spy = jest.spyOn(global.navigator, 'msSaveBlob');
+
+        const printLibraryCardButton = screen.getByTestId('print-library-card');
+        await userEvent.click(printLibraryCardButton);
+
+
+        await waitFor(() => expect(spy).toHaveBeenCalled());
+
+        spy.mockRestore();
+      });
+    });
+
+    describe('when user has profile picture - url for profile picture link in user personal details', () => {
+      beforeEach(() => {
+        useProfilePicture
+          .mockClear()
+          .mockReturnValue({ profilePictureData: 'profilePictureData' });
+
+        const props = {
+          user: {
+            expirationDate: '2024-07-09T23:59:59.000+00:00',
+            active: true,
+            type: 'staff',
+            personal: {
+              firstName: 'firstName',
+              lastName: 'lastName',
+              profilePictureLink: 'profilePictureLink',
+            },
+            patronGroup: 'patronGroupId'
+          },
+          patronGroup: 'patronGroup',
+        };
+        render(<PrintLibraryCardButton {...props} />);
+      });
+
+      it('should display print library card button', () => {
+        expect(screen.getByText('ui-users.printLibraryCard')).toBeInTheDocument();
+      });
+
+      it('should export CSV file', async () => {
+        const printLibraryCardButton = screen.getByTestId('print-library-card');
+        await userEvent.click(printLibraryCardButton);
+
+        await waitFor(() => expect(exportToCsv).toHaveBeenCalled());
+      });
+
+      it('should export user pp jpg file', async () => {
+        global.fetch = jest.fn(() => Promise.resolve({
+          ok: true,
+          blob: () => Promise.resolve(new Blob(['image data'], { type: 'image/jpeg' })),
+        }));
+        global.URL.createObjectURL = jest.fn();
+        global.Blob = jest.fn();
+        global.navigator.msSaveBlob = jest.fn();
+        const spy = jest.spyOn(global.navigator, 'msSaveBlob');
+
+        const printLibraryCardButton = screen.getByTestId('print-library-card');
+        await userEvent.click(printLibraryCardButton);
+
+
+        await waitFor(() => expect(spy).toHaveBeenCalled());
+
+        spy.mockRestore();
+      });
+    });
+  });
+
+  describe('when user is not active or type is other than patron or staff', () => {
+    beforeEach(() => {
+      useProfilePicture
+        .mockClear()
+        .mockReturnValue({ profilePictureData: 'profilePictureData' });
+    });
+
+    it('should disable printLibraryCardButton', () => {
+      const props = {
+        user: {
+          expirationDate: '2024-07-09T23:59:59.000+00:00',
+          active: false,
+          type: 'staff',
+          personal: {
+            firstName: 'firstName',
+            lastName: 'lastName',
+          },
+          patronGroup: 'patronGroupId'
         },
-        patronGroup: 'patronGroupId'
-      },
-      patronGroup: 'patronGroup',
-    };
-    render(<PrintLibraryCardButton {...props} />);
-  });
+        patronGroup: 'patronGroup',
+      };
+      render(<PrintLibraryCardButton {...props} />);
+      expect(screen.getByTestId('print-library-card')).toBeDisabled();
+    });
 
-  it('should display print library card button', () => {
-    expect(screen.getByText('ui-users.printLibraryCard')).toBeInTheDocument();
-  });
-
-  it('should export CSV file', async () => {
-    const printLibraryCardButton = screen.getByTestId('print-library-card');
-    await userEvent.click(printLibraryCardButton);
-
-    expect(exportToCsv).toHaveBeenCalled();
+    it('should disable printLibraryCardButton', () => {
+      const props = {
+        user: {
+          expirationDate: '2024-07-09T23:59:59.000+00:00',
+          active: true,
+          personal: {
+            firstName: 'firstName',
+            lastName: 'lastName',
+          },
+          patronGroup: 'patronGroupId'
+        },
+        patronGroup: 'patronGroup',
+      };
+      render(<PrintLibraryCardButton {...props} />);
+      expect(screen.getByTestId('print-library-card')).toBeDisabled();
+    });
   });
 });
--- src/components/util/util.js
+++ src/components/util/util.js
@@ -250,3 +250,14 @@ export const getReadingRoomSortedData = (e, meta, { data, order, direction }) =>
     direction: newSortDirection
   };
 };
+
+// Function to convert a Base64 string to a Blob
+export const base64ToBlob = (base64, mimeType) => {
+  const byteCharacters = atob(base64);
+  const byteNumbers = new Array(byteCharacters.length);
+  for (let i = 0; i < byteCharacters.length; i++) {
+    byteNumbers[i] = byteCharacters.charCodeAt(i);
+  }
+  const byteArray = new Uint8Array(byteNumbers);
+  return new Blob([byteArray], { type: mimeType });
+};
