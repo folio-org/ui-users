d8847c60672086b1d52c86cc311a7a690c2b6fc0
aidynoJ <azhakizhanov@ebsco.com>
2024-07-11 13:27:35 +0500

    add docs to useAllRolesData hook

--- src/hooks/useAllRolesData/useAllRolesData.js
+++ src/hooks/useAllRolesData/useAllRolesData.js
@@ -2,6 +2,17 @@ import { useNamespace, useOkapiKy, useStripes } from '@folio/stripes/core';
 import { useMemo } from 'react';
 import { useQuery } from 'react-query';
 
+/**
+ * Custom React hook that fetches all roles data from the backend API.
+ *
+ * @return {Object} An object containing the fetched data, loading state, and a map structure of all roles.
+ *  - data: An object containing `roles` and `totalRecords` fields, `roles` which is an array of roles. And `totalRecords` field, which is the total number of roles.
+ *  - isLoading: A boolean indicating if the data is currently being fetched.
+ *  - allRolesMapStructure: A Map object containing all roles, where the key is the role ID and the value is the role object.
+ * Introduced this structure because iterating over allRoles data is expensive in nested loops. @example UserRolesModal, EditUserRoles.
+ *  - isSuccess: A boolean indicating if the data fetch was successful.
+ */
+
 function useAllRolesData() {
   const stripes = useStripes();
   const ky = useOkapiKy();
