cd1cd3a1a88c0e86d27020496557e84cbbdac76e
aidynoJ <121284650+aidynoJ@users.noreply.github.com>
2024-07-16 16:35:44 +0500

    UIU-3145-followup: use react-final-form OnChange listener to keep track actual data of assignedRoleIds (#2723)
    Refs UIU-3145.

--- src/views/UserEdit/UserForm.js
+++ src/views/UserEdit/UserForm.js
@@ -475,7 +475,7 @@ class UserForm extends React.Component {
                           setButtonRef={this.setButtonRef}
                         /> : <EditUserRoles
                           form={form}
-                          assignedRoleIds={form.getFieldState('assignedRoleIds')?.value || []}
+                          initialValues={initialValues}
                           accordionId="userRoles"
                         />
                       }
