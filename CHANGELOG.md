7889b7dd184da4a4e3759668e66d8d2c81cb58ab
Priyanka Terala <104053200+Terala-Priyanka@users.noreply.github.com>
2024-10-17 07:06:19 +0530

    UIU-3219 - navigate to pre patron registrations page and  render list (#2755)
    * UIU-3219 - navigate to pre patron registrations page  and  render list
    
    * upgrade upload artifact version
    
    * cleanup
    
    * update unit test
    
    * UIU-3219 - update tests
    
    * UIU-3219 - update records count
    
    * UIU-3219 - add interface dependency, fix review comments
    
    * UIU-3219 - update query to fetch only 'TIER-2' records
    
    * UIU-3219 - update test data for staging records with 'TIER-2' status
    
    * UIU-3219 - update chanelog to reflect breaking change
    
    * UIU-3221 - create new permission 'Users: Can view patron preregistration data'
    
    * UIU-3219 - update tests for PatronPreRegistrationRecordsContainer
    
    * UIU-3219 - update sub permission of permission 'Users: Can view patron preregistration data'
    
    * UIU-3219 - fix rveiew comments
    
    * UIU-3219 - fix lint issue

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -61,6 +61,9 @@
 * Update `notes` to `v4.0`. Refs UIU-3229.
 * Loan details: Print Due date receipt. Refs UIU-3210.
 * Open loans: `isDcbItem()` must tolerate sparse data. Refs UIU-3230.
+* *BREAKING* Add new okapi interface staging-users. Add action button for searching patrons pre registration records. Refs UIU-3219.
+* Display patrons pre registration records results. Refs UIU-3222
+* Create new permission 'Users: Can view patron preregistration data'. Refs UIU-3221.
 
 ## [10.1.2](https://github.com/folio-org/ui-users/tree/v10.1.2) (2024-09-05)
 [Full Changelog](https://github.com/folio-org/ui-users/compare/v10.1.1...v10.1.2)
---
+++ src/routes/PatronPreRegistrationRecordsContainer.js
@@ -0,0 +1,96 @@
+import PropTypes from 'prop-types';
+import { useHistory } from 'react-router-dom';
+import { get } from 'lodash';
+
+import { stripesConnect } from '@folio/stripes/core';
+import {
+  makeQueryFunction,
+  StripesConnectedSource,
+} from '@folio/stripes/smart-components';
+import PatronsPreRegistrationListContainer from '../views/PatronsPreRegistrationListContainer/PatronsPreRegistrationListContainer';
+
+import { PATRON_PREREGISTRATION_RECORDS_NAME } from '../constants';
+
+const RESULT_COUNT_INCREMENT = 100;
+const PAGE_AMOUNT = 100;
+
+const PatronPreRegistrationRecordsContainer = ({
+  resources,
+  mutator,
+  stripes,
+}) => {
+  const history = useHistory();
+  const source = new StripesConnectedSource({ resources, mutator }, stripes.logger, PATRON_PREREGISTRATION_RECORDS_NAME);
+  const data = get(resources, `${PATRON_PREREGISTRATION_RECORDS_NAME}.records`, []);
+
+  const queryGetter = () => {
+    return get(resources, 'query', {});
+  };
+
+  const onNeedMoreData = (askAmount, index) => {
+    const { resultOffset } = mutator;
+
+    if (source) {
+      if (resultOffset && index >= 0) {
+        source.fetchOffset(index);
+      } else {
+        source.fetchMore(RESULT_COUNT_INCREMENT);
+      }
+    }
+  };
+
+  const onClose = () => {
+    history.push('/users?sort=name');
+  };
+
+  return (
+    <PatronsPreRegistrationListContainer
+      onClose={onClose}
+      queryGetter={queryGetter}
+      onNeedMoreData={onNeedMoreData}
+      source={source}
+      data={data}
+      stripes={stripes}
+    />
+  );
+};
+
+PatronPreRegistrationRecordsContainer.manifest = {
+  query: { initialValue: {} },
+  resultCount: { initialValue: 0 },
+  resultOffset: { initialValue: 0 },
+  [PATRON_PREREGISTRATION_RECORDS_NAME]: {
+    type: 'okapi',
+    records: 'staging_users',
+    resultOffset: '%{resultOffset}',
+    resultDensity: 'sparse',
+    perRequest: PAGE_AMOUNT,
+    path: 'staging-users',
+    GET: {
+      params: {
+        query: makeQueryFunction(
+          'cql.allRecords=1',
+          '(keywords="%{query.query}*") AND status == "TIER-2"',
+          {
+            'firstName': 'personal.firstName',
+            'lastName': 'personal.lastName',
+            'middleName': 'personal.middleName',
+            'preferredFirsName': 'personal.preferredFirstName',
+            'email': 'personal.email',
+          },
+          '',
+          2
+        ),
+      },
+      staticFallback: { params: {} },
+    },
+  }
+};
+
+PatronPreRegistrationRecordsContainer.propTypes = {
+  mutator: PropTypes.object,
+  stripes: PropTypes.object,
+  resources: PropTypes.object,
+};
+
+export default stripesConnect(PatronPreRegistrationRecordsContainer);
