db06b6d2c839444b0929ba2e62cee09f995759b2
Yury Saukou <yury_saukou@epam.com>
2024-10-22 16:38:56 +0400

    sonar fix

--- src/hooks/useStagingUsersQuery/useStagingUsersQuery.js
+++ src/hooks/useStagingUsersQuery/useStagingUsersQuery.js
@@ -15,6 +15,7 @@ const DEFAULT_DATA = [];
 
 const useStagingUsersQuery = (params = {}, options = {}) => {
   const stripes = useStripes();
+  const [namespace] = useNamespace('staging-users');
 
   const {
     limit = stripes?.config?.maxUnpagedResourceCount || MAX_RECORDS,
@@ -29,21 +30,16 @@ const useStagingUsersQuery = (params = {}, options = {}) => {
   } = options;
 
   const ky = useOkapiKy({ tenant: tenantId });
-  const [namespace] = useNamespace();
 
-  const searchParams = {
-    query,
-    limit,
-    offset,
-  };
+  const searchParams = { limit, offset, query };
 
   const {
     data,
+    isFetched,
     isFetching,
     isLoading,
-    isFetched,
   } = useQuery({
-    queryKey: ['staging-users', namespace, tenantId, query, limit, offset],
+    queryKey: ['staging-users', limit, namespace, tenantId, offset, query],
     queryFn: ({ signal }) => ky.get(PATRON_PREREGISTRATIONS_API, { searchParams, signal }).json(),
     enabled,
     ...queryOptions,
@@ -53,8 +49,8 @@ const useStagingUsersQuery = (params = {}, options = {}) => {
     isFetched,
     isFetching,
     isLoading,
-    users: data?.staging_users ?? DEFAULT_DATA,
     totalRecords: data?.totalRecords ?? 0,
+    users: data?.staging_users ?? DEFAULT_DATA,
   };
 };
 
