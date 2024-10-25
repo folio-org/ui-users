924d09e08883266ecbfe5e152963f7d1cb74aeb1
Yury Saukou <yury_saukou@epam.com>
2024-10-25 09:49:41 +0400

    UIU-3224 Fix data visualization issues on the duplicates list

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,2 +1,2 @@
-// eslint-disable-next-line import/prefer-default-export
+export { default as getFullName } from './getFullName';
 export { default as getPatronDuplicatesQuery } from './getPatronDuplicatesQuery';
