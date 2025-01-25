eedb997338caf301a31adbd2d817c08224de4e79
Artem Blazhko <artem_blazhko@epam.com>
2025-01-25 08:00:47 +0200

    UIU-2701: Return appropriate error message when item for manual fee/fine can't be found (#2847)

--- src/components/Accounts/ChargeFeeFine/ChargeFeeFine.js
+++ src/components/Accounts/ChargeFeeFine/ChargeFeeFine.js
@@ -21,7 +21,10 @@ import {
   loadServicePoints,
   deleteOptionalActionFields,
 } from '../accountFunctions';
-import { SHARED_OWNER } from '../../../constants';
+import {
+  SHARED_OWNER,
+  NEW_FEE_FINE_FIELD_NAMES,
+} from '../../../constants';
 
 class ChargeFeeFine extends React.Component {
   static propTypes = {
@@ -72,6 +75,7 @@ class ChargeFeeFine extends React.Component {
       showConfirmDialog: false,
       notify: null,
       paymentNotify: null,
+      itemBarcode: '',
     };
     this.chargeAction = this.chargeAction.bind(this);
     this.payAction = this.payAction.bind(this);
@@ -232,7 +236,10 @@ class ChargeFeeFine extends React.Component {
 
   onClickSelectItem(barcode) {
     if (barcode !== '') {
-      this.props.mutator.activeRecord.update({ barcode });
+      this.props.mutator.activeRecord.update({
+        barcode,
+        isBarcodeValidated: true,
+      });
       if ((this.props.resources.activeRecord || {}).barcode === barcode) {
         this.setState({
           lookup: true,
@@ -241,7 +248,7 @@ class ChargeFeeFine extends React.Component {
     }
   }
 
-  onChangeOwner(ownerId) {
+  onChangeOwner(ownerId, itemBarcode = '') {
     const {
       resources,
       mutator,
@@ -253,7 +260,11 @@ class ChargeFeeFine extends React.Component {
       mutator.activeRecord.update({ shared });
     }
     mutator.activeRecord.update({ ownerId });
-    this.setState({ ownerId });
+
+    this.setState({
+      ownerId,
+      itemBarcode,
+    });
   }
 
   onChangeItem(item) {
@@ -395,7 +406,12 @@ class ChargeFeeFine extends React.Component {
     );
   }
 
-  onSubmitCharge = (data) => {
+  onSubmitCharge = (dataToSend) => {
+    const data = _.cloneDeep(dataToSend);
+
+    _.unset(data, NEW_FEE_FINE_FIELD_NAMES.ITEM_BARCODE);
+    _.unset(data, NEW_FEE_FINE_FIELD_NAMES.KEY_OF_ITEM_BARCODE);
+
     if (data.pay) {
       delete data.pay;
       this.type.remaining = data.amount;
@@ -433,6 +449,7 @@ class ChargeFeeFine extends React.Component {
       ownerId,
       feeFineTypeId,
       pay,
+      itemBarcode,
     } = this.state;
     this.item = _.get(resources, ['items', 'records', [0]], {});
     const feefines = _.get(resources, ['allfeefines', 'records'], []);
@@ -461,7 +478,7 @@ class ChargeFeeFine extends React.Component {
     parseFloat(selected).toFixed(2);
     let item;
 
-    if (this.item && (loanid || barcode)) {
+    if (this.item && (loanid || barcode || !resources.activeRecord?.isBarcodeValidated)) {
       item = {
         id: this.item.id || '',
         instance: this.item.title || '',
@@ -490,7 +507,8 @@ class ChargeFeeFine extends React.Component {
       ownerId: initialOwnerId,
       notify: !!(selectedFeeFine?.chargeNoticeId || selectedOwner?.defaultChargeNoticeId),
       feeFineId: '',
-      amount: ''
+      amount: '',
+      itemBarcode,
     };
 
     const initialActionValues = {
--- src/components/Accounts/ChargeFeeFine/ItemInfo.test.js
+++ src/components/Accounts/ChargeFeeFine/ItemInfo.test.js
@@ -1,39 +1,148 @@
-import { screen } from '@folio/jest-config-stripes/testing-library/react';
+import {
+  screen,
+  render,
+} from '@folio/jest-config-stripes/testing-library/react';
 import userEvent from '@folio/jest-config-stripes/testing-library/user-event';
 
-import renderWithRouter from 'helpers/renderWithRouter';
 import ItemInfo from './ItemInfo';
+import { NEW_FEE_FINE_FIELD_NAMES } from '../../../constants';
 
 import '__mock__/stripesCore.mock';
 import '__mock__/stripesSmartComponent.mock';
 
 jest.unmock('@folio/stripes/components');
 
-const renderItemInfo = (props) => renderWithRouter(
+const renderItemInfo = (props) => render(
   <ItemInfo {...props} />
 );
 
-const selectMock = jest.fn();
-
-const props = {
-  onClickSelectItem: selectMock,
+const basicProps = {
+  onClickSelectItem: jest.fn(),
   item: {},
-  editable: true
+  editable: true,
+  resources: {
+    items: {
+      records: [],
+    },
+    activeRecord: {
+      barcode: 'itemBarcode',
+    },
+  },
+  mutator: {
+    activeRecord: {
+      replace: jest.fn(),
+    },
+  },
+  form: {
+    change: jest.fn(),
+  },
+  values: {},
+};
+const labelIds = {
+  itemTitle: 'ui-users.charge.item.title',
+  chargeButton: 'ui-users.charge.item.button',
+};
+const testIds = {
+  itemBarcodeField: 'itemBarcodeField',
 };
 
-describe('Item Info component', () => {
-  it('if it renders', () => {
-    renderItemInfo(props);
-    expect(screen.getByText('ui-users.charge.item.title')).toBeInTheDocument();
+jest.mock('react-final-form', () => ({
+  Field: jest.fn(({
+    children,
+    'data-testid': testId,
+    validate,
+  }) => {
+    return children({
+      meta: {},
+      input: {
+        validate,
+        'data-testid': testId,
+        value: '',
+      },
+    });
+  }),
+}));
+jest.mock('react-router-dom', () => ({
+  Link: jest.fn(() => <div />),
+}));
+
+describe('ItemInfo', () => {
+  afterEach(() => {
+    jest.clearAllMocks();
   });
-  it('Charge Item Button', async () => {
-    renderItemInfo(props);
-    await userEvent.click(screen.getByText('ui-users.charge.item.button'));
-    expect(selectMock).toHaveBeenCalled();
+
+  describe('Initial render', () => {
+    beforeEach(() => {
+      renderItemInfo(basicProps);
+    });
+
+    it('should render item title', () => {
+      const itemTitle = screen.getByText(labelIds.itemTitle);
+
+      expect(itemTitle).toBeInTheDocument();
+    });
+
+    it('should get item information', async () => {
+      const chargeButton = screen.getByText(labelIds.chargeButton);
+
+      await userEvent.click(chargeButton);
+
+      expect(basicProps.onClickSelectItem).toHaveBeenCalled();
+    });
   });
-  it('charge item text field', async () => {
-    renderItemInfo(props);
-    await userEvent.type(document.querySelector('[placeholder="ui-users.charge.item.placeholder"]'), 'Test');
-    expect(document.querySelector('[placeholder="ui-users.charge.item.placeholder"]').value).toBe('Test');
+
+  describe('Component updating', () => {
+    const itemBarcode = 'newItemBarcode';
+    const newProps = {
+      ...basicProps,
+      resources: {
+        ...basicProps.resources,
+        items: {
+          records: [
+            {
+              id: 'itemId',
+            }
+          ]
+        }
+      },
+    };
+
+    beforeEach(async () => {
+      const { rerender } = renderItemInfo(basicProps);
+
+      const itemBarcodeField = screen.getByTestId(testIds.itemBarcodeField);
+
+      await userEvent.type(itemBarcodeField, itemBarcode);
+
+      rerender(<ItemInfo {...newProps} />);
+    });
+
+    it('should change "key" value of barcode field', () => {
+      expect(basicProps.form.change).toHaveBeenCalledWith(NEW_FEE_FINE_FIELD_NAMES.KEY_OF_ITEM_BARCODE, basicProps.values[NEW_FEE_FINE_FIELD_NAMES.KEY_OF_ITEM_BARCODE] ? 0 : 1);
+    });
+  });
+
+  describe('Item barcode changing', () => {
+    const itemBarcode = 'newItemBarcode';
+
+    beforeEach(async () => {
+      renderItemInfo(basicProps);
+
+      const itemBarcodeField = screen.getByTestId(testIds.itemBarcodeField);
+
+      await userEvent.type(itemBarcodeField, itemBarcode);
+    });
+
+    it('should replace activeRecord', () => {
+      expect(basicProps.mutator.activeRecord.replace).toHaveBeenCalledWith({
+        ...basicProps.resources.activeRecord,
+        isBarcodeValidated: false,
+        barcode: '',
+      });
+    });
+
+    it('should change item barcode field', () => {
+      expect(basicProps.form.change).toHaveBeenCalledWith(NEW_FEE_FINE_FIELD_NAMES.ITEM_BARCODE, itemBarcode);
+    });
   });
 });
