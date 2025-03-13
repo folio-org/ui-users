7598fb25ca645d8bb7bb03708d7eea9c2d1f1ac6
Ryan Berger <rberger@ebsco.com>
2025-03-13 14:41:44 -0400

    Revert changes from UIU-3333

--- src/components/Wrappers/withUserRoles.js
+++ src/components/Wrappers/withUserRoles.js
@@ -1,23 +1,32 @@
-import React, { useEffect, useState } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
+import { useQueryClient } from 'react-query';
 
-import { useStripes, useOkapiKy, useCallout } from '@folio/stripes/core';
+import { useNamespace, useStripes, useOkapiKy, useCallout } from '@folio/stripes/core';
 import isEqual from 'lodash/isEqual';
-import { useCreateAuthUserKeycloak, useUserAffiliationRoles } from '../../hooks';
+import { useCreateAuthUserKeycloak } from '../../hooks';
 import { KEYCLOAK_USER_EXISTANCE } from '../../constants';
 import { showErrorCallout } from '../../views/UserEdit/UserEditHelpers';
 
 const withUserRoles = (WrappedComponent) => (props) => {
-  const { okapi } = useStripes();
+  const { okapi, config } = useStripes();
+  const queryClient = useQueryClient();
+  const [affiliationRolesNamespace] = useNamespace({ key: 'user-affiliation-roles' });
   // eslint-disable-next-line react/prop-types
   const userId = props.match.params.id;
-  const initialAssignedRoleIds = useUserAffiliationRoles(userId);
   const [tenantId, setTenantId] = useState(okapi.tenant);
+  const [tenantsLoaded, setTenantsLoaded] = useState([]);
   const [assignedRoleIds, setAssignedRoleIds] = useState({});
+  const [initialAssignedRoleIds, setInitialAssignedRoleIds] = useState({});
   const [isCreateKeycloakUserConfirmationOpen, setIsCreateKeycloakUserConfirmationOpen] = useState(false);
   const callout = useCallout();
   const sendErrorCallout = error => showErrorCallout(error, callout.sendCallout);
 
   const { mutateAsync: createKeycloakUser } = useCreateAuthUserKeycloak(sendErrorCallout, { tenantId });
+
+  const searchParams = {
+    limit: config.maxUnpagedResourceCount,
+    query: `userId==${userId}`,
+  };
 
   const ky = useOkapiKy();
   const api = ky.extend({
@@ -26,29 +35,51 @@ const withUserRoles = (WrappedComponent) => (props) => {
     }
   });
 
-  useEffect(() => {
-    setAssignedRoleIds(initialAssignedRoleIds);
-  }, [JSON.stringify(initialAssignedRoleIds)]);
+  const setAssignedRoleIdsOnLoad = useCallback((data) => {
+    const assignedRoles = data.userRoles.map(({ roleId }) => roleId);
 
-  const updateUserRoles = async (roleIds) => {
-    const requests = Object.keys(roleIds).map((tenantIdKey) => {
+    setTenantsLoaded(tenantsLoaded.concat(tenantId));
+    setAssignedRoleIds({ ...assignedRoleIds, [tenantId]: assignedRoles });
+    setInitialAssignedRoleIds({ ...assignedRoleIds, [tenantId]: assignedRoles });
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [tenantId]);
+
+  useEffect(() => {
+    // eslint-disable-next-line react/prop-types
+    if (props.stripes.hasInterface('users-keycloak') && !!userId && !tenantsLoaded.includes(tenantId)) {
+      api.get(
+        'roles/users', { searchParams },
+      )
+        .json()
+        .then(setAssignedRoleIdsOnLoad)
+        // eslint-disable-next-line no-console
+        .catch(sendErrorCallout);
+    }
+  },
+  // Adding api, searchParams to deps causes infinite callback call. Listed deps are enough to track changes.
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  [userId, setAssignedRoleIdsOnLoad, tenantId]);
+
+  const updateUserRoles = (roleIds) => {
+    Object.keys(roleIds).forEach(async (tenantIdKey) => {
+      // Individually override header for each request.
       const putApi = ky.extend({
         hooks: {
           beforeRequest: [(req) => req.headers.set('X-Okapi-Tenant', tenantIdKey)]
         }
       });
-
-      return putApi.put(`roles/users/${userId}`, {
-        json: {
-          userId,
-          roleIds: roleIds[tenantIdKey],
-        }
-      }).catch((error) => {
-        sendErrorCallout(error);
-      });
+      putApi.put(
+        `roles/users/${userId}`, {
+          json: {
+            userId,
+            roleIds: roleIds[tenantIdKey],
+          }
+        },
+      ).json()
+        .then(await queryClient.invalidateQueries(affiliationRolesNamespace))
+        // eslint-disable-next-line no-console
+        .catch(sendErrorCallout);
     });
-
-    await Promise.allSettled(requests);
   };
 
   const checkUserInKeycloak = async () => {
