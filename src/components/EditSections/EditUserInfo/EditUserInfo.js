64df199eb4ff15ce5dc30325d4bca44fa6a77f9e
nikolai <northfacebodybags@yahoo.com>
2025-04-04 11:09:27 +0400

    UIU-3266: Add Patron Photo Capture Modal

--- src/components/EditSections/EditUserInfo/EditUserInfo.js
+++ src/components/EditSections/EditUserInfo/EditUserInfo.js
@@ -403,7 +403,6 @@ class EditUserInfo extends React.Component {
                 </Col>
               </Row>
             </Col>
-
             {
               displayProfilePicture &&
               <Col xs={3}>
