f01cae993d234911a4fe2d1a086cb69468a3888b
Dmytro-Melnyshyn <77053927+Dmytro-Melnyshyn@users.noreply.github.com>
2025-08-11 18:50:23 +0300

    UIU-3130: Update the user record view page to display custom fields under the Fees/fines, Loans, or Requests accordions as configured in settings. (#2947)

--- src/components/UserDetailSections/UserAccounts/UserAccounts.test.js
+++ src/components/UserDetailSections/UserAccounts/UserAccounts.test.js
@@ -1,8 +1,11 @@
+import { useStripes } from '@folio/stripes/core';
+import { useCustomFieldsQuery } from '@folio/stripes/smart-components';
 import { screen } from '@folio/jest-config-stripes/testing-library/react';
 import accounts from 'fixtures/account';
 import loans from 'fixtures/openLoans';
 import renderWithRouter from 'helpers/renderWithRouter';
 import UserAccounts from './UserAccounts';
+import { buildStripes } from '../../../../test/jest/__mock__/stripesCore.mock';
 
 const renderUserAccounts = (props) => renderWithRouter(<UserAccounts {...props} />);
 
@@ -35,6 +38,7 @@ const props = (emptyData) => {
       isPending: !emptyData,
     },
     accordionId: 'UserAccounts',
+    customFields: [],
     expanded: true,
     onToggle: onToggleMock,
     location: {
@@ -50,7 +54,15 @@ const props = (emptyData) => {
   };
 };
 
+const stripes = buildStripes();
+
 describe('Render UserAccounts component', () => {
+  beforeEach(() => {
+    jest.clearAllMocks();
+
+    useStripes.mockReturnValue(stripes);
+  });
+
   it('Check if List is rendered', () => {
     renderUserAccounts(props(true));
     expect(screen.getAllByText('List Component')[0]).toBeInTheDocument();
@@ -60,6 +72,19 @@ describe('Render UserAccounts component', () => {
     renderUserAccounts(props(false));
     expect(document.querySelector('[id="numOpenAccounts"]')).toBeNull();
   });
+
+  describe('when there is no required permission', () => {
+    it('should not render fees/fines list', () => {
+      useStripes.mockReturnValue(buildStripes({
+        hasPerm: jest.fn().mockReturnValue(false),
+      }));
+
+      renderUserAccounts(props(false));
+
+      expect(screen.queryByText('Fees/Fines')).not.toBeInTheDocument();
+    });
+  });
+
   describe('when user is of type dcb', () => {
     it('should not display "Create fee/fine" button', () => {
       const alteredProps = {
@@ -93,6 +118,19 @@ describe('Render UserAccounts component', () => {
       };
       renderUserAccounts(alteredProps);
       expect(screen.queryByText('Create fee/fine')).toBeNull();
+    });
+  });
+
+  describe('when custom fields are present', () => {
+    it('should display "ViewCustomFieldsRecord"', () => {
+      useCustomFieldsQuery.mockImplementation(() => ({
+        isCustomFieldsError: false,
+        customFields: [{ id: 'customField1' }],
+      }));
+
+      renderUserAccounts(props(true));
+
+      expect(screen.getByText('ViewCustomFieldsRecord')).toBeInTheDocument();
     });
   });
 });
