67b86e5047017e4a2e88e623b2663badb2db8888
Dmytro Melnyshyn <dmytro_melnyshyn@epam.com>
2026-05-25 12:19:21 +0300

    add 'jest.requireActual('@folio/stripes/util')' for stripesUtils.mock.js

--- test/jest/__mock__/stripesUtils.mock.js
+++ test/jest/__mock__/stripesUtils.mock.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
 jest.mock('@folio/stripes/util', () => ({
+  ...jest.requireActual('@folio/stripes/util'),
   getSourceSuppressor: jest.fn(() => () => false),
   getHeaderWithCredentials: jest.fn(() => ({
     headers: {}
