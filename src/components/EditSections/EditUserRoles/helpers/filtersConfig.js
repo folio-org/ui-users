75f15b978267f898264074b9bdc8193f969ff201
John Coburn <jcoburn@ebsco.com>
2026-09-21 16:49:47 -0500

    refactor the crazy conditionals for better clarity

--- src/components/EditSections/EditUserRoles/helpers/filtersConfig.js
+++ src/components/EditSections/EditUserRoles/helpers/filtersConfig.js
@@ -5,32 +5,26 @@ import createBinaryStatusFilter from './createBinaryStatusFilter';
 export const statusFilterConfig = createBinaryStatusFilter({
   label: <FormattedMessage id="ui-users.roles.modal.filter.status.label" />,
   name: 'status',
-  cql: 'status',
   trueValue: {
     displayName: <FormattedMessage id="ui-users.roles.modal.assigned" />,
     name: 'assigned',
-    cql: 'assigned',
   },
   falseValue: {
     displayName: <FormattedMessage id="ui-users.roles.modal.unassigned" />,
     name: 'unassigned',
-    cql: 'unassigned',
   },
 });
 
 export const selectionFilterConfig = createBinaryStatusFilter({
   label: <FormattedMessage id="ui-users.roles.modal.filter.selection.label" />,
   name: 'selection',
-  cql: 'selection',
   trueValue: {
     displayName: <FormattedMessage id="ui-users.roles.modal.selected" />,
     name: 'selected',
-    cql: 'selected',
   },
   falseValue: {
     displayName: <FormattedMessage id="ui-users.roles.modal.unselected" />,
     name: 'unselected',
-    cql: 'unselected',
   },
 });
 
